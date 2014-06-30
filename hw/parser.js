f5999260fa838d053b612de74d335493ae6e36e2
Yannick Schurter <yannick.schurter@inist.fr>
2014-06-30 13:36:42 +0200

    update highwire parser

--- hw/parser.js
+++ hw/parser.js
@@ -7,47 +7,48 @@
 var Parser = require('../.lib/parser.js');
 
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
-  //Ensuite tu peux accéder à ec qui est un objet javascript. 
-  //Donc par exemple ec.size te donnera la taille en octet de la consultation en question
-  var result   = {};
-  //var domain   = parsedUrl.hostname; /* Just the lowercased hostname portion of the host. Example: 'host.com' */
-  var pathname   = parsedUrl.pathname; /* The path section of the URL, that comes after the host and before the query
-                                      //, including the initial slash if present. Example: '/p/a/t/h' */
-  var hostname   = parsedUrl.hostname;
-  var tailleFile = ec ? parseInt(ec.size, 10) : undefined; // because size could be a string (comming from the test CSV)
-  var match;
-  // exemple http://cshprotocols.cshlp.org/content/2012/5/pdb.top069344.full.pdf
-  //console.error(parsedUrl);
-  /*
-{ protocol: 'http:',
-  slashes: true,
-  auth: null,
-  host: 'cshprotocols.cshlp.org',
-  port: null,
-  hostname: 'cshprotocols.cshlp.org',
-  hash: null,
-  search: '',
-  query: {},
-  pathname: '/content/2012/5/pdb.top069344.full.pdf',
-  path: '/content/2012/5/pdb.top069344.full.pdf',
-  href: 'http://cshprotocols.cshlp.org/content/2012/5/pdb.top069344.full.pdf' }
-  */
 
-  // it is necessary to check weight of page, if < 10 ko it is not the article but login page
-  if ((match = /^\/content\/.*\.full$/.exec(pathname)) !== null && (!tailleFile || tailleFile > 10000)) {
-    // example : http://rsbl.royalsocietypublishing.org/content/6/4/458.full
-    result.title_id = hostname;
-    result.unitid   = hostname + pathname.replace(/\.full$/, '');
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
+  var result   = {};
+  var pathname = parsedUrl.pathname;
+  var hostname = parsedUrl.hostname;
+  var fileSize = ec ? parseInt(ec.size, 10) : undefined; // because size could be a string (comming from the test CSV)
+  var match;
+
+  result.title_id = hostname;
+
+  if ((match = /^\/content\/([0-9]+\/[0-9]+\/[0-9a-zA-Z\.]+\.(abstract|full|full\.pdf))$/.exec(pathname)) !== null) {
+    /**
+     * examples : http://rsbl.royalsocietypublishing.org/content/6/4/458.abstract
+     *            http://rsbl.royalsocietypublishing.org/content/6/4/458.full
+     *            http://rsbl.royalsocietypublishing.org/content/6/4/458.full.pdf
+     *            http://cshprotocols.cshlp.org/content/2012/5/pdb.top069344.full.pdf
+     */
+    result.unitid  = hostname + '/' + match[1];
+
+    switch (match[2]) {
+    case 'abstract':
+      result.rtype = 'ABS';
+      result.mime  = 'HTML';
+      break;
+    case 'full':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'HTML';
+      break;
+    case 'full.pdf':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'PDF';
+      break;
+    }
+  } else if ((match = /^\/content\/([0-9]+\/[0-9]+\.toc)$/.exec(pathname)) !== null) {
+    // example : http://www.jimmunol.org.gate1.inist.fr/content/188/3.toc
+    result.unitid = hostname + '/' + match[1];
+    result.rtype  = 'TOC';
+    result.mime   = 'HTML';
   }
 
-  if ((match = /^\/content\/.*\.full\.pdf$/.exec(pathname)) !== null) {
-    // example : http://cshprotocols.cshlp.org/content/2012/5/pdb.top069344.full.pdf
-    result.title_id = hostname;
-    result.unitid   = hostname + pathname.replace(/\.full\.pdf$/, '');
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
+  // if the size is less than 10ko, it's not the actual article but a login page
+  if (fileSize && fileSize < 10000) {
+    result._granted = false;
   }
 
   return result;
