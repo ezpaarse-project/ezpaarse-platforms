a552c698764eb1ef1cc0bc9f099b656cdee948f3
Yannick Schurter <yannick.schurter@inist.fr>
2014-07-17 14:49:30 +0200

    add scraper and new Kbart

---
+++ cbo/scrapers/scrape_cbo_books.js
@@ -0,0 +1,64 @@
+#!/usr/bin/env node
+
+// Write a kbart file for the CBO PKB
+// Write on stderr the progression
+// Usage : ./scrape_cbo_books.js
+
+/*jslint maxlen: 180*/
+
+'use strict';
+
+var fs      = require('fs');
+var path    = require('path');
+var unzip   = require('unzip');
+var request = require('request');
+
+var r = request({
+  method: 'GET',
+  uri: 'http://ebooks.cambridge.org/download?f=CambridgeBooks_GlobalTitleList_txt&prodCode=CBO&type=kbart',
+  proxy: process.env.HTTP_PROXY || process.env.http_proxy,
+  headers: {
+    'User-Agent': 'node'
+  }
+});
+
+r.on('error', function (err) {
+  console.error(err);
+  process.exit(1);
+});
+
+r.on('response', function (res) {
+  if (res.statusCode != 200) {
+    console.error('[Error] The server responded with a status of ' + res.statusCode + ', exiting');
+    process.exit(1);
+  }
+
+  var match = /filename=(.*)$/.exec(res.headers['content-disposition']);
+  var fileName;
+  var fileDate;
+  if (match) {
+    // CambridgeBooks_GlobalTitleList_2014-07-16.zip
+    fileName = match[1];
+  } else {
+    console.error('[Error] Couldn\'t get the name of the downloaded file');
+    process.exit(1);
+  }
+
+  var filePath   = path.join(__dirname, '../pkb', fileName.replace(/\.zip$/, '.txt'));
+  var fileStream = fs.createWriteStream(filePath);
+
+  var nbEntries = 0;
+  res.pipe(unzip.Parse())
+  .on('entry', function (entry) {
+    if (++nbEntries > 1) {
+      console.error('[Warning] the zip file contains multiple entries, ignoring ' + entry.path);
+      entry.autodrain();
+    } else {
+      console.error('[Info] writing ' + entry.path + ' into ' + filePath);
+    }
+    entry.pipe(fileStream);
+  })
+  .on('close', function () {
+    console.error('[Info] Completed');
+  });
+});
