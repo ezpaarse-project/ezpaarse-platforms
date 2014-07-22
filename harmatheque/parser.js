051ab2352d45c269130fe73bedcc456a84b401c4
Stéphane Gully <stephane.gully@gmail.com>
2014-07-22 14:38:27 +0200

    integrates platform analysis update: http://analogist.couperin.org/platforms/harmatheque/start

--- harmatheque/parser.js
+++ harmatheque/parser.js
@@ -22,6 +22,35 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.mime     = 'PDF';
     result.title_id = match[1];
     result.unitid   = match[1];
+  } else   if ((match = /^\/downloadebook\/(.+)$/.exec(path)) !== null) {
+    // http://www.harmatheque.com/downloadebook/la-socionalyse-narrative-theorie-critique-et-pratique-du-changement-social-42310
+
+    result.rtype    = 'BOOK';
+    result.mime     = 'PDF';
+    result.title_id = match[1];
+    result.unitid   = match[1];
+  } else if ((match = /^\/downloadepub\/(.+)$/.exec(path)) !== null) {
+    // http://www.harmatheque.com/downloadepub/apprendre-dans-les-reseaux-de-pme-le-role-des-contacts-personnels-41598
+
+    result.rtype    = 'BOOK';
+    result.mime     = 'EPUB';
+    result.title_id = match[1];
+    result.unitid   = match[1];
+  } else if ((match = /^\/playepub\/(.+)$/.exec(path)) !== null) {
+    // http://www.harmatheque.com/playepub/apprendre-dans-les-reseaux-de-pme-le-role-des-contacts-personnels-41598
+
+    result.rtype    = 'BOOK';
+    result.mime     = 'EPUB';
+    result.title_id = match[1];
+    result.unitid   = match[1];
+  } else if ((match = /^\/pdfswf\/livre/.exec(path)) !== null) {
+    // http://pdf.harmattan.fr/pdfswf/livre.asp?idbook=776436cb79d84789b054729000dc3b58&imc=1&isbn=9782343006932
+
+    result.rtype    = 'BOOK';
+    result.mime     = 'PDF';
+    result.title_id = parsedUrl.query.idbook;
+    result.unitid   = parsedUrl.query.idbook;
+    result.pisbn    = parsedUrl.query.isbn;
   }
 
   return result;
