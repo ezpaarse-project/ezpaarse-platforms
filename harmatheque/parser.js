56c6804be1bd3c989e958b3bbda13dc58819ac9a
Yannick Schurter <yannick.schurter@inist.fr>
2014-07-25 15:48:44 +0200

    jshint

--- harmatheque/parser.js
+++ harmatheque/parser.js
@@ -20,7 +20,7 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
 
     result.rtype    = 'BOOK';
     result.mime     = 'PDF';
-    result.title_id = match[1].replace(new RegExp('-[0-9]+$'), '');;
+    result.title_id = match[1].replace(new RegExp('-[0-9]+$'), '');
     result.unitid   = match[1];
   } else   if ((match = /^\/downloadebook\/(.+)$/.exec(path)) !== null) {
     // http://www.harmatheque.com/downloadebook/la-socionalyse-narrative-theorie-critique-et-pratique-du-changement-social-42310
@@ -34,14 +34,14 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
 
     result.rtype    = 'BOOK';
     result.mime     = 'EPUB';
-    result.title_id = match[1].replace(new RegExp('-[0-9]+$'), '');;
+    result.title_id = match[1].replace(new RegExp('-[0-9]+$'), '');
     result.unitid   = match[1];
   } else if ((match = /^\/playepub\/(.+)$/.exec(path)) !== null) {
     // http://www.harmatheque.com/playepub/apprendre-dans-les-reseaux-de-pme-le-role-des-contacts-personnels-41598
 
     result.rtype    = 'BOOK';
     result.mime     = 'EPUB';
-    result.title_id = match[1].replace(new RegExp('-[0-9]+$'), '');;
+    result.title_id = match[1].replace(new RegExp('-[0-9]+$'), '');
     result.unitid   = match[1];
   } else if ((match = /^\/pdfswf\/livre/.exec(path)) !== null) {
     // http://pdf.harmattan.fr/pdfswf/livre.asp?idbook=776436cb79d84789b054729000dc3b58&imc=1&isbn=9782343006932
