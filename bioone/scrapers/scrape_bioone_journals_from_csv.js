3e20e1a4b6da710019728fc11a7c046008e4e23c
Yannick Schurter <yannick.schurter@inist.fr>
2014-08-14 11:07:44 +0200

    fix bioone scraper

--- bioone/scrapers/scrape_bioone_journals_from_csv.js
+++ bioone/scrapers/scrape_bioone_journals_from_csv.js
@@ -7,17 +7,17 @@
 
 'use strict';
 
-var request     = require('request').defaults({
+var request = require('request').defaults({
   proxy: process.env.http_proxy ||
          process.env.HTTP_PROXY ||
          process.env.https_proxy ||
          process.env.HTTPS_PROXY
 });
 
-var csv         = require('csv');
-var PkbRows     = require('../../.lib/pkbrows.js');
-var pkb         = new PkbRows('bioone');
-var URL         = require('url');
+var csvParse = require('csv').parse;
+var PkbRows  = require('../../.lib/pkbrows.js');
+var pkb      = new PkbRows('bioone');
+var URL      = require('url');
 
 // setKbartName() is required to fix the kbart output file name
 //pkb.consortiumName = 'BioOne';       // default empty
@@ -31,35 +31,41 @@ var journalsUrl = 'http://www.bioone.org/userimages/ContentEditor/1376686837415/
 
 console.error('Downloading: ' + journalsUrl);
 
-request.get({uri: journalsUrl, encoding: 'binary'}, function (err, resp, body) {
-  if (err) { throw err; }
+var parser = csvParse({ delimiter: ',', columns: true});
 
-  csv()
-    .from(body, { delimiter: ',', columns: true})
-    .on('record', function (data) {
-      var journalInfo = {}, match;
-      // initialize a kbart record
-      journalInfo = pkb.initRow(journalInfo);
-      // collect informations
-      journalInfo.publication_title = data['Title'];
-      journalInfo.print_identifier = data['ISSN'];
-      journalInfo.online_identifier = data['E-ISSN'];
-      journalInfo.title_url = data['BioOne URL'];
-      if (journalInfo.title_url.length) {
-        var parsedUrl = URL.parse(journalInfo.title_url, true);
-        if ((match = /\/loi\/([^\/]+)/.exec(parsedUrl.pathname))) {
-          // http://www.bioone.org/loi/rama
-          journalInfo.title_id = match[1];
-        } else if ((match = /\/action\/showBookSeries/.exec(parsedUrl.pathname))) {
-          // http://www.bioone.org/action/showBookSeries?seriesCode=rbba
-          if (parsedUrl.query['seriesCode']) {
-            journalInfo.title_id = parsedUrl.query['seriesCode'];
-          }
-        }
+request.get({ uri: journalsUrl }).pipe(parser)
+.on('error', function (err) {
+    console.error(err);
+    process.exit(1);
+})
+.on('readable', function () {
+
+  var data        = parser.read();
+  var journalInfo = pkb.initRow({});
+  var match;
+
+  // collect informations
+  journalInfo.publication_title = data['Title'].trim();
+  journalInfo.print_identifier  = data['ISSN'].trim();
+  journalInfo.online_identifier = data['E-ISSN'].trim();
+  journalInfo.title_url         = data['BioOne URL'].trim();
+
+  if (journalInfo.title_url.length) {
+    var parsedUrl = URL.parse(journalInfo.title_url, true);
+    if ((match = /\/loi\/([^\/]+)/.exec(parsedUrl.pathname))) {
+      // http://www.bioone.org/loi/rama
+      journalInfo.title_id = match[1];
+    } else if ((match = /\/action\/showBookSeries/.exec(parsedUrl.pathname))) {
+      // http://www.bioone.org/action/showBookSeries?seriesCode=rbba
+      if (parsedUrl.query['seriesCode']) {
+        journalInfo.title_id = parsedUrl.query['seriesCode'];
       }
-      pkb.addRow(journalInfo);
-    })
-    .on('end', function() { pkb.writeKbart(); });
+    }
+  }
+
+  pkb.addRow(journalInfo);
+})
+.on('end', function () {
+  pkb.writeKbart();
   console.error('BioOne scraping is finished..\nFile : ' + pkb.kbartFileName + ' generated');
 });
-
