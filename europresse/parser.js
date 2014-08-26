8e6c8e87768f67e14e9c04a46659fd56a2fdbc34
pseudom <dominique.lechaudel@gmail.com>
2014-08-26 18:53:45 +0200

    first version of europresse platform

---
+++ europresse/parser.js
@@ -0,0 +1,59 @@
+#!/usr/bin/env node
+
+// ##EZPAARSE
+
+/*jslint maxlen: 150*/
+'use strict';
+var Parser = require('../.lib/parser.js');
+
+/**
+ * Identifie les consultations de la plateforme europresse
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  var result = {};
+  var path   = parsedUrl.pathname;
+  var param  = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  var match, idElements;
+
+  //console.error(param);
+
+  if ((match = /^\/WebPages\/Pdf\/Document.aspx$/.exec(path)) !== null) {
+    // ttp://www.bpe.europresse.com.bases-doc.univ-lorraine.fr/WebPages/Pdf/Document.aspx?DocName=pdf%c2%b720140606%c2%b7LM_p%c2%b7LIV6
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    if (param.DocName) {
+      idElements = param.DocName.split('·');
+      //console.error(idElements);
+      result.title_id = idElements[2].replace('_p', '');
+      result.unitid   = param.DocName;
+    }
+  } else if ((match = /^\/WebPages\/Search\/Doc.aspx$/.exec(path)) !== null) {
+    // http://www.bpe.europresse.com.bases-doc.univ-lorraine.fr/WebPages/Search/Doc.aspx?
+    // DocName=news%C2%B720140606%C2%B7ML%C2%B76225112&ContainerType=SearchResult
+    // 
+    // http://www.bpe.europresse.com.bases-doc.univ-lorraine.fr/WebPages/Search/Doc.aspx?
+    // DocName=bio%C2%B7EVI%C2%B72944&ContainerType=SearchResultBio
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'HTML';
+    if (param.DocName) {
+      idElements = param.DocName.split('·');
+      //console.error(idElements);
+      result.unitid   = param.DocName;
+      if (param.ContainerType === "SearchResult") {
+        result.title_id = idElements[2].replace('_p', '');
+      } else if (param.ContainerType === "SearchResultBio") {
+        result.title_id = idElements[1];
+      }
+    }
+  }
+  return result;
+});
+
---
+++ europresse/scrapers/scrape_europresse_titles.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+// Write a kbart file for OHB journals
+
+/*jslint maxlen: 180*/
+
+'use strict';
+
+
+var XLSX = require('xlsx');
+
+var PkbRows = require('../../.lib/pkbrows.js');
+var europresseFileCollections = __dirname + '/../pkb/sources_europresse_pour_bibliotheque_d_enseignement.xlsx';
+
+var xlsx = XLSX.readFile(europresseFileCollections);
+var json;
+
+for (var i = 0, j = xlsx.SheetNames.length; i < j; i++) {
+  var sheet = xlsx.Sheets[xlsx.SheetNames[i]];
+
+  json = XLSX.utils.sheet_to_json(sheet, { header: "A" });
+  if (json.length > 0) { break; }
+}
+
+if (json.length === 0) {
+  console.error('No data found in the Excel file');
+  process.exit(1);
+}
+
+var pkb       = new PkbRows('europresse');
+pkb.setKbartName();
+
+for (var l = 0; l < json.length; l++) {
+  var kbartRow = pkb.initRow({});
+
+  if (json[l]['A']) {kbartRow.publication_title = json[l]['A']; }
+  if (json[l]['B']) {kbartRow.print_identifier = json[l]['B']; }
+  if (json[l]['C']) {kbartRow.title_id = json[l]['C']; }
+
+  pkb.addRow(kbartRow);
+}
+
+pkb.writeKbart();
