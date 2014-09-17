e354ae8cf036512126fa852049facac9d5331488
pseudom <dominique.lechaudel@gmail.com>
2014-09-17 18:16:35 +0200

    factorize KBplus file requesting

--- .lib/pkbrows.js
+++ .lib/pkbrows.js
@@ -303,4 +303,96 @@ PkbRows.prototype.writeKbart = function (callback) {
   writeRow(i++);
 };
 
+PkbRows.prototype.getKbartFromKBPlus = function (KBPlusPkg, callback) {
+  var request     = require('request').defaults({
+    proxy: process.env.http_proxy ||
+           process.env.HTTP_PROXY ||
+           process.env.https_proxy ||
+           process.env.HTTPS_PROXY
+  });
+
+  callback = callback || function () {};
+  var self = this;
+
+// we use json JUSP format because CSV is not well formated : eg non quoted " in title
+// json format contains a header like this :
+/*{"header":
+   {
+    "version":"2.0",
+    "jcid":"",
+    "url":"uri://kbplus/pkg/99",
+    "pkgcount":81,
+    "titles": [
+      {
+        "title":"AI Communications",
+        "issn":"0921-7126",
+        "eissn":"1875-8452",
+        "jusp":"11926",
+        "startDate":"1987-01-01",
+        "endDate":"",
+        "startVolume":"0",
+        "endVolume":"",
+        "startIssue":"1",
+        "endIssue":"",
+        "embargo":"",
+        "titleUrl":"http://iospress.metapress.com/content/103140",
+        "doi":"",
+        "coverageDepth":null,
+        "coverageNote":null,
+        "publisher":"IOS Press"
+
+**/
+
+  var journalsUrl = 'http://www.kbplus.ac.uk/kbplus/publicExport/pkg/' + KBPlusPkg + '?format=json';
+  console.error('Downloading: ' + journalsUrl);
+
+  request.get({uri: journalsUrl, encoding: 'binary'}, function (err, resp, body) {
+    if (err) { throw err; }
+
+    // convert the result string into a JSON object
+    var jsonSource = JSON.parse(body);
+    if (jsonSource.header.pkgcount) {
+      console.error('Masterlist contains ' + jsonSource.header.pkgcount + ' items');
+    } else {
+      throw "KB+ file doesn't contains header";
+    }
+
+    jsonSource.titles.forEach(function (jsonRow) {
+      // extract data
+      var journalInfo = {};
+      // initialize a kbart record
+      journalInfo = self.initRow(journalInfo);
+      if (jsonRow.title && jsonRow.title !== null) {
+        journalInfo.publication_title = jsonRow.title;
+      }
+      if (jsonRow.titleUrl && jsonRow.titleUrl !== null) {
+        journalInfo.title_url = jsonRow.titleUrl;
+        // make title_id form last part of titleUrl
+        var titleUrlParts = jsonRow.titleUrl.split('/');
+        if (titleUrlParts.length) {
+          journalInfo.title_id = titleUrlParts[titleUrlParts.length - 1];
+        }
+      } 
+      if (jsonRow.issn && jsonRow.issn !== null ) {
+        journalInfo.print_identifier = jsonRow.issn;
+      }
+      if (jsonRow.eissn && jsonRow.eissn !== null) {
+        journalInfo.online_identifier = jsonRow.eissn;
+      }
+      if (jsonRow.publisher && jsonRow.publisher !== null) {
+        journalInfo.publisher_name = jsonRow.publisher;
+      }
+
+      self.addRow(journalInfo);
+
+    });
+
+
+    // Loop on JSON row is finished, we can write the result.
+    self.writeKbart();
+    console.error('Masterlist scraping is finished..\nFile : '
+     + self.kbartFileName + ' generated with ' + self.rows.length + ' elements');
+  });
+}
+
 module.exports = PkbRows;
