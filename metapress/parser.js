54987cf4805a657a23c4c1d0abb7d748969463ae
pseudom <dominique.lechaudel@gmail.com>
2014-09-17 18:13:37 +0200

    correction to title_id recognition

--- metapress/parser.js
+++ metapress/parser.js
@@ -14,17 +14,22 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
   // http://iospress.metapress.com/content/977v06v38w552j8r/fulltext.html
   // http://iospress.metapress.com/content/977v06v38w552j8r/fulltext.pdf
   if ((match = /^\/content\/([a-z0-9]+)\/fulltext\.(html|pdf)\/?$/i.exec(path)) !== null) {
-    result.title_id = match[1];
+    //result.title_id = match[1];
     result.unitid   = match[1];
     result.mime     = match[2].toUpperCase();
     result.rtype    = 'ARTICLE';
 
   // http://iospress.metapress.com/content/103143
+  } else if ((match = /^\/content\/([a-z0-9]{6})\/?$/i.exec(path)) !== null) {
+    result.title_id = match[1];
+    result.mime     = 'HTML';
+    result.rtype    = 'TOC';
+
   // http://iospress.metapress.com/content/m24924142248/?p=a1cde377bd124a8894d178f0966e27ca&pi=3
-  } else if ((match = /^\/content\/([a-z0-9]+)\/?$/i.exec(path)) !== null) {
-    result.title_id   = match[1];
-    result.mime  = 'HTML';
-    result.rtype = 'TOC';
+  } else if ((match = /^\/content\/([a-z0-9]{7,})\/?$/i.exec(path)) !== null) {
+    result.unitid = match[1];
+    result.mime   = 'HTML';
+    result.rtype  = 'TOC';
 
   // http://iospress.metapress.com/content/1875-8584
   } else if ((match = /^\/content\/([0-9]{4}-[0-9]{4})\/?$/i.exec(path)) !== null) {
