8b07bb7708dfb96295418ed28ad5f9bff56dfd4e
pseudom <dominique.lechaudel@gmail.com>
2014-12-03 16:55:48 +0100

    remove all from parser except ARTICLE

--- irevues/parser.js
+++ irevues/parser.js
@@ -30,12 +30,7 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.mime  = 'HTML';
     result.title_id = match[1];
  **/
- if ((match = /^\/(handle)\/([0-9]+)\/([0-9]+)/.exec(path)) !== null) {
-    // http://documents.irevues.inist.fr/handle/2042/38749
-    result.rtype = 'MISC';
-    result.mime  = 'HTML';
-    result.title_id = match[3];
-  } else if ((match = /^\/(bitstream\/handle)\/([0-9]+)\/([0-9]+)\/(.*\.pdf)$/.exec(path)) !== null) {
+ if ((match = /^\/(bitstream\/handle)\/([0-9]+)\/([0-9]+)\/(.*\.pdf)$/.exec(path)) !== null) {
     // http://documents.irevues.inist.fr/bitstream/handle/2042/39472/FM%20XXIV-2%20148-158.pdf?sequence=1
     result.rtype = 'ARTICLE';
     result.mime  = 'PDF';
@@ -47,11 +42,6 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.mime  = 'HTML';
     result.title_id = match[3];
     result.unitid = match[4];
-  } else if ((match = /^\/$/.exec(path)) !== null) {
-    // http://irevues.inist.fr/?mmddom=off&sousdom=BIOL
-    result.rtype = 'TOC';
-    result.mime  = 'HTML';
-    if (param.sousdom) { result.title_id = param.sousdom; }
   }
   return result;
 });
