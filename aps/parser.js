07b2ce48b54aece800b81c305b19b5b95eb8ee07
pseudom <dominique.lechaudel@gmail.com>
2015-03-30 15:44:04 +0200

    new version of parser with journals.aps.org (generic domain)

--- aps/parser.js
+++ aps/parser.js
@@ -7,6 +7,8 @@
 /**
  * parser for http://publish.aps.org platform
  * http://analogist.couperin.org/platforms/aps/
+ * update 2015/03/30
+ * http://analogist.couperin.org/platforms/aps/start-2
  */
 'use strict';
 var URL    = require('url');
@@ -18,6 +20,7 @@ module.exports = new Parser(function analyseEC(url) {
   var domain   = url.hostname; /* Just the lowercased hostname portion of the host. Example: 'host.com' */
 
   var match;
+  console.error(domain);
 
   if (domain == 'physics.aps.org') {
     result.title_id  = domain.replace(".aps.org", "");
@@ -33,6 +36,33 @@ module.exports = new Parser(function analyseEC(url) {
       result.rtype  = 'ARTICLE';
       result.mime   = 'PDF';
     }
+  } else if (domain == 'journals.aps.org') {
+    if ((match = /\/(([a-zA-Z]+)\/issues\/([0-9]+\/[0-9]+))$/.exec(path)) !== null) {
+      // http://journals.aps.org/pra/issues/91/1
+      result.unitid = match[1];
+      result.title_id = match[2];
+      result.rtype = 'TOC';
+      result.mime = 'HTML';
+    } else if ((match = /\/(([a-zA-Z]+)\/abstract\/([0-9\.]+\/[a-zA-Z0-9\.]+))$/.exec(path)) !== null) {
+      // http://journals.aps.org/pra/abstract/10.1103/PhysRevA.91.010501
+      result.unitid = match[3];
+      result.title_id = match[2];
+      result.rtype = 'ABS';
+      result.mime = 'HTML';
+    } else if ((match = /\/(([a-zA-Z]+)\/article\/([0-9\.]+\/[a-zA-Z0-9\.]+))\/section\/fulltext$/.exec(path)) !== null) {
+      // http://journals.aps.org/pra/article/10.1103/PhysRevA.91.032105/section/fulltext
+      result.unitid = match[3];
+      result.title_id = match[2];
+      result.rtype = 'ARTICLE';
+      result.mime = 'HTML';
+    } else if ((match = /\/(([a-zA-Z]+)\/pdf\/([0-9\.]+\/[a-zA-Z0-9\.]+))$/.exec(path)) !== null) {
+      // http://journals.aps.org/pra/pdf/10.1103/PhysRevA.91.033602
+      result.unitid = match[3];
+      result.title_id = match[2];
+      result.rtype = 'ARTICLE';
+      result.mime = 'PDF';
+    }
+
   } else {
     if ((match = /\/toc\/(([a-zA-Z]+)\/v[a-zA-Z0-9]+\/i[a-zA-Z0-9]+)$/.exec(path)) !== null) {
       // http://prl.aps.org.bases-doc.univ-lorraine.fr/toc/PRL/v111/i25
