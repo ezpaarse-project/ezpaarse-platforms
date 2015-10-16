149975b0e5e521feb994efd1b0ee99f13eed58e9
aloukili <tech.abdel@gmail.com>
2015-10-16 16:57:32 +0200

    parser hw is update

--- hw/parser.js
+++ hw/parser.js
@@ -13,21 +13,31 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   var hostname = parsedUrl.hostname;
   var fileSize = ec ? parseInt(ec.size, 10) : undefined; // because size could be a string (comming from the test CSV)
   var match;
+  var testvalue;
+  var info ;
 
   result.title_id = hostname;
 //console.log(parsedUrl);
 
-  if ((match = /^\/content\/([0-9]+\/[0-9\-]+\/[0-9a-zA-Z\.]+\.(abstract|full|full\.pdf))(\+html)?$/.exec(pathname)) !== null) {
+  if ((match = /^\/content\/(([0-9]+)\/([0-9\-]+)\/([0-9a-zA-Z\.]+)\.(abstract|full|full\.pdf))(\+html)?$/.exec(pathname)) !== null) {
     /**
      * examples : http://rsbl.royalsocietypublishing.org/content/6/4/458.abstract
      *            http://rsbl.royalsocietypublishing.org/content/6/4/458.full
      *            http://rsbl.royalsocietypublishing.org/content/6/4/458.full.pdf
      *            http://cshprotocols.cshlp.org/content/2012/5/pdb.top069344.full.pdf
      *            http://dmm.biologists.org.gate1.inist.fr/content/1/2-3/69.1.full.pdf+html
+     *            http://geophysics.geoscienceworld.org/content/78/2/B49.full
      */
     result.unitid  = hostname + '/' + match[1];
-
-    switch (match[2]) {
+     testvalue = match[4].split('.')[0];
+    if (testvalue != "pdb") {
+    result.vol = match[2];
+    result.issue = match[3];
+    result.first_page = match[4].split('.')[0];
+    } else {
+      result.doi = "10.1101/" + match[4];
+    }
+    switch (match[5]) {
     case 'abstract':
       result.rtype = 'ABS';
       result.mime  = 'HTML';
@@ -41,13 +51,20 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
       result.mime  = 'PDF';
       break;
     }
-  } else if ((match = /^\/content\/([a-z]+\/[0-9]+\/[0-9\-]+\/[0-9a-zA-Z\.]+\.(abstract|full|full\.pdf))(\+html)?$/.exec(pathname)) !== null) {
+  } else if ((match = /^\/content\/(([a-z]+)\/([0-9]+)\/([0-9\-]+)\/([0-9a-zA-Z\.]+)\.(abstract|full|full\.pdf))(\+html)?$/.exec(pathname)) !== null) {
     /**
      *            http://advan.physiology.org.gate1.inist.fr/content/ajpadvan/38/3/229.full.pdf
      */
     result.unitid  = hostname + '/' + match[1];
-
-    switch (match[2]) {
+    testvalue = match[5].split('.')[0];
+    if (testvalue != "pdb") {
+    result.vol = match[3];
+    result.issue = match[4];
+    result.first_page = match[5].split('.')[0];
+    } else {
+      result.doi = "10.1101/" + match[5];
+    }
+    switch (match[6]) {
     case 'abstract':
       result.rtype = 'ABS';
       result.mime  = 'HTML';
@@ -67,11 +84,17 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime  = 'PDF';
     //result.title_id = match[4];
     result.unitid = hostname + '/' + match[2];
+    result.doi = match[2];
   } else if ((match = /^\/cgi\/reprint\/([a-zA-Z]+\;[0-9\/]+)$/.exec(pathname)) !== null) {
     // http://preventionportal.aacrjournals.org.gate1.inist.fr/cgi/reprint/canres;74/16/4378
     result.unitid = hostname + '/' + match[1].replace(";","/");
     result.rtype  = 'ARTICLE';
     result.mime   = 'PDF';
+    
+    info = match[1].split(';')[1];
+    result.vol = info.split('/')[0];
+    result.issue = info.split('/')[1];
+    result.first_page = info.split('/')[2];
   } else if ((match = /^\/content\/([0-9]+\/[0-9]+\.toc)$/.exec(pathname)) !== null) {
     // example : http://www.jimmunol.org.gate1.inist.fr/content/188/3.toc
     result.unitid = hostname + '/' + match[1];
@@ -80,11 +103,70 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   } else if ((match =  /^\/content\/([a-z]+)\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/([A-Z]+\.[0-9]{4}\.[0-9]+\.[0-9]+\.[0-9A-Z]+)\/([0-9a-zA-Z]+\_[A-Z]+\.[0-9]{4}\.[0-9]+\.[0-9]+\.pdf)$/.exec(pathname))!== null) {
     //http://jco.ascopubs.org.gate1.inist.fr/content/suppl/2015/03/23/JCO.2014.55.9898.DC1/Protocol1_JCO.2014.55.9898.pdf
     //http://jco.ascopubs.org.gate1.inist.fr/content/suppl/2015/03/09/JCO.2014.58.9747.DC2/Protocol_JCO.2014.58.9747.pdf
+
+    var completdoi = match[5].split('.');
     result.rtype  = 'ARTICLE';
     result.mime   = 'PDF';
     result.title_id = match[6];
     result.unitid = "content" +"/"+ match[1] +"/"+ match[2] +"/"+ match[3] +"/"+ match[4] +"/"+ match[5];
+    result.doi = '10.1200/' + completdoi[0] + '.'+ completdoi[1] + '.'+ completdoi[2] + '.'+ completdoi[3];
+  } else if ((match = /^\/docserver\/([a-z]+)\/([a-z]+)\/([0-9]{2})\/([0-9]+)\/(([0-9]+)_([a-z0-9]+)).pdf$/.exec(pathname)) !== null) {
+    // http://www.sgmjournals.org.gate1.inist.fr/docserver/fulltext/ijsem/65/8/2410_ijs000272.pdf //?expires=1442326648&id=id&accname=sgid024696&checksum=A5D313D11B167AB2B10D11EAC5EC7C13
+ 
+    result.rtype = 'ARTICLE';
+    result.mime  = 'PDF';
+    //result.title_id = match[4];
+    result.unitid = match[5];
+    result.title_id = match[2];
+    result.doi = '10.1099/' + match[7];
+  } else if ((match = /^\/content\/([a-z]+)\/([0-9]+)\/([0-9a-zA-Z\.]+)\.(abstract|full|full\.pdf)$/.exec(pathname)) !== null) {
+    // http://www.bmj.com.gate1.inist.fr/content/bmj/343/bmj.d4464.full.pdf
+  switch (match[4]) {
+    case 'abstract':
+      result.rtype = 'ABS';
+      result.mime  = 'HTML';
+      break;
+    case 'full':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'HTML';
+      break;
+    case 'full.pdf':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'PDF';
+      break;
+    }
+
+    //result.title_id = match[4];
+    result.unitid = match[3];
+    result.title_id = match[1];
+    result.doi = '10.1136/' + match[3];
+  } else if ((match = /^\/content\/([a-z]+)\/([a-z]+)\/([0-9]{4})\/([0-9]{2})\/([0-9]{2})\/([0-9a-zA-Z\-]+)\.(abstract|full|full\.pdf)$/.exec(pathname)) !== null) {
+    // http://www.bloodjournal.org.gate1.inist.fr/content/bloodjournal/early/2015/02/25/blood-2014-10-608596.full.pdf
+
+  switch (match[7]) {
+    case 'abstract':
+      result.rtype = 'ABS';
+      result.mime  = 'HTML';
+      break;
+    case 'full':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'HTML';
+      break;
+    case 'full.pdf':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'PDF';
+      break;
+    }
+
+    //result.title_id = match[4];
+    result.unitid = match[2] +'/' + match[3] + '/' + match[4] +'/' + match[5] + '/' + match[6];
+    result.title_id = match[1] ;
+    result.doi = '10.1136/' + match[6];
   }
+
+
+
+
   //([0-9a-zA-Z]+\_[A-Z]+\.[0-9]{4}\.[0-9]+\.[0-9]+\.[0-9A-Z]+\.pdf)
   // if the size is less than 10ko, it's not the actual article but a login page
   if (fileSize && fileSize < 10000) {
