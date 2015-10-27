74c26c4ac292920d6ac1547829dc3a32217593fd
aloukili <tech.abdel@gmail.com>
2015-10-27 15:32:55 +0100

    parser erudit is updated

--- erudit/parser.js
+++ erudit/parser.js
@@ -23,9 +23,16 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.mime = 'MISC';
     result.title_id = match[1];
     result.unitid= match[1]+ "/" + match[2] +"/" + match[3] +"/" +  match[4];
+    result.publication_date = match[2] ;
+    result.vol = match[3];
+    result.issue = match[4];
   } else if ((match = /\/revue\/([a-zA-Z]+)\/([0-9]{4})\/([^\\]+)\/([^\\]+)\/([a-zA-Z0-9]+).html$/.exec(pathname)) !== null) {
-    result.title_id = match[1];
+      result.title_id = match[1];
       result.unitid= match[1]+ "/" + match[2] +"/" + match[3] +"/" +  match[4]+"/" +  match[5];
+      result.publication_date = match[2] ;
+      result.vol = match[3];
+      result.issue = match[4];
+      result.doi = '10.7202/' + match[5];
     if (param.vue && param.vue === "resume") {
       // http://www.erudit.org.gate3.inist.fr/revue/ac/1974/v7/n1/017030ar.html?vue=resume
       result.rtype = 'ABS';
@@ -43,6 +50,10 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     // http://www.erudit.org.gate3.inist.fr/revue/ae/2009/v85/n4/045072ar.pdf
     result.title_id = match[1];
      result.unitid= match[1]+ "/" + match[2] +"/" + match[3] +"/" +  match[4]+"/" +  match[5];
+      result.publication_date = match[2] ;
+      result.vol = match[3];
+      result.issue = match[4];
+      result.doi = '10.7202/' + match[5];
     result.rtype = 'ARTICLE';
     result.mime = 'PDF';
   }
