259c0b2d38468bb17549e9c12f92a91f00d3d745
Thomas Porquet <thomas.porquet@couperin.org>
2015-11-02 09:34:02 +0100

    correct ti parser and test file

--- ti/parser.js
+++ ti/parser.js
@@ -29,18 +29,18 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
     //result.title_id = match[1];
-    result.unit_id   = match[1];
+    result.unitid   = match[1];
   } else if ((match = /^\/base\-documentaire\/[a-z\-]+(th[0-9]+)\/[a-z\-]+([0-9]+)\/[a-z\-]+\-([a-z0-9]+)\/$/.exec(path)) !== null) {
     // https://www.techniques-ingenieur.fr/base-documentaire/sciences-fondamentales-th8/mathematiques-fondamentales-analyse-42103210/topologie-et-mesure-af99/
     result.rtype    = 'PREVIEW';
     result.mime     = 'HTML';
     //console.error(match[1] + "-" + match[2] + "-" + match[3]);
     result.title_id = match[2];
-    result.unit_id   = match[1] + "-" + match[2] + "-" + match[3];
+    result.unitid   = match[1] + "-" + match[2] + "-" + match[3];
   } else if ((match = /^\/res\/pdf\/encyclopedia\/([0-9]+)\-([a-z0-9]+)\.pdf$/.exec(path)) !== null) {
     // https://www.techniques-ingenieur.fr/res/pdf/encyclopedia/42103210-af99.pdf
-    // unit_id 42103210-af99
-    result.unit_id = match[1] + "-" + match[2];
+    // unitid 42103210-af99
+    result.unitid = match[1] + "-" + match[2];
     result.title_id = match[1];
     // titleid 42103210
     result.rtype = 'ARTICLE';
@@ -49,13 +49,13 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // http://www.techniques-ingenieur.fr/confirm-download/?irId=10626-707219290ce4e891ec22f97a720b0a8c&type=WP&wpId=whitepaper85
     result.rtype = 'BOOK';
     result.mime  = 'PDF';
-    result.unit_id = param['wpId'];
+    result.unitid = param['wpId'];
     result.title_id = param['wpId']; 
   } else if ((match = /^\/lexique\/([a-z0-9\-]+)\.html$/.exec(path)) !== null) {
     // http://www.techniques-ingenieur.fr/lexique/materiel-24227.html
     result.rtype = 'ENCYCLOPAEDIA_ENTRY';
     result.mime  = 'HTML';
-    result.unit_id = 'lexique/' + match[1];
+    result.unitid = 'lexique/' + match[1];
     result.title_id  = match[1];
   } else if ((match = /^\/base\-documentaire\/archives\-th[0-9]+\/[a-z\-0-9]+\/archive\-[0-9]\/([a-z0-9\-]+)\/$/.exec(path)) !== null) {
     // http://www.techniques-ingenieur.fr/base-documentaire/archives-th12/archives-mathematiques-pour-l-ingenieur-tiafm/archive-1/classification-periodique-des-elements-25/
@@ -63,7 +63,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime     = 'HTML';
     //console.error(match[1] + "-" + match[2] + "-" + match[3]);
     result.title_id = match[1];
-    result.unit_id   = match[1];
+    result.unitid   = match[1];
   } 
 
   return result;
