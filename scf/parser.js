dd66e48b42c59cdb40be7340a5070d68006d20ea
aloukili <tech.abdel@gmail.com>
2015-12-16 14:55:10 +0100

    parser scf is updated

--- scf/parser.js
+++ scf/parser.js
@@ -33,7 +33,31 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.rtype = 'ARTICLE';
     result.mime  = 'PDF';
     result.unitid = 'pdfsecure';
-    //if (param.q) { result.unitid = param.q; }
+    
+  } else if ((match = /^\/([A-Za-z]+)$/.exec(path)) !== null) {
+    // http://www.lactualitechimique.org/Electrochimie
+    result.rtype = 'TOC';
+    result.mime  = 'MISC';
+    result.unitid = match[1];
+    
+  } else if ((match = /^\/numero\/([0-9]+)$/.exec(path)) !== null) {
+    // http://www.lactualitechimique.org:80/numero/402
+    result.rtype = 'TOC';
+    result.mime  = 'MISC';
+    result.unitid = 'numero/' + match[1] ;
+    
+  } else if ((match = /^\/([A-Za-z\-]+)$/.exec(path)) !== null) {
+    // http://www.lactualitechimique.org/Quand-la-Republique-avait-besoin-de-savants
+    result.rtype = 'ABS';
+    result.mime  = 'HTML';
+    result.unitid =  match[1] ;
+ 
+  } else if ((match = /^\/IMG\/([a-z]+)\/([0-9A-Za-z\-]+).pdf$/.exec(path)) !== null) {
+    // http://www.lactualitechimique.org:80/IMG/pdf/2015-402-decembre-p13-jacquesy-hd.pdf?6238/dc117cd12caeb2395e8b1589559155b91a8ecaf8
+    result.rtype = 'ARTICLE';
+    result.mime  = 'PDF';
+    result.unitid =  match[2] ;
+ 
   }
   return result;
 });
