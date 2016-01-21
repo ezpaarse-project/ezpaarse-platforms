9256864affd24509a42692de3c681e6b7a7afda9
aloukili <tech.abdel@gmail.com>
2016-01-21 18:35:21 +0100

    scaper and pkb for ams , es and ios

---
+++ es/scrapers/scrape_es-from-kb+.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+// Write on KBART file the Masterlist Collection journals PKB
+// Write on stderr the progression
+
+// 
+/*jslint maxlen: 180*/
+
+'use strict';
+var path = require('path');
+
+var PkbKbp     =  require(path.join(__dirname, '../../.lib/parsXmlKb+.js'));
+
+PkbKbp.generatePkbKbp(539 ,  'es');
