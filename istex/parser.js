22adad45e8007a6ae034d36bc2bdc63863f4dce3
aloukili <tech.abdel@gmail.com>
2016-02-05 15:02:01 +0100

    parser istex is updated

--- istex/parser.js
+++ istex/parser.js
@@ -16,38 +16,41 @@ var Parser = require('../.lib/parser.js');
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   var result = {};
   var path   = parsedUrl.pathname;
-
+  var param  = parsedUrl.query || {};
   var match;
+  if (param.q) {
+    result.rtype = 'QUERY';
+  } else {
+    if ((match = /^\/document\/([0-9A-Z]+)\/([a-z]+)\/([a-zA-Z]+)([^.]?)/.exec(path)) !== null) {
+      ///document/4C46BB8FC3AE3CB005C44243414E9D0E9C8C6057/enrichments/catWos
+      ///document/55420CDEEA0F6538E215A511C72E2E5E57570138/fulltext/original
+      ///document/55420CDEEA0F6538E215A511C72E2E5E57570138/metadata/xml
+     
 
-  if ((match = /^\/document\/([0-9A-Z]+)\/([a-z]+)\/([a-zA-Z]+)([^.]?)/.exec(path)) !== null) {
-    ///document/4C46BB8FC3AE3CB005C44243414E9D0E9C8C6057/enrichments/catWos
-    ///document/55420CDEEA0F6538E215A511C72E2E5E57570138/fulltext/original
-    ///document/55420CDEEA0F6538E215A511C72E2E5E57570138/metadata/xml
-
-    if (match[2] != 'fulltext') {
-      result.rtype = 'METADATA';
-    }
-
-    switch(match[3]) {
-    case 'txt':
-      result.mime = 'TEXT';
-      break;
-    case 'mrc':
-      result.mime = 'MARC';
-      break;
-    case 'catWos':
-      result.mime = 'TEI';
-      break;
-    case 'original':
-      if (match[2] != 'fulltext') {
-        result.mime = 'XML';
+      switch(match[3]) {
+      case 'txt':
+        result.mime = 'TEXT';
+        break;
+      case 'mrc':
+        result.mime = 'MARC';
+        break;
+      case 'catWos':
+        result.mime = 'TEI';
+        break;
+      case 'original':
+        result.mime = 'MISC';
+        if (match[2] != 'fulltext') {
+          result.mime = 'XML';
+        }
+        break;
+      default:
+       
+        result.mime = match[3].toUpperCase();
+        
       }
-      break;
-    default:
-      result.mime = match[3].toUpperCase();
-    }
 
-    result.unitid = match[1];
+      result.unitid = match[1];
+    }
   }
 
   return result;
