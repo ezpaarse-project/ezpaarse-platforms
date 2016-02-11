477afb4e348fda79af402c0e09e4a0cb4f375e43
Yannick Schurter <yannick.schurter@gmail.com>
2016-02-11 14:28:13 +0100

    fix TF parser

--- tf/parser.js
+++ tf/parser.js
@@ -16,62 +16,54 @@ var Parser = require('../.lib/parser.js');
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   var result = {};
   var path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  // var param  = parsedUrl.query || {};
-
-  // use console.error for debuging
-  // console.error(parsedUrl);
 
   var match;
 
   if ((match = /^\/doi\/(full|pdf|abs)\/(([0-9\.]+)\/([0-9]+))$/.exec(path)) !== null) {
-    //console.error(match);
-       result.rtype    = 'ARTICLE';
-      result.mime     = 'PDF';
-      
-      result.doi   = match[2];
-      result.unitid  = match[4];
-      if (match[1].toUpperCase() == "FULL") {
-        // http://www.tandfonline.com.bases-doc.univ-lorraine.fr/doi/full/10.1080/17400309.2013.861174#abstract
-        result.rtype = 'ARTICLE';
-        result.mime  = 'HTML';
-      } else if (match[1].toUpperCase() == "PDF") {
-        // http://www.tandfonline.com:80/doi/pdf/10.1080/17400309.2013.861174      
-        result.rtype = 'ARTICLE';
-        result.mime  = 'PDF';
-      } else if (match[1].toUpperCase() == "ABS") {
-        // http://www.tandfonline.com:80/doi/abs/10.1080/00039420412331273295
-        result.rtype = 'ABS';
-        result.mime  = 'HTML';
-      }
+    result.rtype  = 'ARTICLE';
+    result.mime   = 'PDF';
+    result.doi    = match[2];
+    result.unitid = match[4];
+
+    if (match[1].toUpperCase() == "FULL") {
+      // http://www.tandfonline.com.bases-doc.univ-lorraine.fr/doi/full/10.1080/17400309.2013.861174#abstract
+      result.rtype = 'ARTICLE';
+      result.mime  = 'HTML';
+    } else if (match[1].toUpperCase() == "PDF") {
+      // http://www.tandfonline.com:80/doi/pdf/10.1080/17400309.2013.861174
+      result.rtype = 'ARTICLE';
+      result.mime  = 'PDF';
+    } else if (match[1].toUpperCase() == "ABS") {
+      // http://www.tandfonline.com:80/doi/abs/10.1080/00039420412331273295
+      result.rtype = 'ABS';
+      result.mime  = 'HTML';
+    }
   } else if ((match = /^\/doi\/(full|pdf|abs)\/(([0-9\.]+)\/(([0-9]+)\.([0-9]+)\.([0-9]+)))$/.exec(path)) !== null) {
-    //console.error(match);
-       result.rtype    = 'ARTICLE';
-      result.mime     = 'PDF';
-      
-      result.title_id = match[5].substr(0,4) + '-' + match[5].substr(4,7) ;
-      result.issn = match[5].substr(0,4) + '-' + match[5].substr(4,7) ; 
-      result.doi   = match[2];
-      result.unitid  = match[4];
-      if (match[1].toUpperCase() == "FULL") {
-        // http://www.tandfonline.com.bases-doc.univ-lorraine.fr/doi/full/10.1080/17400309.2013.861174#abstract
-        result.rtype = 'ARTICLE';
-        result.mime  = 'HTML';
-      } else if (match[1].toUpperCase() == "PDF") {
-        // http://www.tandfonline.com:80/doi/pdf/10.1080/17400309.2013.861174      
-        result.rtype = 'ARTICLE';
-        result.mime  = 'PDF';
-      } else if (match[1].toUpperCase() == "ABS") {
-        // http://www.tandfonline.com:80/doi/abs/10.1080/00039420412331273295
-        result.rtype = 'ABS';
-        result.mime  = 'HTML';
-      }
-  }else if ((match = /^\/toc\/([a-zA-Z0-9]+)\/current$/.exec(path)) !== null) {
+    result.rtype            = 'ARTICLE';
+    result.mime             = 'PDF';
+    result.title_id         = match[5].substr(0,4) + '-' + match[5].substr(4,7) ;
+    result.print_identifier = match[5].substr(0,4) + '-' + match[5].substr(4,7) ;
+    result.doi              = match[2];
+    result.unitid           = match[4];
+
+    if (match[1].toUpperCase() == "FULL") {
+      // http://www.tandfonline.com.bases-doc.univ-lorraine.fr/doi/full/10.1080/17400309.2013.861174#abstract
+      result.rtype = 'ARTICLE';
+      result.mime  = 'HTML';
+    } else if (match[1].toUpperCase() == "PDF") {
+      // http://www.tandfonline.com:80/doi/pdf/10.1080/17400309.2013.861174
+      result.rtype = 'ARTICLE';
+      result.mime  = 'PDF';
+    } else if (match[1].toUpperCase() == "ABS") {
+      // http://www.tandfonline.com:80/doi/abs/10.1080/00039420412331273295
+      result.rtype = 'ABS';
+      result.mime  = 'HTML';
+    }
+  } else if ((match = /^\/toc\/([a-zA-Z0-9]+)\/current$/.exec(path)) !== null) {
     // http://www.tandfonline.com:80/toc/gaan20/current
     result.rtype    = 'TOC';
     result.mime     = 'HTML';
     result.title_id = match[1];
-    //see the comment block above
     result.unitid   = match[1];
   }
 
