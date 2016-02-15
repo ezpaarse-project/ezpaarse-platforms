2c9cbfc89b6c2174dba35def1f8e8d5b28fd6d99
Yannick Schurter <yannick.schurter@gmail.com>
2016-02-15 14:08:50 +0100

    add GSW scraping to HW

--- hw/scrapers/scrape_hw_journals_from_xls.js
+++ hw/scrapers/scrape_hw_journals_from_xls.js
@@ -8,6 +8,7 @@
 'use strict';
 
 const request = require('request');
+const cheerio = require('cheerio');
 const XLS     = require('xlsjs');
 const URL     = require('url');
 
@@ -16,18 +17,27 @@ const pkb     = new PkbRows('hw');
 
 pkb.setKbartName();
 
-let fileUrl = 'http://highwire.stanford.edu/librarians/AtoZList.xls';
+let atozUrl = 'http://highwire.stanford.edu/librarians/AtoZList.xls';
 
-console.error(`Downloading ${fileUrl}`);
+console.error(`Downloading ${atozUrl}`);
 
-request({ url: fileUrl, encoding: null }, (err, res, body) => {
+request({ url: atozUrl, encoding: null }, (err, res, body) => {
   if (err) { throw err; }
   if (res.statusCode != 200) {
     console.error(`Download failed (status = ${res.statusCode})`);
     process.exit(1);
   }
 
+  console.error('Parsing the Excel file');
   parseXLS(body);
+
+  console.error('Scraping GSW journals');
+  scrapeGSW(err => {
+    if (err) { throw err; }
+
+    pkb.writeKbart();
+    console.error(`Kbart file generated. Path : ${pkb.kbartFileName}`);
+  });
 });
 
 // parse XLS and create a PKB from it
@@ -55,14 +65,73 @@ function parseXLS(xlsContent) {
       if (lp.includes('main url')) {
         journalInfo.title_url = row[p].trim();
         journalInfo.title_id  = URL.parse(journalInfo.title_url).host;
-        journalInfo['pkb-piddomain'] = journalInfo.pid;
+        journalInfo['pkb-piddomain'] = journalInfo.title_id;
       }
     }
 
     pkb.addRow(journalInfo);
   });
+}
 
-  pkb.writeKbart();
+function scrapeGSW(callback) {
+  let gswJournals = 'http://www.geoscienceworld.org/site/misc/journals_glance.xhtml';
 
-  console.error(`Kbart file generated. Path : ${pkb.kbartFileName}`);
+  request(gswJournals, (err, res, body) => {
+    if (err) { return callback(err); }
+
+    let columns;
+    let $ = cheerio.load(body);
+
+    $('table tr').each((index, element) => {
+      let tds = $(element).children();
+
+      if (columns) {
+        let journal = pkb.initRow({});
+
+        tds.each((i, e) => {
+          if (columns[i]) { journal[columns[i]] = $(e).text(); }
+        });
+
+        if (journal.print_identifier) {
+          let match = /(([a-z0-9\-]+)\s*([a-z0-9\-]+))/i.exec(journal.print_identifier);
+          if (match) {
+            journal.print_identifier  = match[1];
+            journal.online_identifier = match[2];
+          }
+        }
+
+        if (!journal.title_url || !journal.print_identifier) { return; }
+
+        journal.title_id = URL.parse(journal.title_url).host;
+        journal['pkb-piddomain'] = journal.title_id;
+
+        return pkb.addRow(journal);
+      }
+
+      // if the first cell contains "journal title", it's the header row
+      if (tds.first().text().toLowerCase().includes('journal title')) {
+        columns = [];
+        tds.each((i, e) => {
+          switch($(e).text().toLowerCase()) {
+          case 'journal title':
+            columns.push('publication_title');
+            break;
+          case 'publisher':
+            columns.push('publisher_name');
+            break;
+          case 'print issn (online issn)':
+            columns.push('print_identifier');
+            break;
+          case 'url':
+            columns.push('title_url');
+            break;
+          default:
+            columns.push(null);
+          }
+        });
+      }
+    });
+
+    callback();
+  });
 }
