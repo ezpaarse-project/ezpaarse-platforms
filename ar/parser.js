30ca32e69c851366c9f2d0339f3a952cef3ae8e7
Yannick Schurter <yannick.schurter@gmail.com>
2016-02-16 11:10:00 +0100

    AR cleaning

--- ar/parser.js
+++ ar/parser.js
@@ -17,44 +17,44 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
 
   var match;
 
-  if ((match = /\/journal\/([a-z]+[0-9]?)$/.exec(path)) !== null) {
-    // http://www.annualreviews.org.gate1.inist.fr/journal/anchem
-    result.title_id = match[1];
-    result.rtype = 'TOC';
-    result.mime = 'MISC';
-  } else if ((match = /\/toc\/([a-z]+[0-9]?)\/current$/.exec(path)) !== null) {
+  if ((match = /^\/(journal|loi|toc)\/([a-z]+[0-9]?)(\/current)?$/.exec(path)) !== null) {
+    // /journal/achre4
     // /toc/achre4/current
-    result.unitid = match[1] + "/current";
-    result.title_id = match[1];
-    result.rtype = 'TOC';
-    result.mime = 'MISC';
+    result.title_id = match[2];
+    result.unitid   = match[2] + (match[3] || '');
+    result.rtype    = 'TOC';
+    result.mime     = 'MISC';
+
   } else if ((match = /\/toc\/([a-z]+[0-9]?)\/([0-9]+)\/([0-9]+)$/.exec(path)) !== null) {
     // /toc/achre4/46/4
-    result.unitid = match[1] + "/" + match[2] + '/' + match[3];
+    result.unitid   = match[1] + "/" + match[2] + '/' + match[3];
     result.title_id = match[1];
-    result.rtype = 'TOC';
-    result.mime = 'MISC';
-  } else if ((match = /^\/doi\/abs\/([0-9]{2}\.[0-9]{4})\/annurev\-([^\-]+)\-([^\-]+)\-([^\-]+)$/.exec(path)) !== null) {
+    result.rtype    = 'TOC';
+    result.mime     = 'MISC';
+    result.vol      = match[2];
+    result.issue    = match[3];
+
+  } else if ((match = /^\/doi\/(abs|pdf|full)\/([0-9]{2}\.[0-9]{4}\/(annurev\-([^\-]+)\-[^\-]+\-[^\-]+))$/.exec(path)) !== null) {
     // http://www.annualreviews.org.gate1.inist.fr/doi/abs/10.1146/annurev-neuro-062111-150343
-    result.doi = match[1] + "/annurev-" + match[2] + "-" + match[3] + "-" + match[4];
-    result.unitid = "annurev-" + match[2] + "-" + match[3] + "-" + match[4];
-    result.title_id = match[2];
-    result.rtype = 'ABS';
-    result.mime = 'HTML';
-  } else if ((match = /^\/doi\/pdf\/([0-9]{2}\.[0-9]{4})\/annurev\-([^\-]+)\-([^\-]+)\-([^\-]+)$/.exec(path)) !== null) {
-    // http://www.annualreviews.org.gate1.inist.fr/doi/pdf/10.1146/annurev-anchem-062012-092547
-    result.doi = match[1] + "/annurev-" + match[2] + "-" + match[3] + "-" + match[4];
-    result.unitid = "annurev-" + match[2] + "-" + match[3] + "-" + match[4];
-    result.title_id = match[2];
-    result.rtype = 'ARTICLE';
-    result.mime = 'PDF';
-  } else if ((match = /^\/doi\/full\/([0-9]{2}\.[0-9]{4})\/annurev\-([^\-]+)\-([^\-]+)\-([^\-]+)$/.exec(path)) !== null) {
-    // http://www.annualreviews.org.gate2.inist.fr/doi/full/10.1146/annurev-physchem-040412-110115
-    result.doi = match[1] + "/annurev-" + match[2] + "-" + match[3] + "-" + match[4];
-    result.unitid = "annurev-" + match[2] + "-" + match[3] + "-" + match[4];
-    result.title_id = match[2];
-    result.rtype = 'ARTICLE';
-    result.mime = 'HTML';
+    result.doi      = match[2];
+    result.unitid   = match[3];
+    result.title_id = match[4];
+
+    switch (match[1]) {
+    case 'abs':
+      result.rtype = 'ABS';
+      result.mime  = 'HTML';
+      break;
+    case 'pdf':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'PDF';
+      break;
+    case 'full':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'HTML';
+      break;
+    }
   }
+
   return result;
 });
