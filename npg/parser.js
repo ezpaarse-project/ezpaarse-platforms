d450a20480092ee21abdffd4f2cf17c92a91f60a
Yannick Schurter <yannick.schurter@gmail.com>
2016-03-09 11:50:15 +0100

    improve /news support

--- npg/parser.js
+++ npg/parser.js
@@ -13,7 +13,7 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
   let match;
 
   if ((match = /^\/([a-zA-Z0-9]+)\/journal\/v([a-z0-9]+)\/n([0-9]+|current)\/(pdf|full|abs|extref)\/([a-zA-Z0-9\.\-]+)\.[a-z]{2,4}$/.exec(path)) !== null) {
-    // http://www.nature.com.gate1.inist.fr/nrm/journal/vaop/ncurrent/full/nrm3940.html
+    // http://www.nature.com/nrm/journal/vaop/ncurrent/full/nrm3940.html
     result.title_id = match[1];
     result.vol      = match[2];
     result.issue    = match[3];
@@ -44,34 +44,33 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
       result.mime  = 'HTML';
       break;
     case 'FULL':
-      // http://www.nature.com.gate1.inist.fr/nature/journal/v493/n7431/full/493166a.html
-      // http://www.nature.com.gate1.inist.fr/nrm/journal/vaop/ncurrent/full/nrm3940.html
+      // http://www.nature.com/nature/journal/v493/n7431/full/493166a.html
+      // http://www.nature.com/nrm/journal/vaop/ncurrent/full/nrm3940.html
       result.rtype = 'ARTICLE';
       result.mime  = 'HTML';
       break;
     case 'PDF':
     case 'EXTREF':
-      // http://www.nature.com.gate1.inist.fr/nature/journal/v493/n7431/pdf/493166a.pdf
-      // http://www.nature.com.gate1.inist.fr/cdd/journal/vaop/ncurrent/pdf/cdd2014195a.pdf
-      // http://www.nature.com.gate1.inist.fr/nature/journal/v445/n7125/extref/nature05382-s1.pdf
+      // http://www.nature.com/nature/journal/v493/n7431/pdf/493166a.pdf
+      // http://www.nature.com/cdd/journal/vaop/ncurrent/pdf/cdd2014195a.pdf
+      // http://www.nature.com/nature/journal/v445/n7125/extref/nature05382-s1.pdf
       result.rtype = 'ARTICLE';
       result.mime  = 'PDF';
       break;
     }
 
-  } else if ((match = /\/([a-zA-Z0-9]+)\/(?:knowledgeenvironment\/)?([0-9]+)\/[0-9]+\/[a-zA-Z0-9]+\/(pdf|full)\/([a-zA-Z0-9]+)\.[a-z]{2,4}$/.exec(path)) !== null) {
-    // http://www.nature.com.gate1.inist.fr/bonekey/knowledgeenvironment/2012/120613/bonekey2012109/full/bonekey2012109.html
-    // http://www.nature.com.gate1.inist.fr/ncomms/2013/130628/ncomms3097/full/ncomms3097.html
-    // http://www.nature.com.gate1.inist.fr/bonekey/knowledgeenvironment/2012/120613/bonekey2012109/pdf/bonekey2012109.html
-    // http://www.nature.com.gate1.inist.fr/ncomms/2013/130829/ncomms3380/pdf/ncomms3380.pdf
+  } else if ((match = /\/([a-zA-Z0-9]+)\/(?:knowledgeenvironment\/)?([0-9]+)\/[0-9]+\/(?:[a-zA-Z0-9]+\/)?(pdf|full)\/([a-zA-Z0-9\-]+)\.[a-z]{2,4}$/.exec(path)) !== null) {
+    // http://www.nature.com/bonekey/knowledgeenvironment/2012/120613/bonekey2012109/full/bonekey2012109.html
+    // http://www.nature.com/ncomms/2013/130829/ncomms3380/pdf/ncomms3380.pdf
+    // http://www.nature.com/news/2007/070919/pdf/449268a.pdf
+    // http://www.nature.com/news/2005/050131/full/news050131-3.html
 
-    result.title_id = match[1];
+    result.title_id = (match[1] === 'news' ? 'nature' : match[1]);
     result.publication_date = match[2];
     result.unitid = result.doi = `${doiPrefix}/${match[4]}`;
 
-    if (match[1] !== 'nature' && match[4].startsWith(match[1])) {
-      let journal    = match[1];
-      let identifier = match[4].substr(journal.length);
+    if (result.title_id !== 'nature' && match[4].startsWith(result.title_id)) {
+      let identifier = match[4].substr(result.title_id.length);
 
       if (identifier.endsWith('a')) {
         identifier = identifier.substr(0, identifier.length - 1);
@@ -79,7 +78,7 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
 
       if (identifier.match(/^20[0-9]{2}/)) {
         result.publication_date = identifier.substr(0, 4);
-        result.doi = `${doiPrefix}/${journal}.${result.publication_date}.${identifier.substr(4)}`;
+        result.doi = `${doiPrefix}/${result.title_id}.${result.publication_date}.${identifier.substr(4)}`;
       }
     }
 
@@ -95,7 +94,7 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     }
 
   } else if ((match = /\/([a-zA-Z0-9]+)\/journal\/v([0-9]*)\/n([a-zA-Z0-9]*)\/index.html/.exec(path)) !== null) {
-    // http://www.nature.com.gate1.inist.fr/nature/journal/v493/n7431/index.html
+    // http://www.nature.com/nature/journal/v493/n7431/index.html
     result.title_id = match[1];
     result.vol      = match[2];
     result.issue    = match[3];
@@ -115,9 +114,9 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.rtype = 'READCUBE';
 
   } else if ((match = /^\/([a-zA-Z0-9]+)\/(?:archive\/)?(?:index|current_issue)\.html/.exec(path)) !== null) {
-    // http://www.nature.com.gate1.inist.fr/nature/index.html
+    // http://www.nature.com/nature/index.html
     // http://www.nature.com/clpt/archive/index.html?showyears=2013-2011-#y2011
-    // http://www.nature.com.gate1.inist.fr/nature/current_issue.html
+    // http://www.nature.com/nature/current_issue.html
     result.unitid   = match[1];
     result.title_id = match[1];
     result.rtype    = 'TOC';
@@ -139,7 +138,7 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.mime     = 'PDF';
 
   } else if (path === '/siteindex/index.html') {
-    // http://www.nature.com.gate1.inist.fr/siteindex/index.html
+    // http://www.nature.com/siteindex/index.html
     result.rtype = 'TOC';
     result.mime  = 'MISC';
   }
