e61c591062a9ca60b868a5ce55ef965b826d627f
aloukili <tech.abdel@gmail.com>
2016-03-10 17:24:18 +0100

    parser iopscience is updated

--- iopscience/parser.js
+++ iopscience/parser.js
@@ -13,37 +13,42 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
 
   var match;
 
-  if ((match = /\/([0-9]{4}\-[0-9]{3}([0-9Xx])?)(\/[0-9]+\/[0-9]+)$/.exec(path)) !== null) {
+  if ((match = /^\/([0-9]{4}\-[0-9]{3}([0-9Xx])?)(\/[0-9]+\/[0-9]+)$/.exec(path)) !== null) {
     // http://iopscience.iop.org/1758-5090/5/3
     result.rtype    = 'TOC';
     result.mime     = 'MISC';
     result.title_id = match[1];
     result.unitid   = match[1] + match[3];
-  } else if ((match = /\/([0-9]{4}\-[0-9]{3}([0-9Xx])?)(\/[0-9]+\/[0-9]+\/[0-9]+)$/.exec(path)) !== null) {
+  } else if ((match = /^\/([0-9]{4}\-[0-9]{3}([0-9Xx])?)(\/[0-9]+\/[0-9]+\/[0-9]+)$/.exec(path)) !== null) {
     // http://iopscience.iop.org/1758-5090/5/3/035002
     result.rtype    = 'ABS';
     result.mime     = 'MISC';
     result.title_id = match[1];
     result.unitid   = match[1] + match[3];
-  } else if ((match = /\/([0-9]{4}\-[0-9]{3}([0-9Xx])?)(\/[0-9]+\/[0-9]+\/[0-9]+\/article)$/.exec(path)) !== null) {
+  } else if ((match = /^\/([0-9]{4}\-[0-9]{3}([0-9Xx])?)(\/[0-9]+\/[0-9]+\/[0-9]+\/article)$/.exec(path)) !== null) {
     // http://iopscience.iop.org/1758-5090/5/3/035002/article
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
     result.title_id = match[1];
     result.unitid   = match[1] + match[3];
-  } else if ((match = /\/([0-9]{4}\-[0-9]{3}([0-9Xx])?)\/[0-9]+\/[0-9]+\/[0-9]+\/pdf\/([^.]+\.pdf)$/.exec(path)) !== null) {
+  } else if ((match = /^\/([0-9]{4}\-[0-9]{3}([0-9Xx])?)\/[0-9]+\/[0-9]+\/[0-9]+\/pdf\/([^.]+\.pdf)$/.exec(path)) !== null) {
     // http://iopscience.iop.org/1758-5090/5/3/035002/pdf/1758-5090_5_3_035002.pdf
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
     result.title_id = match[1];
     result.unitid   = match[3];
-  } else if ((match = /\/article\/([0-9]{2}\.[0-9]{4,5})\/(([0-9]{4}\-[0-9]{4})\/([0-9]+)\/([0-9]+)\/[A-Z0-9]+)\/([a-z]+)$/.exec(path)) !== null) {
+  } else if ((match = /^\/article\/([0-9]{2}\.[0-9]{4,5})\/(([0-9]{4}\-[0-9A-Z]*)\/([0-9]+)\/([0-9]+)\/([A-Z0-9]*))([\/]?)([a-z]*)/.exec(path)) !== null) {
     // article/10.1088/1748-0221/6/12/C12060/meta
+    //article/10.3847/0004-637X/819/2/158/pdf
     result.rtype = 'ABS';
     result.mime  = 'MISC';
-    if (match[6] === 'pdf') {
+    if (match[8] === 'pdf') {
       result.rtype = 'ARTICLE';
       result.mime  = 'PDF';
+    }
+    if (!match[7]) {
+      result.rtype = 'ARTICLE';
+      result.mime  = 'HTML';
     }
     result.print_identifier = match[3];
     result.vol    = match[4];
