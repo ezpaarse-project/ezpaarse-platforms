77f561dd116ba56f36a310ef8bb7f78f725b2670
Thomas Porquet <thomas.porquet@couperin.org>
2016-05-04 12:02:55 +0200

    oej scraper modified to add a pkb-piddomain field in pkb

--- oej/scrapers/scrape_oej.js
+++ oej/scrapers/scrape_oej.js
@@ -1,67 +1,61 @@
 'use strict';
 
-
-
 var CSV     = require('csv-string');
 var request = require('request');
 var PkbRows = require('../../.lib/pkbrows.js');
+var url     = require('url');
+
+var csvHeaders = {};
 
 
-  var titles = {};
+var opt = {
+  url: 'http://www.openedition.org/index.html',
+  qs: { page: 'coverage', pubtype: 'revue', export: 'tsv', kbart: '1' }
+};
 
+request.get(opt, function (err, res, body) {
+  if (err) {
+    console.error(err);
+    return;
+  }
 
-  var opt = {
-    url: 'http://www.openedition.org/index.html',
-    qs: { page: 'coverage', pubtype: 'revue', export: 'tsv', kbart: '1' }
-  };
-
-  request.get(opt, function (err, res, body) {
-    if (err) {
-      console.error(err);
-      return;
+  var csvSource = CSV.parse(body, CSV.detect(body));
+  if (csvSource !== undefined) {
+    csvHeaders = csvSource[0];
+    csvSource.shift();
+    var pkb = new PkbRows('oej');
+    pkb.setKbartName();
+    for (var i in csvSource) {
+      var kbartRow = pkb.initRow({});
+      kbartRow = associateElements(csvHeaders, csvSource[i]);
+      //add a pkb-piddomain to the kbart row, based on the url
+      kbartRow['pkb-piddomain'] = url.parse(kbartRow['title_url']).hostname;
+      pkb.addRow(kbartRow);
     }
-
-    var csvSource = CSV.parse(body, CSV.detect(body));
-    if (csvSource != undefined) {
-      
-      titles = csvSource[0];
-      csvSource.shift();
-      var pkb = new PkbRows('oej');
-      pkb.setKbartName();
-      for (var i in csvSource) {
-        var kbartRow = pkb.initRow({});
-        kbartRow = associetElement(titles, csvSource[i]);
-        pkb.addRow(kbartRow);
-      }
-      pkb.writeKbart(); 
-       
-    } else {
-      console.error('Aucune information retournée');
-      return;
-    }
+    pkb.writeKbart(); 
+  } else {
+    console.error('Aucune information retournée');
+    return;
+  }
+  console.log('file pkb is created');
+});
 
 
-  
-    console.log('file pkb is created');
-  });
-
-
-function associetElement(titles, value) {
-  if (!value && value != undefined) {
+function associateElements(titles, values) {
+  if (!values && values !== undefined) {
     return false;
   }
-  var elementproportie = {};
-  var url = '';
+  var elementProperties = {};
   var testcounter = 1 ;
-  if(value['title_id'] === '') {
-    return elementproportie;
+  if(values['title_id'] === '') {
+    return elementProperties;
   }
-  for (var k in value) {
+  for (var k in values) {
     testcounter ++;
-    if (value.length == testcounter) { break; }
-    if (value[k] !== null || value[k] !== '') {
-      elementproportie[titles[k]] = value[k];
+    if (values.length == testcounter) { break; }
+    if (values[k] !== null || values[k] !== '') {
+      elementProperties[titles[k]] = values[k];
     }
   }
-  return elementproportie;
+  return elementProperties;
 }
