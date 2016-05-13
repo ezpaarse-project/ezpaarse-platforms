408ef287232f7b4ddf648ed5506b266380628125
Thomas Porquet <thomas.porquet@couperin.org>
2016-05-13 12:27:01 +0200

    new url analysed

--- persee/parser.js
+++ persee/parser.js
@@ -40,10 +40,18 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.print_identifier = match[3];
   } else if ((match = /^\/doc\/(([a-z]+)_([0-9]{4}-[0-9]{3}[0-9xX]{1})_[0-9]{4}_.*)/.exec(path)) !== null) {
       // http://www.persee.fr/doc/xxs_0294-1759_1994_num_41_1_3263
+      //http://www.persee.fr/doc/abpo_0003-391x_1905_num_21_2_1218
     result.unitid = match[1];
     result.title_id = match[2];
     result.rtype = 'ARTICLE';
     result.mime = 'MISC';
+    result.print_identifier = match[3];
+  } else if ((match = /^\/docAsPDF\/(([a-z]+)_([0-9]{4}-[0-9]{3}[0-9xX]{1})_[0-9]{4}_.*)/.exec(path)) !== null) {
+    // /docAsPDF/abpo_0003-391x_1905_num_21_2_1218.pdf
+    result.unitid = match[1];
+    result.title_id = match[2];
+    result.rtype = 'ARTICLE';
+    result.mime = 'PDF';
     result.print_identifier = match[3];
   }
   return result;
