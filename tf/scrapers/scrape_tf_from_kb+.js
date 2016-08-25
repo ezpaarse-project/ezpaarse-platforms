c09eebaf3f07d5c637fc6634432cd220aa375d5d
Yannick Schurter <yannick.schurter@gmail.com>
2016-08-25 17:32:57 +0200

    update TF scraper

--- tf/scrapers/scrape_tf_from_kb+.js
+++ tf/scrapers/scrape_tf_from_kb+.js
@@ -2,12 +2,23 @@
 // Write on KBART file the Masterlist Collection journals PKB
 // Write on stderr the progression
 
-//
-/*jslint maxlen: 180*/
-
 'use strict';
-var path = require('path');
+const URL    = require('url');
+const pkbKbp = require('../../.lib/parsXmlKb+.js');
 
-var PkbKbp     =  require(path.join(__dirname, '../../.lib/parsXmlKb+.js'));
+pkbKbp.generatePkbKbp(957, 'tf', rowModifier);
 
-PkbKbp.generatePkbKbp(1502,  'tf');
+function rowModifier(row) {
+  if (!row.title_url) { return row; }
+
+  const url = URL.parse(row.title_url, true);
+
+  if (url.query && url.query.stitle) {
+    row.title_id = url.query.stitle;
+  } else {
+    let match = /^\/(?:loi|toc)\/([a-z0-9]+)/i.exec(url.pathname);
+    if (match) { row.title_id = match[1]; }
+  }
+
+  return row;
+}
