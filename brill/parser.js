e5a20ae243cda7256950f3e81b970b9854e90dc1
Yannick Schurter <yannick.schurter@gmail.com>
2016-08-29 15:53:46 +0200

    update brill parser

--- brill/parser.js
+++ brill/parser.js
@@ -2,7 +2,6 @@
 
 // ##EZPAARSE
 
-/*jslint maxlen: 150*/
 'use strict';
 var Parser = require('../.lib/parser.js');
 
@@ -21,28 +20,31 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   var title;
   var match;
   var matchinfo;
-  if ((match = /^\/content\/([a-z]+)\/([0-9\.]+)\/([0-9]+)$/.exec(path)) !== null) {
-    // content/journals/10.1163/157006605774832225
+  if ((match = /^\/content\/journals\/([0-9\.]+\/([0-9x\-]+))$/.exec(path)) !== null) {
+    // /content/journals/10.1163/157006605774832225
+    // /content/journals/10.1163/2405836x-00101001
     result.rtype  = 'ABS';
     result.mime   = 'HTML';
-    result.unitid = match[3];
-    result.doi    = match[2] + '/' +  match[3];
-  } else if ((match = /^\/docserver\/([0-9]+)\/(v([0-9]+)n([0-9]+)_([a-z0-9]+).pdf)$/.exec(path)) !== null) {
+    result.unitid = match[2];
+    result.doi    = match[1];
+  } else if ((match = /^\/docserver\/([0-9]+[xX]?)\/(v([0-9]+)n([0-9]+)_([a-z0-9]+).pdf)$/.exec(path)) !== null) {
     // docserver/00224200/v35n4_splitsection3.pdf
-    result.rtype            = 'ARTICLE';
-    result.mime             = 'PDF';
-    result.print_identifier = match[1].substr(0, 4) + '-' +  match[1].substr(4, 4);
-    result.vol              = match[3];
-    result.issue            = match[4];
-    result.unitid           = match[1] + '/' + match[2];
-  } else if ((match = /^\/content\/([a-z]+)\/(([0-9]+)\/([0-9]+)\/([0-9]+))$/.exec(path)) !== null) {
+    result.rtype             = 'ARTICLE';
+    result.mime              = 'PDF';
+    result.title_id          = match[1];
+    result.online_identifier = match[1].substr(0, 4) + '-' +  match[1].substr(4, 4);
+    result.vol               = match[3];
+    result.issue             = match[4];
+    result.unitid            = match[1] + '/' + match[2];
+  } else if ((match = /^\/content\/(?:journals\/)?(([0-9]+[xX]?)(?:\/([0-9]+)\/([0-9]+))?)$/.exec(path)) !== null) {
     // content/journals/15700666/35/4
-    result.rtype            = 'TOC';
-    result.mime             = 'HTML';
-    result.print_identifier = match[3].substr(0, 4) + '-' +  match[3].substr(4, 4);
-    result.vol              = match[4];
-    result.issue            = match[5];
-    result.unitid           = match[2];
+    result.rtype             = 'TOC';
+    result.mime              = 'HTML';
+    result.unitid            = match[1];
+    result.title_id          = match[2];
+    result.online_identifier = match[2].substr(0, 4) + '-' +  match[2].substr(4, 4);
+    result.vol               = match[3];
+    result.issue             = match[4];
 
   } else if ((match = /^\/media\/([a-z0-9]*)\/([^.]+).pdf$/.exec(path)) !== null) {
     //media/pplrdc/er372_411-412.pdf
@@ -59,52 +61,44 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
         title = matchinfo[1];
       }
     }
-    switch (true) {
-    case (/^([a-z0-9]+)_([0-9]+)-([0-9]+)/.test(match[2])):
-      result.print_identifier = param.id.split('.')[1];
+
+    if (/^([a-z0-9]+)_([0-9]+)-([0-9]+)/.test(match[2])) {
+      result.online_identifier = param.id.split('.')[1];
       if (match[1] != 'pplrdc') {
         result.online_identifier = /([a-z]+)([0-9]+)_([0-9]+)-([0-9]+)/.exec(match[2])[2];
         result.title_id = title;
       }
-      break;
-    case (/^([0-9]+)-([0-9]+)/.test(match[2])) :
+    } else if (/^([0-9]+)-([0-9]+)/.test(match[2])) {
       result.online_identifier = match[2].split('-')[0];
       result.title_id = title;
-      break;
-    case (/^([0-9]+)_([^.]+)/.test(match[2])) :
+    } else if (/^([0-9]+)_([^.]+)/.test(match[2])) {
       result.online_identifier = match[2].split('_')[0];
       result.title_id = title;
-      break;
-    default :
-      break;
     }
   } else if ((match = /^\/entries\/([a-z\-]+)\/([^.]+)$/.exec(path)) !== null) {
     //entries/the-hague-academy-collected-courses/*-ej.9789004289376.395_503
-    result.rtype            = 'TOC';
-    result.mime             = 'HTML';
+    result.rtype   = 'TOC';
+    result.mime    = 'HTML';
+
     if ((matchinfo = /([^.]+)\.([0-9]+)\.(([0-9]+)\_([0-9]+))/.exec(match[2])) !== null) {
-      result.print_identifier = matchinfo[2];
+      result.online_identifier = matchinfo[2];
       result.unitid = matchinfo[2] + '.' + matchinfo[3];
     } else {
-      result.rtype            = 'ENCYCLOPAEDIA_ENTRY';
-      result.mime             = 'HTML';
-      result.unitid           = match[1] + '/' + match[2];
+      result.rtype  = 'ENCYCLOPAEDIA_ENTRY';
+      result.mime   = 'HTML';
+      result.unitid = match[1] + '/' + match[2];
     }
 
-  } else if ((match = /^\/(deliver|docserver)\/([0-9]+)\/([0-9]+)\/([0-9]+)\/([0-9A-Za-z\_]+).(pdf|html)$/.exec(path)) !== null) {
+  } else if ((match = /^\/(deliver|docserver)\/([0-9]+[xX]?)\/([0-9]+)\/([0-9]+)\/([0-9A-Za-z\_]+).(pdf|html)$/.exec(path)) !== null) {
     //deliver/17087384/4/3/17087384_004_03_S02_text.pdf
-    result.rtype = 'ARTICLE';
-    if (match[6] === 'pdf') {
-      result.mime     = 'PDF';
-    } else {
-      result.mime     = 'HTML';
-    }
-    result.print_identifier = match[2].substr(0, 4) + '-' + match[2].substr(4, 4);
-    result.vol              = match[3];
-    result.issue            = match[4];
-    result.unitid           = match[5].replace('_text', '');
+    result.rtype             = 'ARTICLE';
+    result.mime              = match[6] === 'pdf' ? 'PDF' : 'HTML';
+    result.title_id          = match[2];
+    result.online_identifier = match[2].substr(0, 4) + '-' + match[2].substr(4, 4);
+    result.vol               = match[3];
+    result.issue             = match[4];
+    result.unitid            = match[5].replace('_text', '');
   }
 
   return result;
 });
-
