0ee2604c444d8b2c06def59b654a8ebb80cdc401
pseudom <dominique.lechaudel@gmail.com>
2016-09-16 15:05:48 +0200

    remove HW AtoZ xls list download which is no longer available

--- hw/scrapers/scrape_hw_journals.js
+++ hw/scrapers/scrape_hw_journals.js
@@ -9,7 +9,6 @@
 
 const request = require('request');
 const cheerio = require('cheerio');
-const XLS     = require('xlsjs');
 const URL     = require('url');
 
 const PkbRows = require('../../.lib/pkbrows.js');
@@ -17,66 +16,19 @@ const pkb     = new PkbRows('hw');
 
 pkb.setKbartName();
 
-let atozUrl = 'http://highwire.stanford.edu/librarians/AtoZList.xls';
 
-console.error(`Downloading ${atozUrl}`);
-
-request({ url: atozUrl, encoding: null }, (err, res, body) => {
+console.error('Scraping GSW journals');
+scrapeGSW(err => {
   if (err) { throw err; }
-  if (res.statusCode != 200) {
-    console.error(`Download failed (status = ${res.statusCode})`);
-    process.exit(1);
-  }
 
-  console.error('Parsing the Excel file');
-  parseXLS(body);
-
-  console.error('Scraping GSW journals');
-  scrapeGSW(err => {
+  console.error('Scraping Highwire AtoZ');
+  scrapeAtoZ(err => {
     if (err) { throw err; }
 
-    console.error('Scraping Highwire AtoZ');
-    scrapeAtoZ(err => {
-      if (err) { throw err; }
-
-      pkb.writeKbart();
-      console.error(`Kbart file generated. Path : ${pkb.kbartFileName}`);
-    });
+    pkb.writeKbart();
+    console.error(`Kbart file generated. Path : ${pkb.kbartFileName}`);
   });
 });
-
-// parse XLS and create a PKB from it
-function parseXLS(xlsContent) {
-  let xls   = XLS.read(xlsContent);
-  let sheet = xls.Sheets['Sheet1'];
-
-  if (!sheet) {
-    console.error('Sheet "Sheet1" not found');
-    process.exit(1);
-  }
-
-  let rows  = XLS.utils.sheet_to_row_object_array(sheet);
-
-  rows.forEach(row => {
-    let journalInfo = pkb.initRow({});
-
-    for (let p in row) {
-      let lp = p.toLowerCase();
-
-      if (lp.includes('journal title')) { journalInfo.publication_title = row[p].trim(); }
-      if (lp.includes('print issn'))    { journalInfo.print_identifier  = row[p].trim(); }
-      if (lp.includes('online issn'))   { journalInfo.online_identifier = row[p].trim(); }
-      if (lp.includes('publisher'))     { journalInfo.publisher_name    = row[p].trim(); }
-      if (lp.includes('main url')) {
-        journalInfo.title_url = row[p].trim();
-        journalInfo.title_id  = URL.parse(journalInfo.title_url).host;
-        journalInfo['pkb-piddomain'] = journalInfo.title_id;
-      }
-    }
-
-    pkb.addRow(journalInfo);
-  });
-}
 
 function scrapeGSW(callback) {
   let gswJournals = 'http://www.geoscienceworld.org/site/misc/journals_glance.xhtml';
