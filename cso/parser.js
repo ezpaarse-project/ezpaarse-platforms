9c680d669d2a1e7bf468446c3d2e81ccbcf11363
Yannick Schurter <yannick.schurter@gmail.com>
2016-10-05 16:10:55 +0200

    CSO cosmetic changes

--- cso/parser.js
+++ cso/parser.js
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-// ##EZPAARSE
-
-/*jslint maxlen: 150*/
 'use strict';
 var Parser = require('../.lib/parser.js');
 
@@ -21,42 +18,43 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   // use console.error for debuging
   // console.error(parsedUrl);
-  var match;
-  if ((match = /^\/[a-z]+\/([0-9]+\.[0-9]+\/[a-z]+\/([0-9]+))\.[0-9]+\.[0-9]+\/([a-z]+\-([0-9]+))$/.exec(path)) !== null) {
-    //view/10.7208/chicago/9780226243276.001.0001/upso-9780226243238
-    result.rtype    = 'BOOK';
-    result.mime     = 'HTML';
-    result.doi = match[1];
-    result.unitid   = match[3];
-    result.print_identifier = match[4];
-    result.online_identifier = match[2];
-  }
-  else if ((match = /^\/[a-z]+\/([0-9]+\.[0-9]+\/[a-z]+\/([0-9]+))\.[0-9]+\.[0-9]+\/([a-z]+\-([0-9]+)\-[a-z]+\-[0-9]+)$/.exec(path)) !==null) {
-  //http://chicago.universitypressscholarship.com/view/10.7208/chicago/9780226243276.001.0001/upso-9780226243238-chapter-1
-  //http://chicago.universitypressscholarship.com/view/10.7208/chicago/9780226243276.001.0001/upso-9780226243238-chapter-1?print=pdf
-    result.rtype    = 'BOOK_SECTION';
-    result.mime     = 'HTML';
-    if (param.print && param.print == 'pdf')
-    {
-      result.rtype   = 'BOOK';
-      result.mime    = 'PDF';
-    }
-    result.doi = match[1];
-    result.unitid   = match[3];
-    result.print_identifier = match[4];
-    result.online_identifier = match[2];
-  }
 
-  else if ((match = /^\/[a-z]+$/.exec(path)) !==null)
-  {
-    //browse?t=OSO:history
-    result.rtype    = 'TOC';
-    result.mime     = 'HTML';
-    if (param.t)
-    {
+  var match;
+
+  if ((match = /^\/[a-z]+\/([0-9]+\.[0-9]+\/[a-z]+\/([0-9]+))\.[0-9]+\.[0-9]+\/([a-z]+\-([0-9]+))$/i.exec(path)) !== null) {
+    // /view/10.7208/chicago/9780226243276.001.0001/upso-9780226243238
+    result.rtype             = 'BOOK';
+    result.mime              = 'HTML';
+    result.doi               = match[1];
+    result.unitid            = match[3];
+    result.print_identifier  = match[4];
+    result.online_identifier = match[2];
+
+  } else if ((match = /^\/[a-z]+\/([0-9]+\.[0-9]+\/[a-z]+\/([0-9]+))\.[0-9]+\.[0-9]+\/([a-z]+\-([0-9]+)\-[a-z]+\-[0-9]+)$/i.exec(path)) !== null) {
+    // http://chicago.universitypressscholarship.com/view/10.7208/chicago/9780226243276.001.0001/upso-9780226243238-chapter-1
+    // http://chicago.universitypressscholarship.com/view/10.7208/chicago/9780226243276.001.0001/upso-9780226243238-chapter-1?print=pdf
+    result.rtype = 'BOOK_SECTION';
+    result.mime  = 'HTML';
+
+    if (param.print && param.print == 'pdf') {
+      result.rtype = 'BOOK';
+      result.mime  = 'PDF';
+    }
+
+    result.doi               = match[1];
+    result.unitid            = match[3];
+    result.print_identifier  = match[4];
+    result.online_identifier = match[2];
+
+  } else if ((match = /^\/[a-z]+$/i.exec(path)) !== null) {
+    // /browse?t=OSO:history
+    result.rtype = 'TOC';
+    result.mime  = 'HTML';
+
+    if (param.t) {
       result.unitid = param.t;
     }
   }
 
-  return result ;
+  return result;
 });
