00a887252f152b7322aab370dad3f661a9d96ec0
aloukili <abdel@dctchad.com>
2016-10-05 15:20:07 +0200

    refactoring parser britannica after comment yannick

--- britannica/parser.js
+++ britannica/parser.js
@@ -26,43 +26,43 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   if ((match = /^\/event\/([a-z\-]+)$/i.exec(path)) !== null) {
     ///event/Harlem-Renaissance-American-literature-and-art
-    result.rtype    = 'ENCYCLOPAEDIA_ENTRY';
-    result.mime     = 'HTML';
-    result.unitid   = match[1];
+    result.rtype  = 'ENCYCLOPAEDIA_ENTRY';
+    result.mime   = 'HTML';
+    result.unitid = match[1];
   } else if ((match = /^\/quiz\/([a-z\-]+)$/i.exec(path)) !== null) {
     ///quiz/art-of-the-harlem-renaissance
-    result.rtype    = 'EXO';
-    result.mime     = 'HTML';
-    result.unitid   = match[1];
+    result.rtype  = 'EXO';
+    result.mime   = 'HTML';
+    result.unitid = match[1];
   } else if ((match = /^\/biography\/([a-z\-]+)$/i.exec(path)) !== null) {
     ///biography/Billy-Wilder
-    result.rtype    = 'BIO';
-    result.mime     = 'HTML';
-    result.unitid   = match[1];
+    result.rtype  = 'BIO';
+    result.mime   = 'HTML';
+    result.unitid = match[1];
   } else if ((match = /^\/list\/([a-z]+)$/i.exec(path)) !== null) {
     ///https://www.britannica.com/list/browse
-    result.rtype    = 'TOC';
-    result.mime     = 'HTML';
-  } else if ((match = /^\/levels\/([a-z]+)\/([a-z]+)\/(([0-9]+)?([a-z]+)?)(\/([a-z]+))?(\/([0-9]+))?$/.exec(path)) !== null) {
+    result.rtype = 'TOC';
+    result.mime  = 'HTML';
+  } else if ((match = /^\/levels\/([a-z]+)\/([a-z]+)\/([0-9]+|[a-z]+)(\/([a-z]+))?(\/([0-9]+))?$/.exec(path)) !== null) {
     ///levels/collegiate/article/470856
     //levels/collegiate/additionalcontent/primarysources?id=127472&path=/primary_source/gutenberg/PGCC_classics_02/16978-8.htm
     //levels/collegiate/browse/atlas
-    result.rtype    = 'ENCYCLOPAEDIA_ENTRY';
-    result.mime     = 'HTML';
+    result.rtype = 'ENCYCLOPAEDIA_ENTRY';
+    result.mime  = 'HTML';
 
-    if (match[7] && match[7] == 'media') {
+    if (match[5] && match[5] == 'media') {
       result.rtype = 'IMAGE';
       result.mime  = 'MISC';
     }
-    result.unitid   = match[4];
+    result.unitid = match[3];
     if (param.id) {
       result.unitid = param.id;
       result.rtype  = 'BOOK';
     } else if (match[2] == 'browse') {
-      result.unitid = match[5];
+      result.unitid = match[3];
       result.rtype  = 'MAP';
-    } else if (match[5] == 'article') {
-      result.unitid = match[9];
+    } else if (match[3] == 'article') {
+      result.unitid = match[7];
       result.rtype  = 'ARTICLE';
     }
 
