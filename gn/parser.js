f2117b5889dd1f19932a1d8d0df29ca35f32a6ae
Yannick Schurter <yannick.schurter@gmail.com>
2017-01-05 11:43:44 +0100

    cosmetic

--- gn/parser.js
+++ gn/parser.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
-// ##EZPAARSE
-
-/*jslint maxlen: 150*/
 'use strict';
-var Parser = require('../.lib/parser.js');
+const Parser = require('../.lib/parser.js');
 
 /**
  * Recognizes the accesses to the platform Garnier Numérique
@@ -14,42 +11,40 @@ var Parser = require('../.lib/parser.js');
  * @return {Object} the result
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
-  var result = {};
-  var path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  var param  = parsedUrl.query || {};
+  let result = {};
+  let path   = parsedUrl.pathname;
+  let param  = parsedUrl.query || {};
 
-  // use console.error for debuging
-  // console.error(parsedUrl);
-
-  var match;
-
+  let match;
 
   if (param && param.link) {
-    var cleanlink = param.link.replace(/\s+/g, '');
-    var type = cleanlink.split('_');
+    let cleanLink = param.link.replace(/\s+/g, '');
+    let type      = cleanLink.split('_');
+
     switch (type[1]) {
     case 'article':
-    //numerique-bases/index.php?module=App&action=PanelText&link=jmp_article_17%09-A-%0A
-      result.rtype    = 'ARTICLE';
-      result.mime     = 'HTML';
-      result.unitid   = cleanlink;
+      // numerique-bases/index.php?module=App&action=PanelText&link=jmp_article_17%09-A-%0A
+      result.rtype  = 'ARTICLE';
+      result.mime   = 'HTML';
+      result.unitid = cleanLink;
       break;
     case 'tabmat':
-    //numerique-bases/index.php?module=App&action=PanelTabmat&link=jmp_tabmat__tmxa_007
-      result.rtype    = 'TOC';
-      result.mime     = 'HTML';
-      result.unitid   = cleanlink;
+      // numerique-bases/index.php?module=App&action=PanelTabmat&link=jmp_tabmat__tmxa_007
+      result.rtype  = 'TOC';
+      result.mime   = 'HTML';
+      result.unitid = cleanLink;
       break;
     }
-  } else if ((match = /^\/doi\/(([a-z]+)\-([a-z]+))?$/.exec(path)) !== null) {
-    ///doi/article-pdf?article=HpeMS02_9
-    if (match[3] === 'pdf') {
-      result.mime     = 'PDF';
+  } else if ((match = /^\/doi\/[a-z]+\-([a-z]+)?$/.exec(path)) !== null) {
+    // /doi/article-pdf?article=HpeMS02_9
+
+    if (match[1] === 'pdf') {
+      result.mime = 'PDF';
     }
+
     if (param && param.article) {
-      result.rtype    = 'ARTICLE';
-      result.unitid   = param.article;
+      result.rtype  = 'ARTICLE';
+      result.unitid = param.article;
     }
 
   }
