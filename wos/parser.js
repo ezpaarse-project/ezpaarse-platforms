e68527aa154c435dbada9e6cd5357e935edc214c
aloukili <abdel@dctchad.com>
2017-01-16 11:57:07 +0100

    update parser wos

--- wos/parser.js
+++ wos/parser.js
@@ -32,6 +32,12 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
       result.rtype    = 'TOC';
       break;
     case 'UA_GeneralSearch_input' :
+    case 'RSCI_GeneralSearch_input' :
+    case 'WOS_GeneralSearch_input' :
+    case 'CCC_GeneralSearch_input' :
+    case 'KJD_GeneralSearch_input' :
+    case 'MEDLINE_GeneralSearch_input' :
+    case 'SCIELO_GeneralSearch_input' :
       result.rtype    = 'SEARCH';
       break;
     case 'full_record' :
@@ -49,9 +55,9 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     }
 
   } else if ((match = /^\/([a-zA-z\_]*)\.action$/i.exec(path)) !== null) {
-    //JCRJournalHomeAction.action?SID=A2-PrkrJv74tBj5ko9eDVx2FpCXtAd4MjBJRW-18x2dJxx17XG5YmNwa4tn0JGiq6Ax3Dx3DlUL1U2XQfq5HIHtc4qxxE1wx3Dx3D-9vvmzcndpRgQCGPd1c2qPQx3Dx3D-wx2BJQh9GKVmtdJw3700KssQx3Dx3D&refineString=null&SrcApp=IC2LS&timeSpan=null&Init=Yes&wsid=Z2E5UA9l6Q6BRAvS44U
-    //https://jcr-incites-thomsonreuters-com.inee.bib.cnrs.fr/JCRJournalProfileAction.action?pg=JRNLPROF&journalImpactFactor=137.578&year=2015&journalTitle=CA-A%20CANCER%20JOURNAL%20FOR%20CLINICIANS&edition=SCIE&journal=CA-CANCER%20J%20CLIN
-    //https://esi-incites-thomsonreuters-com.inee.bib.cnrs.fr/IndicatorsAction.action?SID=B2-jx2FDXmiF0SKhxx0ig0x2BAhKZYxxBanb6AxxZ9-18x2ddx2B4ruM3jsN3soix2BefrHXeAx3Dx3DUVx2FlduvZ8VjUUtFgmiBlyQx3Dx3D-iyiHxxh55B2RtQWBj2LEuawx3Dx3D-1iOubBm4x2FSwJjjKtx2F7lAaQx3Dx3D&refineString=null&SrcApp=IC2LS&timeSpan=null&Init=Yes&wsid=Z2E5UA9l6Q6BRAvS44U
+    //JCRJournalHomeAction.action?
+    //https://jcr-incites-thomsonreuters-com.inee.bib.cnrs.fr/JCRJournalProfileAction.action?
+    //https://esi-incites-thomsonreuters-com.inee.bib.cnrs.fr/IndicatorsAction.action?
     //https://esi-incites-thomsonreuters-com.inee.bib.cnrs.fr/DocumentsAction.action
 
 
