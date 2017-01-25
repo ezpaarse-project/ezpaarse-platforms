0bf825374b51fed5a57a5ec569f17cba061f180f
Yannick Schurter <yannick.schurter@gmail.com>
2017-01-25 14:49:39 +0100

    clean up silverchair

--- silverchair/parser.js
+++ silverchair/parser.js
@@ -13,11 +13,7 @@ const Parser = require('../.lib/parser.js');
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
   let param  = parsedUrl.query || {};
-
-  // use console.error for debuging
-  // console.error(parsedUrl);
 
   let match;
 
@@ -27,7 +23,6 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime     = 'PDF';
     result.title_id = match[1];
     result.unitid   = match[2];
-
 
   } else if (/^\/article\.aspx$/i.test(path)) {
     // http://turbomachinery.asmedigitalcollection.asme.org/article.aspx?articleID=2478820
@@ -41,70 +36,64 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   } else if (/^\/issue\.aspx$/i.test(path)) {
     // http://turbomachinery.asmedigitalcollection.asme.org/issue.aspx?journalid=135&issueid=935001&direction=P
     // http://appliedmechanicsreviews.asmedigitalcollection.asme.org/issue.aspx?journalid=113&issueid=935281&direction=P
+    result.rtype    = 'TOC';
+    result.mime     = 'MISC';
+    result.title_id = param.journalid || parsedUrl.hostname.split('.')[0];
 
-    //http://appliedmechanicsreviews.asmedigitalcollection.asme.org/issue.aspx?journalid=113&issueid=935281&direction=P
-    result.rtype = 'TOC';
-    result.mime  = 'MISC';
-
-
-    if (param.journalid) {
-      result.title_id = param.journalid;
-    } else {
-      let hostname = parsedUrl.hostname;
-      result.title_id = hostname.split('.')[0];
-    }
     if (param.issueid) {
       result.unitid = param.issueid;
     }
-  }  else if ((match = /^\/journals\/([a-z]+)\/[a-z]+\/([0-9]+)$/i.exec(path)) !== null) {
+  } else if ((match = /^\/journals\/([a-z]+)\/(fullarticle|article-abstract)\/([0-9]+)$/i.exec(path)) !== null) {
     // http://jamanetwork.com/journals/jama/fullarticle/2481003
-
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
+    // http://jamanetwork.com/journals/jama/article-abstract/2480993
     result.title_id = match[1];
-    result.unitid   = match[2];
+    result.unitid   = match[3];
+
+    switch (match[2]) {
+    case 'fullarticle':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'HTML';
+      break;
+    case 'article-abstract':
+      result.rtype = 'ABS';
+      result.mime  = 'HTML';
+      break;
+    }
 
   } else if ((match = /^\/journals\/([a-z]+)\/[a-z]+\/Journals\/[a-zA-Z]*\/[0-9]+\/([a-zA-Z0-9]*)\.pdf$/i.exec(path)) !== null) {
     // http://jamanetwork.com//journals/jama/data/Journals/JAMA/934847/joi150160.pdf
-
-    result.rtype = 'ARTICLE';
-    result.mime  = 'PDF';
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
     result.title_id = match[1];
     result.unitid   = match[2];
 
-
   } else if ((match = /^(\/journals)?\/([a-z]+)\/issue\/([0-9]+)\/([0-9]+)$/i.exec(path)) !== null) {
     // http://jamanetwork.com/journals/jama/issue/315/2
-    ///toxsci/issue/29/1
-    result.rtype = 'TOC';
-    result.mime  = 'MISC';
+    // /toxsci/issue/29/1
+    result.rtype    = 'TOC';
+    result.mime     = 'MISC';
     result.title_id = match[2];
-    result.vol   = match[3];
+    result.vol      = match[3];
 
-  } else if ((match = /^\/oup\/[a-z]+\/[a-zA-Z\_]+\/[a-zA-Z]+\/([a-z]+)\/([0-9]+)\/[0-9]+\/([0-9]{2}\.[0-9]{4}\/([a-z]+\/[0-9\.]+))\/[0-9]+\/[0-9\-]*.pdf$/i.exec(path)) !== null) {
+  } else if ((match = /^\/[a-z]+\/backfile\/Content_public\/Journal\/([a-z]+)\/([0-9]+)\/[0-9]+\/([0-9]{2}\.[0-9]{4}\/([a-z]+\/[0-9\.]+))\/[0-9]+\/[0-9\-]*.pdf$/i.exec(path)) !== null) {
     // /oup/backfile/Content_public/Journal/toxsci/29/1/10.1093/toxsci/29.1.18/2/29-1-18.pdf
-
-    result.rtype = 'ARTICLE';
-    result.mime  = 'PDF';
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
     result.title_id = match[1];
-    result.vol   = match[2];
-    result.doi   = match[3];
-    result.unitid = match[4];
+    result.vol      = match[2];
+    result.doi      = match[3];
+    result.unitid   = match[4];
 
   } else if ((match = /^\/([a-z]+)\/(article|article-abstract)\/([0-9]+)\/[0-9]+\/([0-9]+)\/([0-9]+)\/[a-zA-Z\-]+$/i.exec(path)) !== null) {
     // /humrep/article/32/1/14/2645576/Is-the-understanding-of-AMH-being-confounded-by
-    result.rtype = 'ARTICLE';
-    result.mime  = 'HTML';
-    if (match[2] === 'article-abstract') {
-      result.rtype = 'ABS';
-    }
-
-    result.title_id = match[1];
-    result.vol   = match[3];
-    result.first_page   = match[4];
-    result.unitid = match[5];
-
+    result.mime       = 'HTML';
+    result.rtype      = match[2] === 'article' ? 'ARTICLE' : 'ABS';
+    result.title_id   = match[1];
+    result.vol        = match[3];
+    result.first_page = match[4];
+    result.unitid     = match[5];
 
   }
+
   return result;
 });
