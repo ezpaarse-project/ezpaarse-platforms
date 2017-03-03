bd997038e2b4f62ee1acf66b26dda993a0cdd6ca
aloukili <abdel@dctchad.com>
2017-03-03 15:44:18 +0100

    update parser iop science

--- iopscience/parser.js
+++ iopscience/parser.js
@@ -10,9 +10,9 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
   var result = {};
   //var param  = parsedUrl.query || {};
   var path   = parsedUrl.pathname;
-
+  var hash   = parsedUrl.hash || {};
   var match;
-
+console.log(parsedUrl);
   if ((match = /^\/([0-9]{4}\-[0-9]{3}([0-9Xx])?)(\/[0-9]+\/[0-9]+)$/.exec(path)) !== null) {
     // http://iopscience.iop.org/1758-5090/5/3
     result.rtype    = 'TOC';
@@ -40,21 +40,28 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
   } else if ((match = /^\/article\/([0-9]{2}\.[0-9]{4,5})\/(([0-9]{4}\-[0-9A-Z]*)\/([0-9]+)\/([0-9]+)\/([A-Z0-9]*))([\/]?)([a-z]*)/.exec(path)) !== null) {
     // article/10.1088/1748-0221/6/12/C12060/meta
     //article/10.3847/0004-637X/819/2/158/pdf
-    result.rtype = 'ABS';
-    result.mime  = 'MISC';
+    result.rtype = 'ARTICLE';
+    result.mime  = 'HTML';
     if (match[8] === 'pdf') {
       result.rtype = 'ARTICLE';
       result.mime  = 'PDF';
     }
-    if (!match[7]) {
-      result.rtype = 'ARTICLE';
-      result.mime  = 'HTML';
+    if (hash && hash === '#artAbst') {
+      result.rtype = 'ABS';
     }
     result.print_identifier = match[3];
     result.vol    = match[4];
     result.issue  = match[5];
     result.doi    = match[1] + '/' + match[2];
     result.unitid = match[2];
+  } else if ((match = /^\/issue\/([0-9]{4}\-[0-9A-Z]*)\/([0-9]+)\/([0-9]+)$/.exec(path)) !== null) {
+    ///issue/0004-637X/831/2
+    result.rtype  = 'TOC';
+    result.mime   = 'MISC';
+    result.unitid = match[2] + '/' + match[3];
+    result.print_identifier = match[1];
+    result.vol    = match[2];
+    result.issue  = match[3];
   }
 
   return result;
