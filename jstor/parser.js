5b2bce6d5ac28368d246781c2947b61a8924eb66
Yannick Schurter <yannick.schurter@gmail.com>
2017-03-09 15:22:42 +0100

    update JSTOR

--- jstor/parser.js
+++ jstor/parser.js
@@ -1,14 +1,11 @@
 #!/usr/bin/env node
 
-// ##EZPAARSE
-
-/*jslint maxlen: 180*/
-
 /**
- * parser for acs platform
- * http://analogist.couperin.org/platforms/acs/
+ * parser for the JSTOR platform
+ * http://analogist.couperin.org/platforms/jstor/
  */
 'use strict';
+
 var Parser = require('../.lib/parser.js');
 
 module.exports = new Parser(function analyseEC(parsedUrl) {
@@ -24,32 +21,36 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.unitid   = match[1];
     result.title_id = match[1];
 
-  } else if ((match = /^\/stable\/([0-9]{2}\.[0-9]{4})\/([0-9]+)$/i.exec(path)) !== null) {
-    // /stable/10.1086/665036
-    result.title_id = match[2];
-    result.rtype    = 'TOC';
-    result.mime     = 'MISC';
-    result.unitid   = match[2];
-    result.doi      = match[1] +'/'+ match[2];
-
-  } else if ((match = /^\/stable\/([0-9]{2}\.[0-9]{4})\/([a-z]+)\.([0-9]{4})\.([0-9]+)\.([^.]+)$/i.exec(path)) !== null) {
+  } else if ((match = /^\/stable\/[0-9]{2}\.[0-9]{4}\/(([a-z]+)\.([0-9]+)\.([0-9]+)\.issue-([0-9]+))$/i.exec(path)) !== null) {
     // /stable/10.1525/cmr.2013.55.issue-2
+    // /stable/10.5325/jmedirelicult.39.2.issue-2
+    result.unitid   = match[1];
     result.title_id = match[2];
-    result.unitid   = match[2] + '.' + match[3] + '.' + match[4] + '.' + match[5];
-    result.doi      = match[1] +'/'+ match[2] + '.' + match[3] + '.' + match[4] + '.' + match[5];
+    result.issue    = match[5];
     result.rtype    = 'TOC';
     result.mime     = 'MISC';
 
-  } else if ((match = /^\/stable\/([0-9]{2}\.[0-9]{4})\/([^.]+)$/i.exec(path)) !== null) {
+    if (match[3].length >= 4) {
+      result.publication_date = match[3];
+      result.vol = match[4];
+    } else {
+      result.vol = match[3];
+    }
+
+  } else if ((match = /^\/stable\/(([0-9]{2}\.[0-9]{4}\/)?([a-z0-9]+))$/i.exec(path)) !== null) {
+    // /stable/10.1086/665036
     // /stable/10.7312/cari13424
-    result.title_id = match[2];
-    result.unitid   = match[2];
-    result.doi      = match[1] +'/'+ match[2];
+    // /stable/i25703249
+    result.title_id = match[3];
+    result.unitid   = match[3];
     result.rtype    = 'TOC';
     result.mime     = 'MISC';
+
+    if (match[2]) {
+      result.doi = match[1];
+    }
 
   } else if ((match = /^\/stable\/(i[0-9]+)$/i.exec(path)) !== null) {
-    // /stable/i25703249
     result.title_id = match[1];
     result.unitid   = match[1] ;
     result.rtype    = 'TOC';
@@ -63,20 +64,55 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
       result.rtype    = 'TOC';
       result.mime     = 'MISC';
     }
-  } else if ((match = /^\/stable\/([0-9]{2}\.[0-9]+\/(([a-z]+)\.[0-9]+\.[0-9]+\.[^.]+))$/i.exec(path)) !== null) {
-    // /stable/10.5325/jmedirelicult.39.2.issue-2
-    result.title_id = match[3];
-    result.unitid   = match[2];
-    result.doi      = match[1];
-    result.rtype    = 'TOC';
-    result.mime     = 'MISC';
 
-  } else if ((match = /^\/stable\/(pdfplus|pdf)\/([0-9]+)\.pdf$/i.exec(path)) !== null) {
+  } else if ((match = /^\/stable\/(get_image|pdf|pdfplus)\/((10\.[0-9]+\/)?([\w.]+?))(?:\.pdf)?$/i.exec(path)) !== null) {
+    // /stable/get_image/23098031
+    // /stable/get_image/10.1525/gfc.2010.10.4.cover
+    // /stable/get_image/10.1525/gfc.2010.10.4.103a
     // /stable/pdfplus/690326.pdf
-    result.title_id = match[2];
-    result.unitid   = match[2];
-    result.rtype    = 'ARTICLE';
-    result.mime     = match[1] === 'pdfplus' ? 'PDFPLUS' : 'PDF';
+    // /stable/pdf/10.13110/merrpalmquar1982.59.2.0198.pdf
+    // /stable/pdf/10.1525/gfc.2010.10.4.cover.pdf
+    // /stable/pdf/10.1525/gfc.2010.10.4.98a.pdf
+
+    result.unitid = match[4];
+
+    if (match[3]) {
+      result.doi = match[2];
+    }
+
+    switch (match[1]) {
+    case 'get_image':
+      result.rtype = 'ARTICLE_SECTION';
+      result.mime  = 'GIF';
+      break;
+    case 'pdf':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'PDF';
+      break;
+    case 'pdfplus':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'PDFPLUS';
+      break;
+    }
+
+    const idPattern = /^([a-z0-9]+)((?:\.(\d+))?\.(\d+)\.(\d+)\.(\w+))?/.exec(result.unitid) || [];
+
+    result.title_id         = idPattern[1];
+    result.publication_date = idPattern[3];
+    result.vol              = idPattern[4];
+    result.issue            = idPattern[5];
+
+    if (idPattern[6] === 'cover') {
+      result.rtype = 'COVER';
+    } else if (idPattern[6] === 'toc') {
+      result.rtype = 'TOC';
+    } else {
+      const firstPage = parseInt(idPattern[6]);
+
+      if (!isNaN(firstPage)) {
+        result.first_page = firstPage.toString();
+      }
+    }
 
   } else if ((match = /^\/stable\/(info|view)\/([0-9]+)$/i.exec(path)) !== null) {
     // /stable/info/25703252
@@ -86,15 +122,7 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.rtype    = match[1] === 'info' ? 'ABS' : 'PREVIEW';
     result.mime     = 'MISC';
 
-  } else if ((match = /^\/stable\/(pdf\/)?([0-9]{2}\.[0-9]+\/([a-z]+\.?[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+))(?:\.pdf)?$/i.exec(path)) !== null) {
-    // /stable/10.7312/cari13424
-    // /stable/pdf/10.13110/merrpalmquar1982.59.2.0198.pdf
-    // /stable/10.1525/gfc.2010.10.3.14
-    result.unitid = match[3];
-    result.doi    = match[2];
-    result.rtype  = 'ARTICLE';
-    result.mime   = match[1] ? 'PDF' : 'HTML';
-
   }
+
   return result;
 });
