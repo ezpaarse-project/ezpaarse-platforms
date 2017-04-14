cbbdee6c8302f6323fed07d7e0e4e4863bb7afe3
Yannick Schurter <yannick.schurter@gmail.com>
2017-04-14 11:08:46 +0200

    acland optimization

--- acland/parser.js
+++ acland/parser.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
-// ##EZPAARSE
-
-/*jslint maxlen: 150*/
 'use strict';
-var Parser = require('../.lib/parser.js');
+const Parser = require('../.lib/parser.js');
 
 /**
  * Recognizes the accesses to the platform Acland's Video Atlas of Human Anatomy
@@ -14,39 +11,32 @@ var Parser = require('../.lib/parser.js');
  * @return {Object} the result
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
-  var result = {};
-  var path = parsedUrl.pathname;
-    // uncomment this line if you need parameters
-  var param = parsedUrl.query || {};
+  const result = {};
+  const path   = parsedUrl.pathname;
+  const param  = parsedUrl.query || {};
 
-    // use console.error for debuging
-    // console.error(parsedUrl);
-
-  if ((/^\/MultimediaPlayer\.aspx$/.exec(path)) !== null) {
-        // http://aclandanatomy.com/MultimediaPlayer.aspx?multimediaId=10528543
-    result.rtype = 'VIDEO';
-    result.mime = 'HTML';
-        //unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-        //it described the most fine-grained of what's being accessed by the user
-        //it can be a DOI, an internal identifier or a part of the accessed URL
-        //see http://ezpaarse.couperin.org/doc/ec-attributes.html#description-de-unitid for more details
+  switch (path.toLowerCase()) {
+  case '/multimediaplayer.aspx':
+    // http://aclandanatomy.com/MultimediaPlayer.aspx?multimediaId=10528543
+    result.rtype  = 'VIDEO';
+    result.mime   = 'HTML';
     result.unitid = param.multimediaId;
-  } else if ((/^\/Multimedia\.aspx$/.exec(path)) !== null) {
-        // http://aclandanatomy.com/Multimedia.aspx?categoryid=39464
-    result.rtype = 'TOC';
-    result.mime = 'HTML';
-
-        //see the comment block above
+    break;
+  case '/multimedia.aspx':
+    // http://aclandanatomy.com/Multimedia.aspx?categoryid=39464
+    result.rtype  = 'TOC';
+    result.mime   = 'HTML';
     result.unitid = param.categoryid;
-  } else if ((/^\/atozresults\.aspx$/.exec(path)) !== null) {
-        // http://aclandanatomy.com/atozresults.aspx?resourceindex=16&displayname=abdominal+arteries
-    result.rtype = 'TOC';
-    result.mime = 'HTML';
-
-        //see the comment block above
-    result.unitid = param.resourceindex;
+    break;
+  case '/atozresults.aspx':
+    // http://aclandanatomy.com/atozresults.aspx?resourceindex=16&displayname=abdominal+arteries
+    result.rtype    = 'TOC';
+    result.mime     = 'HTML';
+    result.unitid   = param.resourceindex;
     result.title_id = param.displayname;
+    break;
   }
+
   return result;
 });
 
