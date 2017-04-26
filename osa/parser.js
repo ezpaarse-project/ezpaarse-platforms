65b7d5d801232ca1204a02a7a2a5a54d83c70996
Unknown <narko_54@hotmail.fr>
2017-04-26 14:27:30 +0200

    fix there case where "jot" could be replaced by an other identifier

--- osa/parser.js
+++ osa/parser.js
@@ -18,30 +18,31 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   // use console.error for debuging
   // console.error(parsedUrl);
+  let match;
 
-  if ((/^\/jot\/issue.cfm?$/.test(path)) !== null) {
+  if ((match = /^\/([a-z]+)\/issue.cfm?$/i.exec(path)) !== null) {
     // https://www-osapublishing-org.gaelnomade.ujf-grenoble.fr/jot/issue.cfm?volume=83&issue=2
     result.rtype    = 'TOC';
     result.mime     = 'HTML';
-    result.title_id = 'jot';
+    result.title_id = match[1];
     result.volume   = param.volume;
     result.issue    = param.issue;
-  } else if ((/^\/jot\/abstract.cfm$/.test(path)) !== null) {
+  } else if ((match = /^\/([a-z]+)\/abstract.cfm$/i.exec(path)) !== null) {
     // https://www-osapublishing-org.gaelnomade.ujf-grenoble.fr/jot/abstract.cfm?uri=jot-83-2-81
     // https://www-osapublishing-org.gaelnomade.ujf-grenoble.fr/jot/abstract.cfm?uri=jot-83-2-81#articleReferences
     if (parsedUrl.hash === null) {
-      result.rtype  = 'ABS';
+      result.rtype    = 'ABS';
     } else {
-      result.rtype  = 'REF';
+      result.rtype    = 'REF';
     }
     result.unitid   = param.uri;
     result.mime     = 'HTML';
-    result.title_id = 'jot';
-  } else if ((/^\/jot\/viewmedia.cfm$/.test(path)) !== null) {
+    result.title_id = match[1];
+  } else if ((match = /^\/([a-z]+)\/viewmedia.cfm$/i.exec(path)) !== null) {
     // https://www-osapublishing-org.gaelnomade.ujf-grenoble.fr/jot/viewmedia.cfm?uri=jot-83-2-81&seq=0
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
-    result.title_id = 'jot';
+    result.title_id = match[1];
     result.unitid   = param.uri;
   }
   return result;
