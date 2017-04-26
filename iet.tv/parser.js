3ffe1182d54f10c2a4f35536f5a12caca57fdb3c
Aurelian Popescu <aurelian.popescu@museglobal.ro>
2017-04-26 18:22:11 +0300

    Added the iet.tv platform.

---
+++ iet.tv/parser.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+// ##EZPAARSE
+
+/*jslint maxlen: 150*/
+'use strict';
+var Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform IET.tv
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  var result = {};
+  //var path   = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+  var param  = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  if (param && param.videoid) {
+    // https://tv.theiet.org/?videoid=8132
+    result.rtype    = 'VIDEO';
+    result.mime     = 'HTML';
+    result.unitid   = param.videoid;
+  }
+
+  return result;
+});
+
