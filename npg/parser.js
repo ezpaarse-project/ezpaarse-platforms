70c5c5404c1e353c8e01290a99a2584d2ffdaa43
sfmax54 <narko_54@hotmail.fr>
2017-05-16 13:55:17 +0200

    Fix uncompleted parser

--- npg/parser.js
+++ npg/parser.js
@@ -4,21 +4,21 @@
 
 'use strict';
 
-const Parser    = require('../.lib/parser.js');
+const Parser = require('../.lib/parser.js');
 const doiPrefix = '10.1038';
 
 module.exports = new Parser(function analyseEC(parsedUrl) {
-  let result   = {};
-  let path     = parsedUrl.pathname;
-  var params   = parsedUrl.query || {};
+  let result = {};
+  let path = parsedUrl.pathname;
+  var params = parsedUrl.query || {};
   let match;
 
   if ((match = /^\/([a-zA-Z0-9]+)\/journal\/v([a-z0-9]+)\/n([0-9]+|current)\/(pdf|full|abs|extref)\/([a-zA-Z0-9\.\-_]+)\.[a-z]{2,4}$/.exec(path)) !== null) {
     // http://www.nature.com/nrm/journal/vaop/ncurrent/full/nrm3940.html
     result.title_id = match[1];
-    result.vol      = match[2];
-    result.issue    = match[3];
-    result.unitid   = result.doi = `${doiPrefix}/${match[5]}`;
+    result.vol = match[2];
+    result.issue = match[3];
+    result.unitid = result.doi = `${doiPrefix}/${match[5]}`;
 
     let dashIndex = match[5].search('-');
     if (dashIndex >= 0) {
@@ -26,7 +26,7 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     }
 
     if (match[1] !== 'nature' && match[5].startsWith(match[1])) {
-      let journal = match[1];
+      let journal    = match[1];
       let identifier = match[5].substr(journal.length);
 
       if (identifier.endsWith('a')) {
@@ -65,9 +65,9 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     // http://www.nature.com/news/2007/070919/pdf/449268a.pdf
     // http://www.nature.com/news/2005/050131/full/news050131-3.html
 
-    result.title_id = match[1] === 'news' ? 'nature' : match[1];
+    result.title_id         = match[1] === 'news' ? 'nature' : match[1];
     result.publication_date = match[2];
-    result.unitid = result.doi = `${doiPrefix}/${match[4]}`;
+    result.unitid           = result.doi = `${doiPrefix}/${match[4]}`;
 
     if (result.title_id !== 'nature' && match[4].startsWith(result.title_id)) {
       let identifier = match[4].substr(result.title_id.length);
@@ -103,7 +103,6 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
   } else if ((match = /\/articles\/(?:doi:[0-9]+\.[0-9]+\/)?([a-zA-Z0-9\-_]+)\.epdf/.exec(path)) !== null) {
     // http://www.nature.com:80/articles/doi:10.1038/nature16976.epdf?parent_url=http%3A%2F%2Fwww.readcube.com%2Farticles%2F10.1038%252Fnature16976&pdf_url=http%3A%2F%2Fwww.nature.com%2Fnature%2Fjournal%2Fv531%2Fn7592%2Fpdf%2Fnature16976.pdf
     // http://www.nature.com:80/articles/nature16976.epdf?parent_url=http%3A%2F%2Fwww.readcube.com%2Farticles%2F10.1038%252Fnature16976&pdf_url=http%3A%2F%2Fwww.nature.com%2Fnature%2Fjournal%2Fv531%2Fn7592%2Fpdf%2Fnature16976.pdf
-
     let pdfUrl = parsedUrl && parsedUrl.query.pdf_url;
     if (pdfUrl) {
       result = this.execute({ url: pdfUrl });
@@ -137,9 +136,9 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.mime  = 'MISC';
   }
 
-  if ((match = /\/search/.exec(path)) !== null) {
+  if ((match = /\/(search|facet-search)/.exec(path)) !== null) {
     // http://nano.nature.com.insb.bib.cnrs.fr/search?q=nanoparticules&workflow=article&term=concept%3A%22nanoparticles%22&new-search=true
-    if (params.q == 'nanoparticules') {
+    if (params.q) {
       result.mime   = 'HTML';
       result.rtype  = 'SEARCH';
       result.unitid = params.q;
@@ -151,6 +150,16 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.mime   = 'HTML';
     result.rtype  = 'REF';
     result.unitid = match[1];
+  }
+
+  if ((match = /\/related-nanoobject-summary/i.exec(path)) !== null) {
+    // http://nano.nature.com/related-nanoobject-summary?doi=10.2147/IJN.S59290
+    if (params.doi) {
+      result.mime   = 'MISC';
+      result.rtype  = 'REF';
+      result.doi    = params.doi;
+      result.unitid = result.doi.split('/')[1];
+    }
   }
 
   return result;
