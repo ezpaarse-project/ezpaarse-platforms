0e36d2ea08073bf3d23659d5e02563923f0ef67e
Yannick Schurter <yannick.schurter@gmail.com>
2017-07-05 14:52:45 +0200

    update vocable

--- vn/parser.js
+++ vn/parser.js
@@ -1,59 +1,50 @@
 #!/usr/bin/env node
 
-// ##EZPAARSE
-
-/*jslint maxlen: 150*/
 'use strict';
-var Parser = require('../.lib/parser.js');
+const Parser = require('../.lib/parser.js');
 
 /**
- * Recognizes the accesses to the platform Vocalble Numérique
+ * Recognizes the accesses to the platform Vocable Numérique
  * @param  {Object} parsedUrl an object representing the URL to analyze
  *                            main attributes: pathname, query, hostname
  * @param  {Object} ec        an object representing the EC whose URL is being analyzed
  * @return {Object} the result
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
-  var result = {};
-  var path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  var param  = parsedUrl.query || {};
+  let result = {};
+  let path   = parsedUrl.pathname;
+  let param  = parsedUrl.query || {};
+  let match;
 
-  // use console.error for debuging
-  // console.error(parsedUrl);
-
-  var match;
-
-  if ((match = /^\/fr\/([a-zA-z0-9]+).asp$/.exec(path)) !== null) {
+  if (/^\/[a-z]{2,3}\/pvPage(2|H5B)\.asp$/i.test(path) || /^\/numerique\/learning\/pveditorsla6$/i.test(path)) {
+    // /numerique/learning/pveditorsla6?puc=005431&nu=739&tmpid=aa74dc0d9931ad93c5a0dd750270cd96&idp=248
+    // /fr/pvPage2.asp?puc=005431&nu=725&tmpid=abc2b40acad1147c223835752177d0f8
     // /fr/pvPageH5B.asp?puc=005879&nu=736&pa=1#8
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
-    if (param.nu) {
-      result.unitid   = param.nu;
+    result.rtype = 'ARTICLES_BUNDLE';
+    result.mime  = 'HTML';
+    result.issue = param.nu;
+
+    if (param.nu && param.puc) {
+      result.unitid = `${param.puc}${param.nu}`;
     }
 
-  } else if ((match = /^\/basearticle\/([a-z]+)\/([0-9]+)\/(([\w\W]+).pdf)$/.exec(path)) !== null) {
-    ///basearticle/artfiles/4184/Pages%20de%20VocGB715-2.pdf
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
-    result.unitid   = match[2] + '/' + match[3];
-  } else if ((match = /^\/numerique\/([a-z]+)\/([\w\W]+)$/.exec(path)) !== null) {
-    //numerique/learning/recherche#video-youtube
-    result.rtype    = 'VIDEO';
-    result.mime     = 'MISC';
+  } else if ((match = /^\/basearticle\/artfiles\/([0-9]+\/[^\/]+)\.pdf$/i.exec(path)) !== null) {
+    // /basearticle/artfiles/4184/Pages%20de%20VocGB715-2.pdf
+    result.rtype  = 'ARTICLE';
+    result.mime   = 'PDF';
+    result.unitid = match[1].toLowerCase().replace(/\s/g, '_');
 
-  } else if ((match = /^\/services\/([a-z]+)\/dwlaudio.php$/.exec(path)) !== null) {
-    //services/vocablearticles/dwlaudio.php?pub=es2&num=687&piste=2&tmpid=f74a7669695a72deb13658959673a444
-    result.rtype    = 'AUDIO';
-    result.mime     = 'MISC';
+  } else if (/^\/numerique\/learning$/.test(path)) {
+    // /numerique/learning
+    result.rtype = 'TOC';
+    result.mime  = 'HTML';
 
-  } else if ((match = /^\/basearticle\/([a-z]+)\/(([\w\W]+).pdf)$/.exec(path)) !== null) {
-    ////basearticle/reglepdf/VOCplusESP687%2015.pdf
-    result.rtype    = 'SUPPL';
-    result.mime     = 'PDF';
-    result.unitid   = match[2];
+  } else if ((match = /^\/numerique\/learning\/quiz\/([0-9]+)$/i.exec(path)) !== null) {
+    // /numerique/learning/quiz/10002#
+    result.rtype  = 'EXERCISE';
+    result.mime   = 'HTML';
+    result.unitid = match[1];
   }
 
   return result;
 });
-
