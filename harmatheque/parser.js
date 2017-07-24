28bd8ebe31fa75724f0ddeffaa0bdc00620de614
Yannick Schurter <yannick.schurter@gmail.com>
2017-07-24 17:22:40 +0200

    update harmatheque

--- harmatheque/parser.js
+++ harmatheque/parser.js
@@ -1,56 +1,65 @@
 #!/usr/bin/env node
 
-// ##EZPAARSE
-// very simple skeleton parser
-
-/*jslint maxlen: 150*/
 'use strict';
-var Parser = require('../.lib/parser.js');
+const Parser = require('../.lib/parser.js');
 
 module.exports = new Parser(function analyseEC(parsedUrl) {
-  var result = {};
+  let result = {};
+  let path   = parsedUrl.pathname;
+  let match;
 
-  //var param  = parsedUrl.query || {};
-  var path   = parsedUrl.pathname;
-
-  var match;
-
-  if ((match = /^\/playebook\/(.+)$/.exec(path)) !== null) {
+  if ((match = /^\/playebook\/(([a-z0-9_-]+?)(-[0-9]+)?)$/i.exec(path)) !== null) {
     // https://www.harmatheque.com/playebook/le-hitopadesha-recueil-de-contes-de-l-inde-ancienne
-
     result.rtype    = 'BOOK';
     result.mime     = 'PDF';
-    result.title_id = match[1].replace(new RegExp('-[0-9]+$'), '');
+    result.title_id = match[2];
     result.unitid   = match[1];
-  } else   if ((match = /^\/downloadebook\/(.+)$/.exec(path)) !== null) {
+
+  } else   if ((match = /^\/downloadebook\/(([a-z0-9_-]+?)(-[0-9]+)?)$/i.exec(path)) !== null) {
     // http://www.harmatheque.com/downloadebook/la-socionalyse-narrative-theorie-critique-et-pratique-du-changement-social-42310
-
     result.rtype    = 'BOOK';
     result.mime     = 'PDF';
-    result.title_id = match[1].replace(new RegExp('-[0-9]+$'), '');
+    result.title_id = match[2];
     result.unitid   = match[1];
-  } else if ((match = /^\/downloadepub\/(.+)$/.exec(path)) !== null) {
+
+  } else if ((match = /^\/downloadepub\/(([a-z0-9_-]+?)(-[0-9]+)?)$/i.exec(path)) !== null) {
     // http://www.harmatheque.com/downloadepub/apprendre-dans-les-reseaux-de-pme-le-role-des-contacts-personnels-41598
-
     result.rtype    = 'BOOK';
     result.mime     = 'EPUB';
-    result.title_id = match[1].replace(new RegExp('-[0-9]+$'), '');
+    result.title_id = match[2];
     result.unitid   = match[1];
-  } else if ((match = /^\/playepub\/(.+)$/.exec(path)) !== null) {
+
+  } else if ((match = /^\/playepub\/(([a-z0-9_-]+?)(-[0-9]+)?)$/i.exec(path)) !== null) {
     // http://www.harmatheque.com/playepub/apprendre-dans-les-reseaux-de-pme-le-role-des-contacts-personnels-41598
-
     result.rtype    = 'BOOK';
     result.mime     = 'EPUB';
-    result.title_id = match[1].replace(new RegExp('-[0-9]+$'), '');
+    result.title_id = match[2];
     result.unitid   = match[1];
-  } else if ((match = /^\/pdfswf\/livre/.exec(path)) !== null) {
-    // http://pdf.harmattan.fr/pdfswf/livre.asp?idbook=776436cb79d84789b054729000dc3b58&imc=1&isbn=9782343006932
 
+  } else if ((match = /^\/pdfswf\/livre\.asp$/.exec(path)) !== null) {
+    // http://pdf.harmattan.fr/pdfswf/livre.asp?idbook=776436cb79d84789b054729000dc3b58&imc=1&isbn=9782343006932
     result.rtype            = 'BOOK';
     result.mime             = 'PDF';
     result.title_id         = parsedUrl.query.isbn;
     result.unitid           = parsedUrl.query.idbook;
     result.print_identifier = parsedUrl.query.isbn;
+
+  } else if ((match = /^\/ebook\/(([a-z0-9_-]+?)(-[0-9]+)?)$/i.exec(path)) !== null) {
+    // http://www.harmatheque.com/ebook/migrations--decentralisation-et-cooperation-decentralisee-enjeux-et-perspectives-54119
+    result.rtype    = 'ABS';
+    result.mime     = 'HTML';
+    result.title_id = match[2];
+    result.unitid   = match[1];
+
+  } else if (/^\/rechercheavancee\/result$/i.test(path)) {
+    // http://www.harmatheque.com/rechercheavancee/result
+    result.rtype = 'TOC';
+    result.mime  = 'HTML';
+
+  } else if (/^\/advanced$/i.test(path)) {
+    // http://www.harmatheque.com/advanced
+    result.rtype = 'SEARCH';
+    result.mime  = 'HTML';
   }
 
   return result;
