7aba4e7bd078493b9d63c588ef8c41cba060eeb9
Yannick Schurter <yannick.schurter@gmail.com>
2017-07-25 09:49:03 +0200

    lint cyberlibris

--- cyberlibris/parser.js
+++ cyberlibris/parser.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
 
-// ##EZPAARSE
-
-/*jslint maxlen: 150*/
 'use strict';
-var Parser = require('../.lib/parser.js');
+const Parser = require('../.lib/parser.js');
 
 /**
  * Identifie les consultations de la plateforme Cyberlibris
@@ -14,15 +11,10 @@ var Parser = require('../.lib/parser.js');
  * @return {Object} the result
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
-  var result = {};
-  var path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  var param  = parsedUrl.query || {};
-
-  // use console.error for debuging
-  // console.error(path);
-
-  var match;
+  let result = {};
+  let path   = parsedUrl.pathname;
+  let param  = parsedUrl.query || {};
+  let match;
 
   if ((match = /^\/book\/([0-9]+)$/.exec(path)) !== null) {
     // http://univ-paris1.cyberlibris.com/book/88826141
@@ -30,6 +22,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime     = 'HTML';
     result.title_id = match[1];
     result.unitid   = match[1];
+
   } else if ((match = /^\/catalog\/book\/docid\/([0-9]+).*$/.exec(path)) !== null) {
     // http://univ-paris1.cyberlibris.com/catalog/book/docid/88805591/searchstring/femmes
     // http://univ-paris1.cyberlibris.com/catalog/book/docid/88805591
@@ -37,21 +30,24 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime     = 'HTML';
     result.title_id = match[1];
     result.unitid   = match[1];
-  } else if ((match = /^\/reader\/istream\/([a-z]+)\/([0-9]+)\/([a-z]+)\/([0-9]+)$/.exec(path)) !== null) {
+
+  } else if ((match = /^\/reader\/istream\/[a-z]+\/(([0-9]+)\/[a-z]+\/[0-9]+)$/.exec(path)) !== null) {
     // feuilletage en ligne HTML5
     // http://univ-paris1.cyberlibris.com/reader/istream/docid/88826141/page/1
     result.rtype    = 'BOOK';
     result.mime     = 'HTML';
     result.title_id = match[2];
-    result.unitid   = match[2]+'/'+match[3]+'/'+match[4];
+    result.unitid   = match[1];
+
   } else if ((match = /^\/reader\/flashpagesrv\/$/.exec(path)) !== null) {
     // feuilletage en ligne, version flash reader (traffic firebug)
     // http://univ-rennes1.cyberlibris.fr:80/reader/flashpagesrv/?docid=88814647&p=20&sig=001b540391345e4b79812e5285313c0a83a96dc9
     result.rtype    = 'BOOK';
     result.mime     = 'FLASH';
     result.title_id = param.docid;
-    result.unitid   = param.docid+'/page/'+param.p;
+    result.unitid   = `${param.docid}/page/${param.p}`;
   }
+
   return result;
 });
 
