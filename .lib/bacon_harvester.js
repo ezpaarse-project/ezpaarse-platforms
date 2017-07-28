2c949d5544957e1ca7df6fe82310e0f61bd0dfef
Yannick Schurter <yannick.schurter@gmail.com>
2017-07-28 14:52:25 +0200

    new bacon harvester

--- .lib/bacon_harvester.js
+++ .lib/bacon_harvester.js
@@ -1,145 +1,89 @@
-/* eslint global-require: 0 */
+/* eslint global-require:0 */
 'use strict';
-var fs      = require('fs');
-var path    = require('path');
-var request = require('request');
-var PkbRows = require('./pkbrows.js');
 
-exports.generatePkb = function (platformName, callback) {
-  var data = require('../' + platformName + '/manifest.json');
-  var pkbfolder = path.join(__dirname, '../pkb');
-  var nbFileCreated = 0;
+const fs      = require('fs');
+const path    = require('path');
+const co      = require('co');
+const request = require('request');
 
-  fs.mkdir(pkbfolder, function(err) {
-    if (err &&  err.code != 'EEXIST') { return callback(err); }
+/**
+ * Download the packages of a platform, given its root directory
+ * @param {String} platformDir platform root directory
+ */
+exports.downloadPackages = co.wrap(function* (platformDir) {
+  if (!platformDir) { return Promise.reject(new Error('no platform directory given')); }
 
-    console.log('Check pkb folder ', pkbfolder);
-    var url = 'https://bacon.abes.fr/list.json';
-    request.get(url, function(err, res, body) {
-      var list;
-      if (err) {
-        return callback(err);
-      }
+  const { bacon } = require(path.resolve(platformDir, 'manifest.json'));
+
+  if (!bacon || !bacon.provider) {
+    console.log('no bacon provider in the manifest');
+    return Promise.resolve();
+  }
+
+  let list = yield exports.fetchPackagesList();
+
+  list = list.filter(pkg => pkg.provider === bacon.provider);
+
+  if (bacon.filter) {
+    let reg = new RegExp(bacon.filter, 'i');
+    list = list.filter(pkg => reg.test(pkg.package));
+  }
+
+  for (const { package_id } of list) {
+    const file = path.resolve(platformDir, 'pkb', `${package_id}.txt`);
+
+    if (yield fileExists(file)) {
+      console.log(`  [No changes]  ${package_id}`);
+    } else {
+      console.log(`  [Downloading] ${package_id}`);
+      yield downloadPackage(package_id, file);
+    }
+  }
+});
+
+/**
+ * Fetch the list of all packages
+ */
+exports.fetchPackagesList = function () {
+  return new Promise((resolve, reject) => {
+    request.get('https://bacon.abes.fr/list.json', (err, res, body) => {
+      if (err) { return reject(err); }
+
+      let list;
       try {
         list = JSON.parse(body).bacon.query.results;
       } catch (e) {
-        return callback(e);
+        return reject(e);
       }
 
-      console.log('Search %s platform in bacon platform list', data.baconprovider);
-      var i = -1;
-      (function checklist() {
-        i++;
-        if (!list[i]) {
-          return callback(null, nbFileCreated);
-        }
-
-        var package_id = '';
-        if (list[i].element.provider != data.baconprovider) {
-          return checklist();
-        }
-
-        if (data.bacon_match_packageid) {
-          let reg = new RegExp(data.bacon_match_packageid);
-          if (!reg.test(list[i].element.package_id)) {
-            return checklist();
-          }
-        }
-        package_id = list[i].element.package_id;
-        var pkb = new PkbRows(platformName);
-        var pkbUrl = 'http://bacon.abes.fr/package2kbart/' + package_id + '.json';
-        request.get(pkbUrl, function(err, res, body) {
-          pkb.setKbartName(package_id);
-          if (err) {
-            return callback(err);
-          }
-
-          var listpkb;
-          try {
-            listpkb = JSON.parse(body).bacon.query.kbart;
-          } catch (e) {
-            console.error(pkbUrl, 'json parse error');
-            return checklist();
-          }
-
-          var j = 0;
-          while (j < listpkb.length) {
-            pkb.addRow(listpkb[j].element);
-            j++;
-          }
-          pkb.writeKbart();
-          nbFileCreated++;
-          console.log('File %s created', pkb.kbartFileName);
-          return callback();
-        });
-
-      })();
-
-    });
-  });
-
-
-  //récupération de la liste des platforme sur le site de abes
-
-};
-
-exports.checkNewPkb = function() {
-  var re;
-  var data;
-  var url = 'https://bacon.abes.fr/list.json';
-  request.get(url, function(err, res, body) {
-    var list;
-    if (err) {
-      return console.error(err);
-    }
-    try {
-      var result = JSON.parse(body);
-      list = result.bacon.query.results;
-    } catch (e) {
-      return console.error(e);
-    }
-    var p = path.join(__dirname, '/../');
-
-    fs.readdir(p, function(err, folder) {
-      var i = 0;
-      var j = 4;
-      if (!list) {
-        return console.error('Bacon not connected');
-      }
-      while (i < list.length) {
-        j = 4;
-        if (!folder) {
-          return console.error('empty folder');
-        }
-        while (j < folder.length) {
-          re = /(.json|.md)/ig;
-          if (re.exec(folder[j]) == null) {
-            data = require('../' + folder[j] + '/manifest.json');
-            if (list[i].element.provider === data.baconprovider) {
-              console.log(' ' + list[i].element.provider + ' ==> OK');
-              break;
-            }
-          }
-          j++;
-        }
-        i++;
-      }
-      i = 0;
-      while (i < list.length) {
-        j = 4;
-        while (j < folder.length) {
-          re = /(.json|.md)/ig;
-          if (re.exec(folder[j]) == null) {
-            data = require('../' + folder[j] + '/manifest.json');
-            if (list[i].element.provider != data.baconprovider) {
-              console.log(' ' + list[i].element.provider + ' à télécharger');
-              break;
-            }
-          }
-          j++;
-        }
-        i++;
-      }
+      resolve(list.map(pkg => pkg.element));
     });
   });
 };
+
+/**
+ * Download a package
+ * @param {Object} packageId  bacon package information
+ * @param {String} dest destination directorylist
+ */
+function downloadPackage(packageId, dest) {
+  return new Promise((resolve, reject) => {
+    request.get(`https://bacon.abes.fr/package2kbart/${packageId}`)
+      .pipe(fs.createWriteStream(dest))
+      .on('error', reject)
+      .on('finish', resolve);
+  });
+}
+
+/**
+ * Check that a file exists
+ * @param {String} file path of the file
+ */
+function fileExists(file) {
+  return new Promise((resolve, reject) => {
+    fs.stat(file, (err, stat) => {
+      if (err && err.code !== 'ENOENT') { return reject(err); }
+      resolve(!!stat);
+    });
+  });
+}
