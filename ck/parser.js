00f88a28fee0bf610599becf33249617b866a708
AGCooper <alexander.g.cooper@gmail.com>
2017-11-08 18:25:11 -0500

    Updates files for pull request #8

--- ck/parser.js
+++ ck/parser.js
@@ -72,7 +72,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.rtype    = 'BOOK';
     result.mime     = 'HTML';
     result.unitid   = match[1];
-/**    match = /eid=3-s2\.0-(.*?)&/.exec(path);
+    /**match = /eid=3-s2\.0-(.*?)&/.exec(path);
     result.pii      = match[1];*/
   } else if ((match = /\/service\/browse\/.*?\/toc\?/.exec(path)) !== null) {
     // https://www.clinicalkey.com:443/service/browse/1-s2.0-S0306460317X00117/toc?hubeid=1-s2.0-S0306460317X00117&product=en_US&rows=9999&start=0
