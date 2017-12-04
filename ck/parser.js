b9b73da90d8134655701ab1c17a31ba9c4653d6f
AGCooper <alexander.g.cooper@gmail.com>
2017-12-04 12:18:29 -0500

    Cosmetic and semantic updates

--- ck/parser.js
+++ ck/parser.js
@@ -12,6 +12,7 @@ const Parser = require('../.lib/parser.js');
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
+  let pathname = parsedUrl.pathname;
   let path   = parsedUrl.path;
   // uncomment this line if you need parameters
   // let param = parsedUrl.query || {};
@@ -21,7 +22,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   let match;
 
-  if ((match = /^\/ui\/service\/search\?/.exec(path)) !== null) {
+  if (/^\/ui\/service\/search$/i.test(pathname)) {
     // https://www.clinicalkey.com:443/ui/service/search?ckproduct=CK_US&fields=cid,itemtitle,eid,contenttype,hubeid,refimage,pdfeid,sourcetitle,pagerange,coverdatestart,authorlist,volumeissue,authorg,condition,intervention,copyrightyear,issn,pubdate,datecreatedtxt,daterevisedtxt,statustype,exactsourcetitle,label,sectionid,sourcecopyright,sourceeditor,sourcetype,toc,volume,issue,pagefirst,pagelast,pageinfo,chaptertitle,chapternumber,ngcsynthesisurl,altlangeid,provider,summary,ref,reftitle,altlang,lang,srcid,tradenames,subtype,sections,sectiontitle,chptitle,doctype,embargo,sectionids&onlyEntitled=true&query=cancer&rows=20&sections=results,facets,resultsanalysis,bestbets,topicpages,didyoumean,didyoumean,disambiguations&start=0
     result.rtype    = 'SEARCH';
     result.mime     = 'HTML';
@@ -35,7 +36,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     }
     result.mime     = 'HTML';
     result.unitid   = match[1];
-  } else if ((match = /\/service\/content\/pdf\/watermarked\/(([0-9a-z-.]*)(.*?))\?/.exec(path)) !== null) {
+  } else if ((match = /^\/service\/content\/pdf\/watermarked\/(([0-9a-z-.]*)(.*))$/.exec(pathname)) !== null) {
     // https://www.clinicalkey.com:443/service/content/pdf/watermarked/1-s2.0-S0003999317303908.pdf?locale=en_US
     if (match[2] === '1-s2.0-') {
       result.rtype    = 'ARTICLE';
@@ -43,26 +44,26 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime     = 'PDF';
     result.unitid   = match[1];
     result.pii      = match[3];
-  } else if ((match = /\/service\/content\/ck\/(([0-9a-z-]*?)-([0-9a-z.]*?)-([0-9a-zA-Z-_]*))/.exec(path)) !== null) {
+  } else if ((match = /^\/ui\/service\/content\/ck\/(([0-9a-z-]*?)-([0-9a-z.]*?)-([0-9a-zA-Z-_]*))$/.exec(pathname)) !== null) {
     // https://www.clinicalkey.com:443/ui/service/content/ck/31-s2.0-50300
     result.rtype    = 'REF';
     result.mime     = 'HTML';
     result.unitid   = match[1];
-  } else if ((match = /\/ui\/service\/clinical_overviews\/html\/(.*?)\?/.exec(path)) !== null) {
+  } else if ((match = /^\/ui\/service\/clinical_overviews\/html\/(.*?)$/.exec(pathname)) !== null) {
     // https://www.clinicalkey.com:443/ui/service/clinical_overviews/html/67-s2.0-3ccbf894-f46b-41db-9321-02dea462dc2c?separateUrgentAction=true&boxes=Differential%20Diagnosis&accordions=Diagnostic%20Procedures&references=true&imageHtml=true&crossLinkHtml=true&product=global
     result.rtype    = 'REF';
     result.mime     = 'HTML';
     result.unitid   = match[1];
-  } else if ((match = /\/service\/browse\/(.*)?\/toc\?/.exec(path)) !== null) {
+  } else if ((match = /^\/service\/browse\/(.*)\/toc$/.exec(pathname)) !== null) {
     // https://www.clinicalkey.com:443/service/browse/1-s2.0-S0306460317X00117/toc?hubeid=1-s2.0-S0306460317X00117&product=en_US&rows=9999&start=0
     result.rtype    = 'TOC';
     result.mime     = 'HTML';
     result.unitid   = match[1];
-  } else if ((match = /service\/browse\/drugs\?ckproduct=CK_US&contenttype=DM/.exec(path)) !== null) {
+  } else if (/^\/service\/browse\/drugs\?ckproduct=CK_US&contenttype=DM/.test(path)) {
     // https://www.clinicalkey.com:443/service/browse/drugs?ckproduct=CK_US&contenttype=DM&firstchar=O&onlyEntitled=true&product=en_US&rows=50
     result.rtype    = 'TOC';
     result.mime     = 'HTML';
-  } else if ((match = /service\/content\/ck\/(.*)/.exec(path)) !== null) {
+  } else if ((match = /^\/service\/content\/ck\/(.*)$/.exec(pathname)) !== null) {
     // https://www.clinicalkey.com:443/service/content/ck/6-s2.0-3547
     result.rtype    = 'REF';
     result.mime     = 'HTML';
