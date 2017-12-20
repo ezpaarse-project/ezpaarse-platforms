9767f466b87ce4a165acaffa98a4a21de5c61f67
AGCooper <alexander.g.cooper@gmail.com>
2017-12-20 15:40:58 -0500

    Adds parsing for Lisa's request

--- nexisuni/parser.js
+++ nexisuni/parser.js
@@ -25,7 +25,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // https://advance.lexis.com:443/search/?pdmfid=1516831&crid=f4b28c75-23dc-4b70-9131-db6f9c0ca518&pdtypeofsearch=searchboxclick&pdsearchtype=SearchBox&pdqttype=and&pdbcts=1513723230381&pdpsf=undefined&earg=pdpsf&pdsearchterms=trump&pdquerytemplateid=urn%3Aquerytemplate%3A78338d18781c574d11af5fa2f7097c99~%5ENews&pdstartin=undefined&ecomp=s8ykkhk&prid=8b8a9048-9ae2-4636-a607-6a2bd9590118&_=1513723234590
     result.rtype    = 'SEARCH';
     result.mime     = 'HTML';
-  } else if (/^\/document\/$/i.test(path)) {
+  } else if (/\/document\//i.test(path)) {
     // https://advance.lexis.com:443/document/?pdmfid=1516831&crid=cf9d3984-a5ab-4c29-8d8c-959f2d179bb8&pddocfullpath=%2Fshared%2Fdocument%2Fnews%2Furn%3AcontentItem%3A5MPY-92H1-JCT0-N1F7-00000-00&pddocid=urn%3AcontentItem%3A5MPY-92H1-JCT0-N1F7-00000-00&pdcontentcomponentid=237501&pdteaserkey=sr0&pditab=allpods&ecomp=dy_k&earg=sr0&prid=f4b28c75-23dc-4b70-9131-db6f9c0ca518&_=1513723378045
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
@@ -39,6 +39,26 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // https://advance.lexis.com:443/r/delivery/content/190521299/download/58522529/FullDoc
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
+    result.unitid   = match[1];
+  } else if (/^\/en-us\/about-us\/media\/press-release.page$/i.test(path)) {
+    // http://www.lexisnexis.com:80/en-us/about-us/media/press-release.page?id=1485461802825730&y=2017
+    result.rtype    = 'REF';
+    result.mime     = 'html';
+    result.id       = param.id;
+    result.publication_date = param.y;
+  } else if (/\/practice-advisor-browse-results\/$/i.test(path)) {
+    // https://advance.lexis.com:443/open/practice-advisor-browse-results/?pdcatfilters=UHJhY3RpY2VBcmVhXnVybjp0b3BpYzo4NjRCRTQ0MjhDN0QzRTJCOEIxMkY2OTExRjJFNkY0Q3xDb250ZW50VHlwZV5BUlRJQ0xFUw&pdbcts=1513798142432&crid=ee519864-de91-4aca-a60d-4cc39438ee24&prid=90072459-91ea-4f7d-87cc-2d0a6dfde4c5&pdpt=Articles
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+  } else if (/^\/en-us\/search.page$/i.test(path)) {
+    // https://www.lexisnexis.com:443/en-us/search.page?page=5&q=search%20for%20stuff%20here
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+  } else if ((match = /^\/pdf\/lexis-advance\/(.*)$/i.exec(path)) !== null) {
+    // https://www.lexisnexis.com:443/pdf/lexis-advance/Common-Research-Tasks.pdf
+    result.rtype    = 'REF';
+    result.mime     = 'PDF';
+    result.title_id = match[1];
     result.unitid   = match[1];
   }
 
