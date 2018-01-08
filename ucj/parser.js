01ef3eae107f62c5994614318bec1cab8ca6d385
Yannick Schurter <yannick.schurter@gmail.com>
2018-01-08 09:45:22 +0100

    tweaks

--- ucj/parser.js
+++ ucj/parser.js
@@ -16,33 +16,30 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   // uncomment this line if you need parameters
   // let param = parsedUrl.query || {};
 
-  // use console.error for debuging
-  // console.error(parsedUrl);
-
   let match;
 
-  if ((match = /^\/action\/doSearch$/i.exec(path)) !== null) {
+  if (/^\/action\/doSearch$/i.test(path)) {
     // http://www.journals.uchicago.edu:80/action/doSearch?SeriesKey=aft&AllField=picasso
-    result.rtype    = 'SEARCH';
-    result.mime     = 'HTML';
-  } else if ((match = /^\/doi\/full\/((.*)\/(.*))$/i.exec(path)) !== null) {
+    result.rtype = 'SEARCH';
+    result.mime  = 'HTML';
+  } else if ((match = /^\/doi\/full\/(10\.[0-9]+\/(.*))$/i.exec(path)) !== null) {
     // http://www.journals.uchicago.edu:80/doi/full/10.1086/676545
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
-    result.doi      = match[1];
-    result.unitid   = match[3];
-  } else if ((match = /^\/doi\/pdfplus\/((.*)\/(.*))$/i.exec(path)) !== null) {
+    result.rtype  = 'ARTICLE';
+    result.mime   = 'HTML';
+    result.doi    = match[1];
+    result.unitid = match[2];
+  } else if ((match = /^\/doi\/pdfplus\/(10\.[0-9]+\/(.*))$/i.exec(path)) !== null) {
     // http://www.journals.uchicago.edu:80/doi/pdfplus/10.1086/449144
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDFPLUS';
-    result.doi      = match[1];
-    result.unitid   = match[3];
-  } else if ((match = /^\/doi\/abs\/((.*)\/(.*))$/i.exec(path)) !== null) {
+    result.rtype  = 'ARTICLE';
+    result.mime   = 'PDFPLUS';
+    result.doi    = match[1];
+    result.unitid = match[2];
+  } else if ((match = /^\/doi\/abs\/(10\.[0-9]+\/(.*))$/i.exec(path)) !== null) {
     // http://www.journals.uchicago.edu:80/doi/abs/10.1086/449146
-    result.rtype    = 'ABS';
-    result.mime     = 'HTML';
-    result.doi      = match[1];
-    result.unitid   = match[3];
+    result.rtype  = 'ABS';
+    result.mime   = 'HTML';
+    result.doi    = match[1];
+    result.unitid = match[2];
   } else if ((match = /^\/toc\/[a-z]*\/([0-9]*)\/([0-9]*)\//i.exec(path)) !== null) {
     // http://www.journals.uchicago.edu:80/toc/aft/2017/44/+
     result.rtype    = 'TOC';
@@ -51,34 +48,34 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.issue    = match[2];
   } else if ((match = /^\/doi\/(([0-9.]*)\/([0-9]*))$/i.exec(path)) !== null) {
     // http://www.journals.uchicago.edu:80/doi/10.1086/693954
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
-    result.doi      = match[1];
-    result.unitid   = match[3];
+    result.rtype  = 'ARTICLE';
+    result.mime   = 'HTML';
+    result.doi    = match[1];
+    result.unitid = match[3];
   } else if ((match = /^\/toc\/[a-z]*\/([a-z]*)$/i.exec(path)) !== null) {
     // http://www.journals.uchicago.edu:80/toc/aje/current
-    result.rtype    = 'TOC';
-    result.mime     = 'HTML';
-    result.issue    = match[1];
+    result.rtype = 'TOC';
+    result.mime  = 'HTML';
+    result.issue = match[1];
   } else if (/^\/journals\/[a-z]*\/received$/i.test(path)) {
     // http://www.journals.uchicago.edu:80/journals/rq/received
-    result.rtype    = 'TOC';
-    result.mime     = 'HTML';
+    result.rtype = 'TOC';
+    result.mime  = 'HTML';
   } else if (/^\/loi\/[a-z]*$/i.test(path)) {
     // http://www.journals.uchicago.edu:80/loi/jacr
-    result.rtype    = 'TOC';
-    result.mime     = 'HTML';
+    result.rtype = 'TOC';
+    result.mime  = 'HTML';
   } else if ((match = /^\/pb-assets\/docs\/journals\/[a-z]*-books-recd\/(.*)\.pdf$/i.exec(path)) !== null) {
     // http://www.journals.uchicago.edu:80/pb-assets/docs/journals/rq-books-recd/RQ-books-received-70-3.pdf
-    result.rtype    = 'REF';
-    result.mime     = 'PDF';
-    result.unitid   = match[1];
-  } else if ((match = /^\/doi\/citedby\/((.*)\/(.*))$/i.exec(path)) !== null) {
+    result.rtype  = 'REF';
+    result.mime   = 'PDF';
+    result.unitid = match[1];
+  } else if ((match = /^\/doi\/citedby\/(10\.[0-9]+\/(.*))$/i.exec(path)) !== null) {
     // http://www.journals.uchicago.edu:80/doi/citedby/10.1086/693161
-    result.rtype    = 'REF';
-    result.mime     = 'HTML';
-    result.doi      = match[1];
-    result.unitid   = match[3];
+    result.rtype  = 'REF';
+    result.mime   = 'HTML';
+    result.doi    = match[1];
+    result.unitid = match[2];
   }
 
   return result;
