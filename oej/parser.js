dc8abe67173e7dafa78b5c4c07fac63279b27069
Yannick Schurter <yannick.schurter@gmail.com>
2018-02-07 15:29:16 +0100

    update oej

--- oej/parser.js
+++ oej/parser.js
@@ -1,10 +1,7 @@
 #!/usr/bin/env node
-
-// ##EZPAARSE
-
-/*jslint maxlen: 150*/
 'use strict';
-var Parser = require('../.lib/parser.js');
+
+const Parser = require('../.lib/parser.js');
 
 /**
  * Recognizes the accesses to the platform OpenEdition Journals
@@ -14,34 +11,33 @@ var Parser = require('../.lib/parser.js');
  * @return {Object} the result
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
-  var result = {};
-  var path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  // var param  = parsedUrl.query || {};
+  const result   = {};
+  const path     = parsedUrl.pathname;
+  const host     = parsedUrl.hostname || '';
+  const fileSize = parseInt(ec.size, 10);
+  let match;
 
-  // use console.error for debuging
-  // console.error(parsedUrl);
 
-  var match;
-  if ((match = /^\/([0-9]+)$/.exec(path)) !== null) {
-    //http://www.openedition.org/13191
-    result.rtype    = 'TOC';
-    if (parsedUrl.host.split('.')[0] !== 'www') {
-      result.rtype    = 'ARTICLE';
-      if (parsedUrl.hash && parsedUrl.hash === '#abstract') {
-        result.rtype    = 'ABS';
-      }
-      result.title_id = parsedUrl.host.split('.')[0];
-    }
-
-    result.mime     = 'HTML';
-    result.unitid   = match[1];
-  } else if ((match = /^\/pdf\/([0-9]+)$/.exec(path)) !== null) {
+  if ((match = /^(\/[a-z-]+)?\/pdf\/([0-9]+)$/i.exec(path)) !== null) {
     // http://socio.revues.org/pdf/1882
+    // http://journals.openedition.org/crau/pdf/370
+
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
-    result.title_id = parsedUrl.host.split('.')[0];
-    result.unitid   = match[1];
+    result.title_id = match[1] ? match[1].substr(1) : host.split('.')[0];
+    result.unitid   = match[2];
+
+  } else if ((match = /^(\/[a-z-]+)?\/([0-9]+)$/i.exec(path)) !== null) {
+    // http://socio.revues.org/1877
+    // http://journals.openedition.org/socio/3061
+
+    // if the size is less than 10ko, it's unlikely to be an article
+    if (!fileSize || fileSize > 10000) {
+      result.rtype    = 'ARTICLE';
+      result.mime     = 'HTML';
+      result.unitid   = match[2];
+      result.title_id = match[1] ? match[1].substr(1) : host.split('.')[0];
+    }
   }
 
   return result;
