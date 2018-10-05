e5a6ee05f6f0575a41416d1a0500ef4dd33d963d
eurruby <laurent.lhuillier@gmail.com>
2018-10-05 10:05:47 +0200

    update lextenso parser

--- lextenso/parser.js
+++ lextenso/parser.js
@@ -3,61 +3,51 @@
 'use strict';
 const Parser = require('../.lib/parser.js');
 
-module.exports = new Parser(function analyseEC(parsedUrl) {
+/**
+ * Recognizes the accesses to the platform Lextenso
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
-  let param = parsedUrl.query || {};
-  let path  = parsedUrl.pathname;
+  let path   = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+  // let param = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
   let match;
 
-  //ancienne plateforme
-  if ((match = /^\/weblextenso\/article\/afficher$/i.exec(path)) !== null) {
-    // http://www.lextenso.fr/weblextenso/article/afficher
-    if (param['id']) {
-      let id = param['id'];
-
-      if ((match = /([A-Z]+)([0-9]+)-([^-]+)-([^-]+)/.exec(id)) !== null) {
-        // http://www.lextenso.fr/weblextenso/article/afficher?id=CAPJA2013-1-002&d=3575203170535
-        result.title_id = match[1];
-        result.rtype    = 'ARTICLE';
-        result.mime     = 'HTML';
-      } else if ((match = /^(C[A-Z0-9]+)/.exec(id)) !== null) {
-        // http://www.lextenso.fr/weblextenso/article/afficher?id=C010IXCXCX2001X12X01X00177X053&origin=recherche;1&d=3575204329777
-        // confirm title_id value for jurisprudence
-        result.title_id = match[1];
-        result.rtype    = 'JURISPRUDENCE';
-        result.mime     = 'HTML';
-      } else {
-        console.log('unrecognized id : ' + id);
-      }
-      if (param['d']) {
-        result.unitid = param['d'];
-      }
-    }
-  }
-
   // nouvelle plateforme
-  else if ((match = /^\/numero_revue\/(([a-z-]+)\/[0-9]+\/[0-9]+\/[0-9,]+)$/i.exec(path)) !== null) {
-    //numero_revue/bulletin-joly-bourse/158/3/1456786800
-    //numero_revue/flash-defrenois/163/7/1455663600%2C1456095600
+  if ((match = /^\/revue(\/([A-Z]+)\/\d+\/\d+)$/i.exec(path)) !== null) {
+    //https://www.lextenso.fr/revue/BJB/2018/05
+    //https://www.lextenso.fr/revue/DFF/2018/39
     result.rtype    = 'TOC';
     result.mime     = 'HTML';
-    result.unitid   = match[1];
+    result.unitid   = match[1].replace(/\W/gi, '');
     result.title_id = match[2];
   }
 
-  else if ((match = /^\/lextenso\/ud\/urn:(([a-z-]+)[a-z0-9_-]+)$/i.exec(path)) !== null) {
-    // http://www.lextenso.fr/lextenso/ud/urn%3AEDCO2016023
-    // http://www.lextenso.fr/lextenso/ud/urn%3APA201602013
-    // http://www.lextenso.fr/lextenso/ud/urn%3ACONSTEXT000031256027
+  else if ((match = /^\/jurisprudence\/(([A-Z]+).+)$/i.exec(path)) !== null) {
+    //https://www.lextenso.fr/jurisprudence/CONSTEXT000031256027
+    //https://www.lextenso.fr/jurisprudence/CAPARIS-18042013-10_21459
+    result.rtype    = 'JURISPRUDENCE';
+    result.mime     = 'HTML';
+    result.unitid   = match[1].replace(/\W/gi, '');
+    result.title_id = match[2];
+  }
 
+  else if ((match = /^\/[a-z-]+\/(([A-Z]+).+)$/i.exec(path)) !== null) {
+    //https://www.lextenso.fr/lessentiel-droit-des-contrats/EDCO-116023-11602
+    //https://www.lextenso.fr/petites-affiches/PA199902103
+    //https://www.lextenso.fr/petites-affiches/PA201602202
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
-    result.unitid   = match[1];
-    result.title_id = match[2].replace(/-+$/, '');
-
-    if (result.title_id.toUpperCase().startsWith('CONSTEXT')) {
-      result.rtype = 'JURISPRUDENCE';
-    }
+    result.unitid   = match[1].replace(/\W/gi, '');
+    result.title_id = match[2];
   }
 
   return result;
