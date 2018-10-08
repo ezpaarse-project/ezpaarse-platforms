0c42458bc768b6b69d7e2cf70e4a837d543da9e5
Yannick Schurter <yannick.schurter@gmail.com>
2018-10-08 11:57:46 +0200

    add PressReader

---
+++ pr/parser.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform PressReader
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path   = parsedUrl.pathname;
+  let match;
+
+  if ((match = /^\/[a-z-]+\/(([^/]+)\/([0-9]{4})([0-9]{2})([0-9]{2}))(\/textview)?$/i.exec(path)) !== null) {
+    // /usa/the-washington-post/20180906
+    // /france/la-recherche/20171123/textview
+    result.rtype            = 'ISSUE';
+    result.mime             = 'MISC';
+    result.title_id         = match[2];
+    result.unitid           = match[1];
+    result.publication_date = `${match[3]}-${match[4]}-${match[5]}`;
+  }
+
+  return result;
+});
