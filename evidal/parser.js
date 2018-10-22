b9248b151ea4160f9433154615418126930bd4b0
eurruby <laurent.lhuillier@gmail.com>
2018-10-22 12:57:31 +0200

    update evidal parser
    new platform

--- evidal/parser.js
+++ evidal/parser.js
@@ -1,40 +1,61 @@
 #!/usr/bin/env node
 
-// ##EZPAARSE
-
 'use strict';
-var Parser = require('../.lib/parser.js');
+const Parser = require('../.lib/parser.js');
 
 /**
- * Identifie les consultations de la plateforme Evidal
+ * Recognizes the accesses to the platform Elsevier Elibrary
  * @param  {Object} parsedUrl an object representing the URL to analyze
  *                            main attributes: pathname, query, hostname
  * @param  {Object} ec        an object representing the EC whose URL is being analyzed
  * @return {Object} the result
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
-  var result = {};
-  var path   = parsedUrl.pathname;
-  var param  = parsedUrl.query || {};
+  let result = {};
+  let path   = parsedUrl.pathname;
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
-  if (/^\/(([a-zA-Z]+).html)$/.test(path)) {
-    //http://www.evidal.fr/showProduct.html?productId=5480
-    //http://www.evidal.fr/showReco.html?recoId=4048
-    result.rtype = 'ARTICLE';
-    result.mime  = 'HTML';
+  let match;
 
-    if (param.recoId) {
-      result.title_id = param.recoId;
-      result.unitid   = param.recoId;
-    } else if (param.productId) {
-      result.title_id = param.productId;
-      result.unitid   = param.productId;
-    }
+  if ((match = /^\/recherche/i.exec(path)) !== null) {
+    // https://evidal.vidal.fr/recherche.html?q=DOLIPRANE%20500%20MG%20G%C3%89L
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+
+  } else if ((match = /^\/medicament\/.+-(\d+)\.html$/i.exec(path)) !== null) {
+    // https://evidal.vidal.fr/medicament/doliprane_500_mg_gel-5480.html
+    result.rtype    = 'ABS';
+    result.mime     = 'HTML';
+    result.unitid   = match[1];
+
+  } else if ((match = /^\/medicament\/html\/(\d+)\/.+\.html$/i.exec(path)) !== null) {
+    // https://evidal.vidal.fr/medicament/html/5480/doliprane_500_mg_gel.html
+    result.rtype    = 'RECORD';
+    result.mime     = 'HTML';
+    result.unitid   = match[1];
+
+  } else if ((match = /^\/medicament\/pdf\.html/i.exec(path)) !== null) {
+    // https://evidal.vidal.fr/medicament/pdf.html?id=5480
+    result.rtype    = 'RECORD';
+    result.mime     = 'PDF';
+    result.unitid   = param.id;
+
+  } else if ((match = /^\/recos\/details\/(\d+)\/.+$/i.exec(path)) !== null) {
+    // https://evidal.vidal.fr/recos/details/2720/grippe_saisonniere/prise_en_charge
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'HTML';
+    result.unitid   = match[1];
+
+  } else if ((match = /^\/baseDocumentaire\/pdf\.html/i.exec(path)) !== null) {
+    // https://evidal.vidal.fr/baseDocumentaire/pdf.html?id=2720&permalink=grippe_saisonniere
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.unitid   = param.id;
+
   }
 
   return result;
 });
-
