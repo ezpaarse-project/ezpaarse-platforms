35b6280ece751a6e7da702cae6d1ebfd96fbb7be
root <root@ezpaarse.ezpaarse.labocleo.org>
2018-11-29 14:49:05 +0100

    OEJ : Remplacement de format par norecordurl dans le critère d'exclusion des urls

--- oej/parser.js
+++ oej/parser.js
@@ -20,7 +20,8 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let match;
 
   // URLs with "format=..." are just partial pages
-  if (param.format) {
+	// AD : norecordurl=1 ; format can be used
+  if (param.norecordurl) {
     return result;
   }
 
@@ -28,10 +29,11 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // http://socio.revues.org/pdf/1882
     // http://journals.openedition.org/crau/pdf/370
 
-    result.rtype    = 'ARTICLE';
+    result.rtype    = 'ARTICLEy';
     result.mime     = match[2].toUpperCase();
     result.title_id = match[1] ? match[1].substr(1) : host.split('.')[0];
     result.unitid   = `${result.title_id}/${match[3]}`;
+    result.lodelid   = `${match[3]}`;
     result.doi      = `${doiPrefix}/${result.title_id}.${match[3]}`;
 
   } else if ((match = /^(\/[a-z-]+)?\/([0-9]+)$/i.exec(path)) !== null) {
@@ -40,10 +42,11 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
     // if the size is less than 10ko, it's unlikely to be an article
     if (!fileSize || fileSize > 10000) {
-      result.rtype    = 'ARTICLE';
+      result.rtype    = 'ARTICLEx';
       result.mime     = 'HTML';
       result.title_id = match[1] ? match[1].substr(1) : host.split('.')[0];
       result.unitid   = `${result.title_id}/${match[2]}`;
+      result.lodelid   = `${match[2]}`;
       result.doi      = `${doiPrefix}/${result.title_id}.${match[2]}`;
     }
   }
