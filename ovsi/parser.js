e668c9d31e951e6c146f0645bf52881ba31b2dfb
Yannick Schurter <yannick.schurter@gmail.com>
2019-01-10 15:03:33 +0100

    update ovsi

--- ovsi/parser.js
+++ ovsi/parser.js
@@ -25,14 +25,14 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
       result.unitid = param.t0;
     }
 
-  } else if ((match = /^\/view\/([0-9]{2}\.[0-9]{4})\/([a-z]+)\/([0-9.]+)\/(([a-z]+)-([0-9]+)([a-z0-9-]+)?)$/.exec(path)) !== null) {
-    //view/10.1093/actrade/9780190458164.001.0001/actrade-9780190458164-chapter-1
+  } else if ((match = /^\/view\/(10\.[0-9]+\/([a-z]+\/[0-9.]+))\/[a-z0-9-]+?(-chapter-[0-9]+)?$/.exec(path)) !== null) {
+    // /view/10.1093/actrade/9780190458164.001.0001/actrade-9780190458164-chapter-1
     result.rtype  = 'TOC';
     result.mime   = 'HTML';
-    result.unitid = match[4];
-    result.doi    = match[1] + '/' + match[3];
+    result.unitid = match[2];
+    result.doi    = match[1];
 
-    if (match[7]) {
+    if (match[3]) {
       result.rtype = 'BOOK_SECTION';
     }
   }
