f5fcba9feeb415ce6729e3f7ae428eeb33debb7e
Yannick Schurter <yannick.schurter@gmail.com>
2019-02-11 14:17:04 +0100

    update heinonline

--- heinonline/parser.js
+++ heinonline/parser.js
@@ -1,9 +1,7 @@
 #!/usr/bin/env node
 
-// ##EZPAARSE
-
 'use strict';
-var Parser = require('../.lib/parser.js');
+const Parser = require('../.lib/parser.js');
 
 /**
  * Identifie les consultations de la plateforme HeinOnline
@@ -13,47 +11,46 @@ var Parser = require('../.lib/parser.js');
  * @return {Object} the result
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
-  var result = {};
-  var path   = parsedUrl.pathname;
-  var param  = parsedUrl.query || {};
+  const result = {};
+  const path   = parsedUrl.pathname;
+  const param  = parsedUrl.query || {};
+  let match;
 
-  if (/^\/HOL\/(P[^]*)/.test(path)) {
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/Page?handle=hein.journals/antil77&id=53
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/PageMulti?collection=journals&number_of_pages=1&handle=hein.journals/antil77&id=53
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/PageMulti?collection=journals&number_of_pages=2&handle=hein.journals/antil77&id=53
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/PageMulti?collection=journals&number_of_pages=3&handle=hein.journals/antil77&id=53
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/PageMulti?collection=journals&number_of_pages=4&handle=hein.journals/antil77&id=53
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/PageMulti?collection=journals&number_of_pages=5&handle=hein.journals/antil77&id=53
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/PageMulti?collection=journals&number_of_pages=6&handle=hein.journals/antil77&id=53
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/PageMulti?collection=journals&number_of_pages=7&handle=hein.journals/antil77&id=53
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/Print?collection=journals&handle=hein.journals/antil77&id=53
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
+  if ((match = /^\/HOL\/(Page|PageMulti|Print|Contents)$/i.exec(path)) !== null) {
+    if (match[1].toLowerCase() === 'contents') {
+      // /HOL/Contents?handle=hein.journals/antil77&id=1&size=2&index=&collection=journals
+      result.rtype = 'TOC';
+      result.mime  = 'HTML';
+    } else {
+      // /HOL/Page?handle=hein.journals/antil77&id=53
+      // /HOL/PageMulti?collection=journals&number_of_pages=4&handle=hein.journals/antil77&id=53
+      // /HOL/Print?collection=journals&handle=hein.journals/antil77&id=53
+      result.rtype = 'ARTICLE';
+      result.mime  = 'PDF';
+    }
+
     if (param.handle) {
-      result.title_id = param.handle.split('/')[1];
-      result.unitid = param.handle +'/'+param.id ;
+      result.unitid = `${param.handle}/${param.id}` ;
+
+      const titleIdMatch = /^[a-z0-9._-]+\/([a-z]+)[0-9]*$/i.exec(param.handle);
+      if (titleIdMatch) {
+        result.title_id = titleIdMatch[1];
+      }
       if (param.number_of_pages) {
-        result.unitid += '/'+ param.number_of_pages;
+        result.unitid += `/${param.number_of_pages}`;
       }
     }
-  } else if (/^\/HOL\/Index/.test(path)) {
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/Index?index=journals/antil&collection=usjournals
-    result.rtype    = 'TOC';
-    result.mime     = 'HTML';
+  } else if (/^\/HOL\/Index$/i.test(path)) {
+    // /HOL/Index?index=journals/antil&collection=usjournals
+    result.rtype = 'TOC';
+    result.mime  = 'HTML';
+
     if (param.index) {
-      result.title_id = param.index.split('/')[1];
-      result.unitid = param.index ;
-    }
-  } else if (/^\/HOL\/.*/.test(path)) {
-    // http://heinonline.org.faraway.u-paris10.fr/HOL/Contents?handle=hein.journals/antil77&id=1&size=2&index=&collection=journals
-    result.rtype    = 'TOC';
-    result.mime     = 'HTML';
-    if (param.handle) {
-      result.title_id = param.handle.split('/')[1];
-      result.unitid = param.handle +'/'+param.id ;
-      if (param.number_of_pages) {
-        result.unitid += '/'+ param.number_of_pages;
+      const titleIdMatch = /^[a-z0-9._-]+\/([a-z]+)[0-9]*$/i.exec(param.index);
+      if (titleIdMatch) {
+        result.title_id = titleIdMatch[1];
       }
+      result.unitid = param.index;
     }
   }
 
