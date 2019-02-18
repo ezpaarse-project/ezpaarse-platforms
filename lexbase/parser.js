47067b32aa5b60c3b7c76619a7b1d81cc5c3190a
peachaku <laurent.lhuillier@gmail.com>
2019-02-18 10:08:28 +0100

    deduplicate browsing

--- lexbase/parser.js
+++ lexbase/parser.js
@@ -13,32 +13,35 @@ const Parser = require('../.lib/parser.js');
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
   let match;
 
+if  (param.json) {
+
   if ((match = /^\/search\/reviews\/(\d+)$/i.exec(path)) !== null) {
-    // http://www.lexbase.fr/search/reviews/36632541
+    // http://www.lexbase.fr/search/reviews/36632541?json=1
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
     result.unitid   = match[1];
 
   } else if ((match = /^\/search\/sources\/(\d+)$/i.exec(path)) !== null) {
-    // http://www.lexbase.fr/search/sources/43129652
+    // http://www.lexbase.fr/search/sources/43129652?json=1
     result.rtype    = 'CODE_JURIDIQUE';
     result.mime     = 'HTML';
     result.unitid   = match[1];
 
   } else if ((match = /^\/search\/encyclopedia\/(\d+)$/i.exec(path)) !== null) {
-    // http://www.lexbase.fr/search/encyclopedia/36753798
+    // http://www.lexbase.fr/search/encyclopedia/36753798?json=1
     result.rtype    = 'ENCYCLOPAEDIA_ENTRY';
     result.mime     = 'HTML';
     result.unitid   = match[1];
   }
+
+}
 
   return result;
 });
