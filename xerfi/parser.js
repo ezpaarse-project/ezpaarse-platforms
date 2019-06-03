68c3c9231fc776a3486b8927911e8af307042d5d
Yannick Schurter <yannick.schurter@gmail.com>
2019-06-03 15:30:28 +0200

    update xerfi

--- xerfi/parser.js
+++ xerfi/parser.js
@@ -15,16 +15,34 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let path   = parsedUrl.pathname;
   let match;
 
-  if ((match = /^\/[a-z]+\/[a-z-]+\/([A-Za-z-]+)$/.exec(path)) !== null) {
-    //xerfifrance/extrait-video-report/Les-marches-du-luxe-et-la-consommation-collaborative
+  if ((match = /^\/xerfi[a-z-]+\/([a-z0-9-_]+)$/i.exec(path)) !== null) {
+    // /xerfi-canal-campus/Comprendre-lanalyse-SWOT_3746886
     result.rtype  = 'VIDEO';
     result.mime   = 'MISC';
     result.unitid = match[1];
-  } else if ((match = /^\/[A-Za-z_]+\/[a-z0-9]+\/pdf\/([A-Za-z0-9-]+).pdf$/.exec(path)) !== null) {
-    //Etudes_sectorielles_non_imprimablesE/secteur700doc/pdf/4DIS28-jyJAXyVY.pdf
+
+  } else if ((match = /^\/xerfi[a-z-]+\/extrait-video-report\/([a-z0-9-_]+)$/i.exec(path)) !== null) {
+    // /xerfifrance/extrait-video-report/Les-marches-du-luxe-et-la-consommation-collaborative
+    result.rtype  = 'VIDEO';
+    result.mime   = 'MISC';
+    result.unitid = match[1];
+
+  } else if ((match = /^\/STAMP\/PdfEcole\/([a-z0-9-_]+)\.pdf$/i.exec(path)) !== null) {
+    // /STAMP/PdfEcole/2246421-8ERH10-mUtslA3v.pdf
+    result.rtype  = 'ARTICLE';
+    result.mime   = 'PDF';
+    result.unitid = match[1];
+
+  } else if ((match = /^\/Etudes_sectorielles_non_imprimablesE\/[a-z0-9]+\/pdf\/([a-z0-9-]+).pdf$/i.exec(path)) !== null) {
+    // /Etudes_sectorielles_non_imprimablesE/secteur700doc/pdf/4DIS28-jyJAXyVY.pdf
     result.rtype  = 'REPORT';
     result.mime   = 'PDF';
     result.unitid = match[1];
+
+  } else if (/^\/rechercheretude\/[a-z0-9]+$/i.test(path)) {
+    // /rechercheretude/automobile
+    result.rtype = 'SEARCH';
+    result.mime  = 'HTML';
   }
 
   return result;
