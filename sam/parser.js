c9e3f3eb5ca808421e3307f2e00ad1850bc5ba7d
pseudom <dominique.lechaudel@gmail.com>
2019-08-23 16:10:50 +0200

    Update sam

--- sam/parser.js
+++ sam/parser.js
@@ -26,6 +26,18 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.mime  = 'HTML';
     result.title_id = match[2];
     result.unitid = match[3];
+  } else if ((match = /^\/handle\/([0-9]+\/[0-9]+)$/.exec(path)) !== null) {
+  // https://sam.ensam.eu/handle/10985/10188
+    result.rtype = 'REF';
+    result.mime  = 'HTML';
+    result.title_id = match[1];
+    result.unitid = match[1];
+  } else if ((match = /^\/handle\/([0-9]+\/[0-9]+)\/discover$/.exec(path)) !== null) {
+  //http://sam.ensam.eu/handle/10985/6657/discover?filter=Centre%2Bde%2BParis&filter_0=bf19da357d295cfccc70cc93e456d708&filter_1=f2b642db8e400dc3456e146eb9c8c8fc&filter_relational_operator=equals&filter_relational_operator_0=authority&filter_relational_operator_1=authority&filtertype=institution&filtertype_0=author&filtertype_1=author&locale-attribute=fr
+    result.rtype = 'QUERY';
+    result.mime  = 'HTML';
+    result.title_id = match[1];
+    result.unitid = match[1];
   }
   return result;
 });
