12a54590526d54bdeb05acdb99462c2efa7fa0e4
pseudom <dominique.lechaudel@gmail.com>
2019-08-26 16:51:12 +0200

    Add query to uoak

--- uoak/parser.js
+++ uoak/parser.js
@@ -26,13 +26,6 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
     result.title_id = match[1];
-
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
     result.unitid = match[1];
   } else if ((match = /^\/islandora\/object\/islandora:([0-9]+)\/datastream\/PDF\/view$/i.exec(path)) !== null) {
     // https://univoak.eu/islandora/object/islandora%3A64221/datastream/PDF/view
@@ -46,6 +39,12 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime     = 'HTML';
     result.title_id = match[1];
     result.unitid   = match[1];
+  } else if ((match = /^\/search-(authors|structures)$/i.exec(path)) !== null) {
+    // https://univoak.eu/search-authors
+    // https://univoak.eu/search-structures
+    result.rtype    = 'QUERY';
+    result.mime     = 'HTML';
+    result.title_id = match[1];
   }
 
   return result;
