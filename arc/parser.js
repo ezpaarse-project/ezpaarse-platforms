e0edfa6024386f002c5fddc83dbc0d6c8dcdfa05
Yannick Schurter <yannick.schurter@gmail.com>
2020-02-03 11:15:21 +0100

    update ARC

--- arc/parser.js
+++ arc/parser.js
@@ -15,14 +15,14 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let path   = parsedUrl.pathname;
   let match;
 
-  if ((match = /\/loi\/([a-zA-Z]+)$/.exec(path)) !== null) {
+  if ((match = /^\/loi\/([a-zA-Z]+)$/.exec(path)) !== null) {
     // http://arc.aiaa.org/loi/aiaaj
     result.rtype    = 'TOC';
     result.mime     = 'HTML';
     result.title_id = match[1];
     result.unitid   = match[1];
 
-  } else if ((match = /\/toc\/(([a-zA-Z]+)\/([0-9]+)\/([0-9]+))$/.exec(path)) !== null) {
+  } else if ((match = /^\/toc\/(([a-zA-Z]+)\/([0-9]+)\/([0-9]+))$/.exec(path)) !== null) {
     // http://arc.aiaa.org/toc/aiaaj/52/4
     result.rtype    = 'TOC';
     result.mime     = 'HTML';
@@ -31,28 +31,26 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.vol      = match[3];
     result.issue    = match[4];
 
-  } else if ((match = /\/doi\/(abs|full|pdf|pdfplus)\/([0-9.]+\/([^/.]+\.[^/]+))$/.exec(path)) !== null) {
+  } else if ((match = /^\/doi(\/abs|\/full|\/pdf|\/pdfplus)?\/([0-9.]+\/([^/.]+\.[^/]+))$/.exec(path)) !== null) {
     // http://arc.aiaa.org/doi/abs/10.2514/1.J052182
-    result.title_id = match[3];
-    result.unitid   = match[2];
-    result.doi      = match[2];
+    // https://arc.aiaa.org/doi/10.2514/1.J057846
+    // https://arc.aiaa.org/doi/full/10.2514/1.J057846
+    result.unitid = match[3];
+    result.doi    = match[2];
 
     switch (match[1]) {
-    case 'abs':
+    case '/abs':
       result.rtype = 'ABS';
       result.mime  = 'HTML';
       break;
-    case 'full':
-      result.rtype = 'ARTICLE';
-      result.mime  = 'HTML';
-      break;
-    case 'pdf':
+    case '/pdf':
+    case '/pdfplus':
       result.rtype = 'ARTICLE';
       result.mime  = 'PDF';
       break;
-    case 'pdfplus':
+    default:
       result.rtype = 'ARTICLE';
-      result.mime  = 'PDFPLUS';
+      result.mime  = 'HTML';
       break;
     }
   }
