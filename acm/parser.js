2af7ee578a398bd0b10675e4460d4293b7076263
Yannick Schurter <yannick.schurter@gmail.com>
2020-02-03 11:38:00 +0100

    update ACM

--- acm/parser.js
+++ acm/parser.js
@@ -9,20 +9,20 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
   let param  = parsedUrl.query || {};
   let match;
 
-  if ((match = /^\/([0-9.]+)\/([0-9]+)\/([0-9]+)\/[a-z0-9-]+\.pdf$/.exec(path)) !== null) {
+  if ((match = /^\/([0-9.]+)\/([0-9]+)\/([0-9]+)\/[a-z0-9-]+\.pdf$/i.exec(path)) !== null) {
     // http://delivery.acm.org/10.1145/2560000/2556270/a3-shi.pdf
     result.rtype = 'ARTICLE';
     result.mime  = 'PDF';
     result.doi   = match[1] + '/' + match[3];
 
-  } else if ((match = /^\/[0-9]+\/[0-9]+\/(([a-z]+)\/[a-z0-9]+)\.pdf$/.exec(path)) !== null) {
+  } else if ((match = /^\/[0-9]+\/[0-9]+\/(([a-z]+)\/[a-z0-9]+)\.pdf$/i.exec(path)) !== null) {
     // /1120000/1113378/fm/frontmatter.pdf
     result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
     result.title_id = match[2];
     result.unitid   = match[1];
 
-  } else if ((match = /^\/([a-z]+).cfm$/.exec(path)) !== null) {
+  } else if ((match = /^\/([a-z]+).cfm$/i.exec(path)) !== null) {
     // detail.cfm
 
     if (param.id) {
@@ -43,9 +43,24 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
       result.rtype = 'TOC';
       result.mime  = 'HTML';
     }
-  } else if ((match = /\/cert*$/.exec(path)) !== null) {
-    result.rtype = 'ARTICLE';
-    result.mime  = 'HTML';
+  } else if ((match = /^\/doi\/(abs|fullHtml|pdf)\/(10\.[0-9]+\/([a-z0-9]+))$/i.exec(path)) !== null) {
+    result.doi = match[2];
+    result.unitid = match[3];
+
+    switch (match[1].toLowerCase()) {
+    case 'abs':
+      result.rtype = 'ABS';
+      result.mime  = 'HTML';
+      break;
+    case 'fullhtml':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'HTML';
+      break;
+    case 'pdf':
+      result.rtype = 'ARTICLE';
+      result.mime  = 'PDF';
+      break;
+    }
   }
 
   return result;
