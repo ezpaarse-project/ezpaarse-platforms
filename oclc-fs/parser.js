bc1338a3265efc479f57abf6757c0242ce19cc17
Karen Coombs <coombsk@oclc.org>
2020-10-01 20:18:46 -0500

    added parsing OpenURLs into Firstsearch

--- oclc-fs/parser.js
+++ oclc-fs/parser.js
@@ -16,14 +16,35 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  //let param = parsedUrl.query || {};
+  let queryParam = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
   let match;
 
-  if ((match = /^\/WebZ\/([a-z]+)$/i.exec(path)) !== null) {
+  if ((match = /^\/WebZ\/FSOpenURL$/i.exec(path)) !== null) {
+    //http://firstsearch.oclc.org/WebZ/FSOpenURL?database=3&dbname=WorldCat&autho=someAutho&timestamp=23&encryption=318c78ca417f949f5ec3d71c80d79b6a&oclcnum=useuetkok&isbn=9781432933890&title=Cats&aulast=Ganeri&date=2009&sessionid=0&checksum=739b6a56c9ce004708090ba02c8c59f5
+    //http://firstsearch.oclc.org/WebZ/FSOpenURL?database=3&dbname=WorldCat&autho=someAutho&timestamp=1&encryption=6f4a453e7ceb71e016661caf8015fdd9&oclcnum=eeeotkttfq&isbn=9780062878342&title=Sapphire+Flames.&aulast=Andrews&date=2019&sessionid=0&checksum=5c6fc165a29b54629d9f285338eb04a3
+    result.rtype = 'OPENURL';
+    result.login = queryParam.autho;
+    result.db_id = queryParam.dbname;
+    if (queryParam.issn) {
+      //http://firstsearch.oclc.org/WebZ/FSOpenURL?database=3&dbname=WorldCat&autho=someAutho&timestamp=1&encryption=bc90fbbb8e8f0b86003707fa3a3a3e5f&oclcnum=ouxxhqtqx&issn=0737-8831&atitle=Lessons+learned+from+analyzing+library+database+usage+data&aulast=Coombs&volume=23&issue=4&date=2005&sessionid=0&checksum=6418bc3c07053f9036af5d54d6557264
+      result.publication_Title = queryParam.atitle;
+      result.publication_date = queryParam.date;
+      result.title_id = queryParam.issn;
+      result.vol = queryParam.volume;
+      result.num = queryParam.issue;
+    } else {
+      result.publication_Title = queryParam.title;
+      result.publication_date = queryParam.date;
+      result.title_id = queryParam.isbn;
+      result.unitid = queryParam.isbn;
+    }
+
+    
+  } else if ((match = /^\/WebZ\/([a-z]+)$/i.exec(path)) !== null) {
     const category = match[1].toLowerCase();
     const paramString = parsedUrl.search.substring(1);
 
@@ -121,10 +142,11 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
       break;
     }
     }
-    result.platform_name = 'OCLC FirstSearch';
     result.login = param.sessionid;
-    result.mime  = 'HTML';
   }
+  
+  result.platform_name = 'OCLC FirstSearch';
+  result.mime  = 'HTML';
 
 
   return result;
