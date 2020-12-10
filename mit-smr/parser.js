23bf5c49273be6b2a65d819d361a25eb87336b54
Karen Coombs <coombsk@oclc.org>
2020-12-10 16:32:35 -0600

    basic parser for MIT Sloane Management Review

---
+++ mit-smr/parser.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform MIT-Sloan Management Review
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path   = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+  // let param = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  let match;
+
+  if ((match = /^\/article\/(.+)\//i.exec(path)) !== null) {
+    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
+    // https://sloanreview.mit.edu:443/article/youre-going-digital-now-what/?og=Winter+2020+Issue+Tiled
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'HTML';
+    result.title_id = match[1];
+    result.unitid   = match[1];
+
+    /**
+     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
+     * it described the most fine-grained of what's being accessed by the user
+     * it can be a DOI, an internal identifier or a part of the accessed URL
+     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
+     */
+    //result.unitid = match[2];
+
+  } else if ((match = /^\/(topic\/.+)\/$/i.exec(path)) !== null) {
+    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
+    result.rtype    = 'TOC';
+    result.mime     = 'HTML';
+    result.title_id = match[1];
+    result.unitid   = match[1];
+
+  } else if ((match = /^\/(issue\/.+)\/$/i.exec(path)) !== null) {
+    result.rtype    = 'TOC';
+    result.mime     = 'HTML';
+    result.title_id = match[1];
+    result.unitid   = match[1];
+  }
+
+  return result;
+});
