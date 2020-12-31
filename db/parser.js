a24d0ee83d36bc7c7072e31189348be03d49050b
Karen Coombs <coombsk@oclc.org>
2020-12-31 13:28:23 -0600

    basic parser for Dieet Behandelingsrichtlijnen

---
+++ db/parser.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform Dieet Behandelingsrichtlijnen
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path   = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+  // let param = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  let match;
+
+  if ((match = /^\/richtlijnen\/Zoek4.html$/i.exec(path)) !== null) {
+    // https://www.dieetbehandelingsrichtlijnen.nl/richtlijnen/Zoek4.html?tekst=HDL&Zoek=zoeken
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+
+  } else if ((match = /^\/richtlijnen\/([0-9a-z_]+).html$/i.exec(path)) !== null) {
+    // https://www.dieetbehandelingsrichtlijnen.nl/richtlijnen/09HK_overgewicht_en_obesitas_1.html
+    // https://www.dieetbehandelingsrichtlijnen.nl/richtlijnen/01ZK__para_medische_gegevens.html
+    result.rtype    = 'RECORD';
+    result.mime     = 'HTML';
+    result.unitid = match[1];
+  } else if ((match = /^\/richtlijnen\/([0-9a-z_]+)\.pdf$/i.exec(path)) !== null) {
+    // https://www.dieetbehandelingsrichtlijnen.nl/richtlijnen/09HK_overgewicht_en_obesitas_1.pdf
+    result.rtype    = 'RECORD';
+    result.mime     = 'PDF';
+    result.unitid = match[1];
+  }
+
+  return result;
+});
