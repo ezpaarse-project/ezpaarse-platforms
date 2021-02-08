e758709cb9393537664893987cf831bd823a0b9f
wilmouths <wilmouthsteven@gmail.com>
2021-02-08 11:37:06 +0100

    feat: adding parser

---
+++ docteur-imago/parser.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform Docteur Imago
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path   = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+  // let param = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  let match;
+
+  if ((match = /^\/(actualites|entre-nous|grand-angle|management|formation)(\/[a-z0-9-]+)?\/([a-z0-9-]+)\/?$/i.exec(path)) !== null) {
+    // /entre-nous/documents/grilles-indiciaires-manipulateurs-de-categorie-a/
+    // /grand-angle/lesprit-radiologie-de-beaujon/
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'HTML';
+    result.unitid   = match[3];
+  }
+
+  return result;
+});
