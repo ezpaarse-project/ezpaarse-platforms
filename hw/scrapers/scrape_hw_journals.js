fb4386bf52cf704d33e210743de3ce3089411c63
Yannick Schurter <yannick.schurter@gmail.com>
2021-02-08 16:33:26 +0100

    update highwire scraper

--- hw/scrapers/scrape_hw_journals.js
+++ hw/scrapers/scrape_hw_journals.js
@@ -3,136 +3,63 @@
 // Write the progression in stderr
 // Usage : ./scrape_hw_journals_from_xsl.js
 
-/*jslint maxlen: 180*/
-
 'use strict';
 
-const request = require('request');
-const cheerio = require('cheerio');
-const URL     = require('url');
+const { lib } = require('../../.lib/utils.js');
+const URL     = lib('url');
+const path    = lib('path');
+const request = lib('request');
+const cheerio = lib('cheerio');
+const KBart   = lib('./kbart');
 
-const PkbRows = require('../../.lib/pkbrows.js');
-const pkb     = new PkbRows('hw');
-
-pkb.setKbartName();
-
-
-console.error('Scraping GSW journals');
-scrapeGSW(err => {
-  if (err) { throw err; }
-
-  console.error('Scraping Highwire AtoZ');
-  scrapeAtoZ(err => {
-    if (err) { throw err; }
-
-    pkb.writeKbart();
-    console.error(`Kbart file generated. Path : ${pkb.kbartFileName}`);
-  });
+const kbart = new KBart({
+  directory: path.resolve(__dirname, '../pkb'),
+  provider: 'hw'
 });
 
-function scrapeGSW(callback) {
-  let gswJournals = 'http://www.geoscienceworld.org/site/misc/journals_glance.xhtml';
+console.error('Scraping Highwire AtoZ');
+let atozJournals = 'http://portal.highwire.org/lists/allsites.dtl?view=by+publisher';
 
-  request(gswJournals, (err, res, body) => {
-    if (err) { return callback(err); }
+request(atozJournals, (err, res, body) => {
+  if (err) {
+    if (err) {
+      console.error(`Could not load ${atozJournals}`);
+      process.exit(1);
+    }
+  }
 
-    let columns;
-    let $ = cheerio.load(body);
+  let $ = cheerio.load(body);
 
-    $('table tr').each((index, element) => {
-      let tds = $(element).children();
+  let currentPublisher;
 
-      if (columns) {
-        let journal = pkb.initRow({});
+  $('#subpage_content table tr').each((index, element) => {
 
-        tds.each((i, e) => {
-          if (columns[i]) { journal[columns[i]] = $(e).text(); }
-        });
+    let publisherNode = $(element).find('td[align="LEFT"] > strong > font');
 
-        if (journal.print_identifier) {
-          let match = /(([a-z0-9-]+)\s*([a-z0-9-]+))/i.exec(journal.print_identifier);
-          if (match) {
-            journal.print_identifier  = match[1];
-            journal.online_identifier = match[2];
-          }
-        }
+    if (publisherNode.length) {
+      return currentPublisher = publisherNode.text();
+    }
 
-        if (!journal.title_url || !journal.print_identifier) { return; }
+    let journalNode = $(element).find('td:nth-child(2) > a[href]');
 
-        journal.title_id = URL.parse(journal.title_url).host;
-        journal['pkb-piddomain'] = journal.title_id;
+    if (journalNode.length !== 1) { return; }
 
-        return pkb.addRow(journal);
-      }
+    let href = journalNode.attr('href');
+    if (!href) { return; }
+    let hostname = URL.parse(href).host;
 
-      // if the first cell contains "journal title", it's the header row
-      if (tds.first().text().toLowerCase().includes('journal title')) {
-        columns = [];
-        tds.each((i, e) => {
-          switch ($(e).text().toLowerCase()) {
-          case 'journal title':
-            columns.push('publication_title');
-            break;
-          case 'publisher':
-            columns.push('publisher_name');
-            break;
-          case 'print issn (online issn)':
-            columns.push('print_identifier');
-            break;
-          case 'url':
-            columns.push('title_url');
-            break;
-          default:
-            columns.push(null);
-          }
-        });
-      }
+    const result = kbart.add({
+      'title_url': href,
+      'title_id': hostname,
+      'publisher_name': currentPublisher,
+      'pkb-piddomain': hostname,
+      'publication_title': journalNode.text()
     });
 
-    callback();
+    if (result instanceof Error) {
+      console.error(`Error: ${result}`);
+    }
   });
-}
 
-function scrapeAtoZ(callback) {
-  // let atozJournals = 'http://highwire.stanford.edu/lists/allsites.dtl';
-  let atozJournals = 'http://highwire.stanford.edu/lists/allsites.dtl?view=by+publisher';
-
-  request(atozJournals, (err, res, body) => {
-    if (err) { return callback(err); }
-
-    let $ = cheerio.load(body);
-
-    let currentPublisher;
-
-    $('#subpage_content table tr').each((index, element) => {
-
-      let publisherNode = $(element).find('td[align="LEFT"] > strong > font');
-
-      if (publisherNode.length) {
-        return currentPublisher = publisherNode.text();
-      }
-
-      let journalNode = $(element).find('td:nth-child(2) > a[href]');
-
-      if (journalNode.length !== 1) { return; }
-
-      let href = journalNode.attr('href');
-      if (!href) { return; }
-      let hostname = URL.parse(href).host;
-
-      if (!hostname || pkb.rowsMap.hasOwnProperty(hostname)) { return; }
-
-      let journal = pkb.initRow({
-        'title_url': href,
-        'title_id': hostname,
-        'publisher_name': currentPublisher,
-        'pkb-piddomain': hostname,
-        'publication_title': journalNode.text()
-      });
-
-      pkb.addRow(journal);
-    });
-
-    callback();
-  });
-}
+  kbart.save().then(() => kbart.summarize());
+});
