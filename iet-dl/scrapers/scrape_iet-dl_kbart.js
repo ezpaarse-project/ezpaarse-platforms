c746ff69f0a41027195b3e8658a80bd5dbbcdd77
Yannick Schurter <yannick.schurter@gmail.com>
2021-03-16 18:08:27 +0100

    fix: remove double to single quote transforming

--- iet-dl/scrapers/scrape_iet-dl_kbart.js
+++ iet-dl/scrapers/scrape_iet-dl_kbart.js
@@ -2,29 +2,26 @@
 
 // As of 2014/09/20, the KBART files can be found at http://digital-library.theiet.org/about/librarians
 
-
 'use strict';
-var request   = require('request');
-var fs        = require('fs');
-var path      = require('path');
-var Transform = require('stream').Transform;
-//var util      = require('util');
+const request = require('request');
+const fs      = require('fs');
+const path    = require('path');
 
-var baseURL = 'http://digital-library.theiet.org/openurl/kbart/';
+const baseURL = 'http://digital-library.theiet.org/openurl/kbart/';
 
-var kbartFiles = [
+const kbartFiles = [
   'journals',
   'books',
 ];
 
 (function download() {
-  var packageName = kbartFiles.pop();
+  const packageName = kbartFiles.pop();
   if (!packageName) { return; }
 
-  var kbartUrl = baseURL + packageName;
+  const kbartUrl = baseURL + packageName;
   console.log('Downloading : %s', kbartUrl);
 
-  var req = request.get(kbartUrl);
+  const req = request.get(kbartUrl);
 
   req.on('error', function (err) {
     console.error('Failed to download %s (err: %s)', kbartUrl, err);
@@ -32,19 +29,19 @@ var kbartFiles = [
   });
 
   req.on('response', function (res) {
-    var disposition = res.headers['content-disposition'];
-    var match       = /filename="?(.*?)"?$/i.exec(disposition);
-    var filename;
+    const disposition = res.headers['content-disposition'];
+    const match = /filename="?(.*?)"?$/i.exec(disposition);
+    let filename;
 
     if (match) {
       filename = match[1];
     } else {
       console.error('Filename not found in the headers, generating one with the current date');
 
-      var now   = new Date();
-      var year  = now.getFullYear().toString();
-      var month = now.getMonth().toString();
-      var day   = now.getDay().toString();
+      const now = new Date();
+      let year  = now.getFullYear().toString();
+      let month = now.getMonth().toString();
+      let day   = now.getDay().toString();
 
       if (day.length === 1) { day = '0' + day; }
       if (month.length === 1) { month = '0' + month; }
@@ -52,8 +49,7 @@ var kbartFiles = [
       filename = packageName + '_' + year + '-' + month + '-' + day + '.txt';
     }
 
-    req.pipe(new EscapeStream())
-      .pipe(fs.createWriteStream(path.join(__dirname, '../pkb/', filename)))
+    req.pipe(fs.createWriteStream(path.join(__dirname, '../pkb/', filename)))
       .on('finish', download)
       .on('error', function (err) {
         console.error('Failed to download %s (err: %s)', kbartUrl, err);
@@ -61,18 +57,3 @@ var kbartFiles = [
       });
   });
 })();
-
-
-/**
- * A stream that change double quotes into single
- * Prevents the csv parser from crashing
- */
-function EscapeStream(options) {
-  Transform.call(this, options);
-}
-require('util').inherits(EscapeStream, Transform);
-
-EscapeStream.prototype._transform = function (chunk, encoding, callback) {
-  this.push(chunk.toString().replace(/"/g, '\''));
-  callback();
-};
