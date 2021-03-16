d9aa996069765d9170d49bef7a66b085638f3700
Yannick Schurter <yannick.schurter@gmail.com>
2021-03-16 13:56:08 +0100

    fix brill scraper returning wrong title_id

--- brill/scrapers/scrape_brill_from_kb+.js
+++ brill/scrapers/scrape_brill_from_kb+.js
@@ -6,4 +6,11 @@
 
 var PkbKbp = require('../../.lib/parsXmlKb+.js');
 
-PkbKbp.generatePkbKbp(391, 'brill');
+PkbKbp.generatePkbKbp(391, 'brill', null, (row) => {
+  if (row.title_url) {
+    const match = /\/journals\/([a-z]+)\//i.exec(row.title_url);
+    if (match) { row.title_id = match[1]; }
+  }
+
+  return row;
+});
