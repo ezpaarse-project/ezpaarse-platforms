00c0f7822e20f8f5c65c6429a246065ba98c0961
Yannick Schurter <yannick.schurter@gmail.com>
2021-03-17 14:13:16 +0100

    mal: extract title_id from title_url

--- mal/scrapers/scrape_mal_from_kb+.js
+++ mal/scrapers/scrape_mal_from_kb+.js
@@ -6,4 +6,17 @@
 
 var PkbKbp = require('../../.lib/parsXmlKb+.js');
 
-PkbKbp.generatePkbKbp(960, 'mal');
+PkbKbp.generatePkbKbp(960, 'mal', null, (row) => {
+  if (row.title_url) {
+    const match = /https?:\/\/[\w.-]+\/([a-z0-9]+)(?:\/([a-z0-9]+))?\/?$/i.exec(row.title_url);
+    if (match) {
+      if (match[2]) {
+        row.title_id = match[2];
+      } else if (match[1].toLowerCase() !== 'loi') {
+        row.title_id = match[1];
+      }
+    }
+  }
+
+  return row;
+});
