2c40c434c6010d11fa8ba38edd7a83c539106929
Yannick Schurter <yannick.schurter@gmail.com>
2021-03-17 13:50:00 +0100

    ios: extract title_id from title_url

--- ios/scrapers/scrape_ios_from_kb+.js
+++ ios/scrapers/scrape_ios_from_kb+.js
@@ -6,4 +6,11 @@
 
 var PkbKbp = require('../../.lib/parsXmlKb+.js');
 
-PkbKbp.generatePkbKbp(99, 'ios');
+PkbKbp.generatePkbKbp(99, 'ios', null, (row) => {
+  if (row.title_url) {
+    const match = /\/journals\/([a-z-]+)\/?$/i.exec(row.title_url);
+    if (match) { row.title_id = match[1]; }
+  }
+
+  return row;
+});
