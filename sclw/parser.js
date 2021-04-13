e73e869211c366b058873e359dc99e493e7972d1
Yannick Schurter <yannick.schurter@gmail.com>
2021-04-13 16:38:01 +0200

    sclw: add publication_date

--- sclw/parser.js
+++ sclw/parser.js
@@ -16,11 +16,13 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let param  = parsedUrl.query || {};
   let match;
 
-  if ((match = /^\/news\/[0-9]+\/[0-9]+\/[0-9]+\/([a-z0-9-]+)\/?$/i.exec(path)) !== null) {
+  if ((match = /^\/news\/([0-9]+)\/([0-9]+)\/([0-9]+)\/([a-z0-9-]+)\/?$/i.exec(path)) !== null) {
     //https://sclawyersweekly.com/news/2021/03/16/roof-worker-who-fell-through-skylight-settles-claims-for-8-5m/
     result.rtype  = 'ARTICLE';
     result.mime   = 'HTML';
-    result.unitid = match[1];
+    result.unitid = match[4];
+
+    result.publication_date = `${match[1]}-${match[2]}-${match[3]}`;
 
   } else if (/^\/$/i.test(path) && param.s) {
     // https://sclawyersweekly.com/?s=Car+Crash
