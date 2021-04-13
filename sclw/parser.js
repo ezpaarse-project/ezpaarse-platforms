c45c2f7771ae1666579c645f952ea398babd5d00
Yannick Schurter <yannick.schurter@gmail.com>
2021-04-13 16:34:43 +0200

    sclw: add unitid

--- sclw/parser.js
+++ sclw/parser.js
@@ -13,20 +13,19 @@ const Parser = require('../.lib/parser.js');
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  let param = parsedUrl.query || {};
+  let param  = parsedUrl.query || {};
+  let match;
 
-  // use console.error for debuging
-  // console.error(parsedUrl);
-
-  if (/^\/news\/[0-9]+\/[0-9]+\/[0-9]+\/([a-z0-9-]+)\/$/i.test(path)) {
+  if ((match = /^\/news\/[0-9]+\/[0-9]+\/[0-9]+\/([a-z0-9-]+)\/?$/i.exec(path)) !== null) {
     //https://sclawyersweekly.com/news/2021/03/16/roof-worker-who-fell-through-skylight-settles-claims-for-8-5m/
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
+    result.rtype  = 'ARTICLE';
+    result.mime   = 'HTML';
+    result.unitid = match[1];
+
   } else if (/^\/$/i.test(path) && param.s) {
     // https://sclawyersweekly.com/?s=Car+Crash
-    result.rtype    = 'SEARCH';
-    result.mime     = 'HTML';
+    result.rtype = 'SEARCH';
+    result.mime  = 'HTML';
   }
 
   return result;
