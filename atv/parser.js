909d5705c725ffbaab5297963255d377be833d92
Yannick Schurter <yannick.schurter@gmail.com>
2021-04-14 11:21:12 +0200

    atv: add unitid

--- atv/parser.js
+++ atv/parser.js
@@ -12,44 +12,38 @@ const Parser = require('../.lib/parser.js');
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
-  let path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
-
-  // use console.error for debuging
-  // console.error(parsedUrl);
-
-  let hash;
-
-  if (parsedUrl.hash) {
-    hash = parsedUrl.hash.replace('#', '');
-  } else {
-    hash = '';
-  }
+  let path = parsedUrl.pathname;
+  let hash = (parsedUrl.hash || '').replace('#', '');
+  let match;
 
   if (/^\/search$/i.test(path)) {
     // https://www.anatomy.tv/search
     result.rtype    = 'SEARCH';
     result.mime     = 'HTML';
 
-
   } else if (/^\/anatomytv\/html5ui_2018\/$/i.test(path)) {
-    result.mime     = 'HTML';
-    if (/^\/product\/har_hand_2014\/type\/Views\/id\/([0-9]+)\/layer\/([0-9]+)\/angle\/([0-9]+)\/structureID\/([0-9]+)$/i.test(hash)) {
+    result.mime = 'HTML';
+
+    if ((match = /^\/product\/har_hand_2014\/type\/Views\/id\/([0-9]+)\/layer\/([0-9]+)\/angle\/([0-9]+)\/structureID\/([0-9]+)$/i.exec(hash)) !== null) {
       // https://www.anatomy.tv/anatomytv/html5ui_2018/#/product/har_hand_2014/type/Views/id/39255/layer/0/angle/0/structureID/275504
-      result.rtype    = 'FIGURE';
-    } else if (/^\/product\/har_hand_2014\/type\/MRI%20Views\/id\/([0-9]+)\/layer\/([0-9]+)\/angle\/([0-9]+)\/structureID\/([0-9]+)$/i.test(hash)) {
-      // https://www.anatomy.tv/anatomytv/html5ui_2018/#/product/har_hand_2014/type/MRI Views/id/39296/layer/0/angle/0/structureID/263184      
-      result.rtype    = 'IMAGE';
-    } else if (/^\/product\/har_hand_2014\/type\/Slides\/id\/([0-9]+)\/structureID\/-([0-9]+)$/i.test(hash)) {
-      // https://www.anatomy.tv/anatomytv/html5ui_2018/#/product/har_hand_2014/type/Slides/id/7888532/structureID/-1      
-      result.rtype    = 'SUPPL';
-    } else if (/^\/product\/har_hand_2014\/type\/Movies\/id\/([0-9]+)$/i.test(hash)) {
-      // https://www.anatomy.tv/anatomytv/html5ui_2018/#/product/har_hand_2014/type/Movies/id/7888394      
-      result.rtype    = 'VIDEO';
-    } else if (/^\/product\/har_hand_2014\/type\/Text%20articles\/id\/([0-9]+)\/layer\/([0-9]+)\/angle\/([0-9]+)\/structureID\/([0-9]+)$/i.test(hash)) {
-      // https://www.anatomy.tv/anatomytv/html5ui_2018/#/product/har_hand_2014/type/Text articles/id/39256/layer/3/angle/8/structureID/1835224      
-      result.rtype    = 'ARTICLE';
+      result.rtype  = 'FIGURE';
+      result.unitid = match[1];
+    } else if ((match = /^\/product\/har_hand_2014\/type\/MRI%20Views\/id\/([0-9]+)\/layer\/([0-9]+)\/angle\/([0-9]+)\/structureID\/([0-9]+)$/i.exec(hash)) !== null) {
+      // https://www.anatomy.tv/anatomytv/html5ui_2018/#/product/har_hand_2014/type/MRI Views/id/39296/layer/0/angle/0/structureID/263184
+      result.rtype  = 'IMAGE';
+      result.unitid = match[1];
+    } else if ((match = /^\/product\/har_hand_2014\/type\/Slides\/id\/([0-9]+)\/structureID\/-([0-9]+)$/i.exec(hash)) !== null) {
+      // https://www.anatomy.tv/anatomytv/html5ui_2018/#/product/har_hand_2014/type/Slides/id/7888532/structureID/-1
+      result.rtype  = 'SUPPL';
+      result.unitid = match[1];
+    } else if ((match = /^\/product\/har_hand_2014\/type\/Movies\/id\/([0-9]+)$/i.exec(hash)) !== null) {
+      // https://www.anatomy.tv/anatomytv/html5ui_2018/#/product/har_hand_2014/type/Movies/id/7888394
+      result.rtype  = 'VIDEO';
+      result.unitid = match[1];
+    } else if ((match = /^\/product\/har_hand_2014\/type\/Text%20articles\/id\/([0-9]+)\/layer\/([0-9]+)\/angle\/([0-9]+)\/structureID\/([0-9]+)$/i.exec(hash)) !== null) {
+      // https://www.anatomy.tv/anatomytv/html5ui_2018/#/product/har_hand_2014/type/Text articles/id/39256/layer/3/angle/8/structureID/1835224
+      result.rtype  = 'ARTICLE';
+      result.unitid = match[1];
     }
   }
 
