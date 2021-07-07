6c17037ae40f57b7c8c92f0dc7c63c58d2bb0975
felixleo22 <leofelixpro22@gmail.com>
2021-07-07 15:06:58 +0200

    je: create parser

---
+++ je/parser.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform JurisEdit
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path   = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+
+  let param = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  let match;
+  let matchQuery;
+
+  if ((match = /^\/membres\/files\/resultats.php$/i.exec(path)) !== null) {
+    // http://www.jurisedit.com.proxy.bnl.lu/membres/files/resultats.php?base=jurissoc&file=L270516_RCS.htm
+    // http://www.jurisedit.com.proxy.bnl.lu/membres/files/resultats.php?base=juriscomintegral&file=20210506-CAS-2020-00080.pdf
+    result.rtype    = 'JURISPRUDENCE';
+
+    if ((matchQuery = /^([a-z0-9-_]+).([a-z]+)/i.exec(param.file)) !== null) {
+      result.unitid = matchQuery[1];
+      if (matchQuery[2] === 'htm') {
+        result.mime = 'HTML';
+      }
+      if (matchQuery[2] === 'pdf') {
+        result.mime = 'PDF';
+      }
+    }
+  }
+
+  return result;
+});
