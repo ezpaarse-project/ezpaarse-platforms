2d1b7bf4b79c40871091d3caf97395c057c46e08
Yannick Schurter <yannick.schurter@gmail.com>
2021-07-07 16:21:58 +0200

    Update parser.js

--- pe/parser.js
+++ pe/parser.js
@@ -56,60 +56,58 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.title_id = match[1];
     result.vol      = match[2];
     result.issue    = match[3];
-    if (match[4] == '') {
-      result.mime     = 'MISC';
-      result.rtype    = 'TOC';
-    }
-    else {
-      result.mime     = 'HTML';
-      result.rtype    = 'ABS';
+    
+    if (match[4] === '') {
+      result.mime  = 'MISC';
+      result.rtype = 'TOC';
+    } else {
+      result.mime  = 'HTML';
+      result.rtype = 'ABS';
       result.publication_title = match[5];
+      
       if (match[6].startsWith('10.')) {
         result.doi = match[6];
       }
     }
-  }
-  else if ((match = /^\/(journalArticle|ebook|accountAjax)\/Download$/i.exec(path)) !== null) {
+  } else if ((match = /^\/(journalArticle|ebook|accountAjax)\/Download$/i.exec(path)) !== null) {
     // PDF Download
     // https://projecteuclid.org/journalArticle/Download?urlid=10.1215%2F00294527-3346590
-    var urlid = ('urlid' in param) ? param.urlid : param.URLId;
+    const urlid = param.urlid || param.URLId;
     result.mime  = 'PDF';
-    result.rtype = (match[1] === 'journalArticle' | param.downloadType === 'journal article') ? 'ARTICLE' : 'BOOK_CHAPTER';
-    if (param.isFullBook == 'True') {
+    result.rtype = (match[1] === 'journalArticle' || param.downloadType === 'journal article') ? 'ARTICLE' : 'BOOK_CHAPTER';
+    
+    if (param.isFullBook === 'True') {
       result.rtype = 'BOOK';
     }
-    if (urlid.startsWith('10.')) {
+    if (urlid && urlid.startsWith('10.')) {
       result.doi = urlid;
-    }
-    else {
+    } else {
       result.title_id = urlid;
     }
-  }
-  else if ((match = /^\/journals\/([\w-]+)\/current$/i.exec(path)) !== null) {
+  } else if ((match = /^\/journals\/([\w-]+)\/current$/i.exec(path)) !== null) {
     // Current issue
     // https://projecteuclid.org/journals/notre-dame-journal-of-formal-logic/current
     result.rtype    = 'TOC';
     result.mime     = 'MISC';
     result.title_id = match[1];
-  }
-  else if ((match = /^\/(ebooks|proceedings)\/([a-z-]+)\/([\w-]+)\/(toc|Chapter\/([\w-]+))\/([\S]+)$/i.exec(path)) !== null) {
+    
+  } else if ((match = /^\/(ebooks|proceedings)\/([a-z-]+)\/([\w-]+)\/(toc|Chapter\/([\w-]+))\/([\S]+)$/i.exec(path)) !== null) {
     // https://projecteuclid.org/ebooks/institute-of-mathematical-statistics-lecture-notes-monograph-series/Optimality/toc/10.1214/09-LNMS57
     // https://projecteuclid.org/proceedings/advanced-studies-in-pure-mathematics/development-of-iwasawa-theory-the-centennial-of-k-iwasawa-s-birth/toc/10.2969/aspm/08610000
     result.publication_title = match[3];
-    if (match[4] == 'toc') {
-      result.rtype           = 'TOC';
-      result.mime            = 'MISC';
-    }
-    else {
-      result.rtype           = 'ABS';
-      result.mime            = 'HTML';
+    if (match[4] === 'toc') {
+      result.rtype = 'TOC';
+      result.mime  = 'MISC';
+    } else {
+      result.rtype = 'ABS';
+      result.mime  = 'HTML';
     }
     if (match[6].startsWith('10.')) {
       result.doi = match[6];
-    }
-    else {
+    } else {
       result.title_id = match[6];
     }
   }
+  
   return result;
 });
