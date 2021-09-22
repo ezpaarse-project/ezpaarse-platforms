8c23693cb152ebece534e8a786037416cb221ed7
Yannick Schurter <yannick.schurter@gmail.com>
2021-09-22 14:55:55 +0200

    bdsl: add parser

---
+++ bdsl/parser.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform Bibliographie Der Deutschen Sprach- Und Literaturwissenschaft  (BDSL)
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path = parsedUrl.pathname;
+  let query = parsedUrl.query || {};
+
+  if (/^\/[a-z-]+\/suche\/Titelaufnahme\.xml$/i.test(path) && query.Publikation_ID) {
+    // /BDSL-DB/suche/Titelaufnahme.xml?vid=439F755D-2AF2-4283-AC79-F7B66E73678D&erg=0&Anzeige=10&Sprache=de&contenttype=text/html&Skript=titelaufnahme&Publikation_ID=468779787
+    result.rtype  = 'REF';
+    result.mime   = 'HTML';
+    result.unitid = query.Publikation_ID;
+  }
+
+  return result;
+});
