a4f3ef75a5efa8b8b915a1036ddeb0187bd82f43
wilmouths <wilmouthsteven@gmail.com>
2021-09-23 13:13:42 +0200

    update je parser

--- je/parser.js
+++ je/parser.js
@@ -12,9 +12,7 @@ const Parser = require('../.lib/parser.js');
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
-  let path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-
+  let path = parsedUrl.pathname;
   let param = parsedUrl.query || {};
 
   // use console.error for debuging
@@ -23,8 +21,8 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let match;
 
   if (/^\/membres\/files\/resultats.php$/i.test(path)) {
-    // http://www.jurisedit.com.proxy.bnl.lu/membres/files/resultats.php?base=jurissoc&file=L270516_RCS.htm
-    // http://www.jurisedit.com.proxy.bnl.lu/membres/files/resultats.php?base=juriscomintegral&file=20210506-CAS-2020-00080.pdf
+    // /membres/files/resultats.php?base=jurissoc&file=L270516_RCS.htm
+    // /membres/files/resultats.php?base=juriscomintegral&file=20210506-CAS-2020-00080.pdf
     result.rtype    = 'JURISPRUDENCE';
 
     if ((match = /^([a-z0-9_-]+)\.([a-z]+)$/i.exec(param.file)) !== null) {
@@ -36,6 +34,32 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
         result.mime = 'PDF';
       }
     }
+  } else if (/^\/membres\/jurisindex.php$/i.test(path)) {
+    // /jurisindex.php?action=watch&cate=15&file=16.pdf&page=3
+    result.rtype = 'ARTICLE';
+    result.mime = 'PDF';
+
+    const file = /^([0-9]+)\.pdf$/i.exec(param.file);
+
+    result.unitid = `${param.cate}/${file[1]}/${param.page}`;
+  } else if ((match = /^\/membres\/destination\/([a-z0-9-_]+)\.htm$/i.exec(path)) !== null) {
+    // /membres/destination/CSJ_19111874.htm
+    result.rtype = 'REPORT';
+    result.mime = 'HTML';
+    result.unitid = match[1];
+  } else if ((match = /^\/membres\/files\/bancaire\/([a-z0-9-_]+)\.pdf$/i.exec(path)) !== null) {
+    // /files/bancaire/CSSF_1726.pdf
+    result.rtype = 'REPORT';
+    result.mime = 'PDF';
+    result.unitid = match[1];
+  } else if (/^\/membres\/destination\/fichier.php$/i.test(path)) {
+    // /membres/destination/fichier.php?fich=K0604C3811K3606.htm
+    result.rtype = 'JURISPRUDENCE';
+    result.mime = 'HTML';
+
+    const fich = /^([a-z0-9]+)\.htm$/i.exec(param.fich);
+
+    result.unitid = fich[1];
   }
 
   return result;
