eb1c031f187f3b3af7b5188e5d406fd7e02dc5ef
Karen Coombs <coombsk@oclc.org>
2021-10-14 19:07:51 -0500

    fixed karb parser

--- karb/parser.js
+++ karb/parser.js
@@ -27,16 +27,22 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     }
     if (param.id) {
       result.unitid = param.id;
-      result.rtype  = 'ARTICLE';
     }
     if (param.format && param.format == 'pdf') {
       result.unitid = param.ids;
-      result.rtype  = 'ARTICLE';
       result.mime   = 'PDF';
     }
+
     if (param.journal) {
       result.unitid = param.journal;
     }
+
+    if ((param.id && param.id.includes('Ch')) || (param.ids && param.ids.includes('Ch'))) {
+      result.rtype  = 'BOOK_SECTION';
+    } else if (param.id || param.ids) {
+      result.rtype  = 'ARTICLE';
+    }
+
     if (/([a-z]+)-tool/.test(match[2])) {
       result.rtype = 'TOOL';
     }
@@ -51,14 +57,19 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // /document/print?ids=KLI-KCC-1103101-n&title=PDF
     // /document/print?ids=KLI-KCC-1103101-n&title=PDF
     // /document/print?ids=KLI-KA-Paulsson-2016-Ch02%2CKLI-KA-Paulsson-2016-Ch03%2CKLI-KA-Paulsson-2016-Ch04%2CKLI-KA-Paulsson-2016-Ch05&pdf=
-
-    result.rtype  = 'BOOK_CHAPTERS_BUNDLE';
     result.mime   = 'PDF';
 
-    if (!Object.prototype.hasOwnProperty.call(param, 'pdf')) {
-      result.rtype  = undefined;
+    if ((param.id && param.id.includes('Ch')) || (param.ids && param.ids.includes('Ch'))) {
+      if ((param.id && param.id.match(/Ch/g).length > 1) || (param.ids && param.ids.match(/Ch/g).length > 1)) {
+        result.rtype  = 'BOOK_CHAPTERS_BUNDLE';
+      } else {
+        result.rtype  = 'BOOK_SECTION';
+      }
+    } else if (param.id || param.ids) {
+      result.rtype  = 'ARTICLE';
       result.unitid = param.ids;
     }
+
   } else if ((match = /^\/document\/download$/i.exec(path)) !== null) {
     // /document/download?ids=KLI-KA-Paulsson-2016-Ch03%2CKLI-KA-Paulsson-2016-Ch06%2CKLI-KA-Paulsson-2016-Ch07%2CKLI-KA-Paulsson-2016-b001&zip=
 
@@ -74,7 +85,11 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // /document/kli-ka-ai-2020-01-002?title=Arbitration%20International
     // /document/KLI-KCC-1103103-n
 
-    result.rtype  = param.title ? 'ARTICLE' : 'BOOK_SECTION';
+    if (param.title && param.title != 'PDF') {
+      result.rtype = 'ARTICLE';
+    } else {
+      result.rtype  = 'BOOK_SECTION';
+    }
     result.mime   = 'HTML';
     result.unitid = match[1];
   }
