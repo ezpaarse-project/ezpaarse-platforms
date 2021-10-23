bedf915dc2a459add7fa004daaf5a2f86fb124c3
Karen Coombs <coombsk@oclc.org>
2021-10-23 12:16:02 -0500

    created parser for hst

---
+++ hst/parser.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform Henry Stewart Talks
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path   = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+  let param = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  let match;
+
+  if ((match = /^\/upload\/content\/[a-z]+\/[a-z]+\/([0-9]+)\.pdf$/i.exec(path)) !== null) {
+    // https://hstalks.com/upload/content/talk/handouts/4692.pdf
+    result.rtype    = 'SUPPL';
+    result.mime     = 'PDF';
+    result.unitid = match[1];
+
+
+  } else if ((match = /^\/t\/([0-9]+)\/[a-z-]+\/$/i.exec(path)) !== null) {
+    // https://hstalks.com/t/4692/medical-decision-making-in-acute-care-philosophy-a/?biosci
+    // https://hstalks.com/t/4428/logistics-managing-geography/?business
+    result.rtype    = 'VIDEO';
+    result.mime     = 'HTML';
+    result.unitid   = match[1];
+    result.db_id = Object.getOwnPropertyNames(param)[0];
+  }
+
+  return result;
+});
