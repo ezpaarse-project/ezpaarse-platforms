dd1eff3bec88b37c356f424c9c1b3e4003dbca1d
felixleo22 <leofelixpro22@gmail.com>
2021-11-23 11:38:43 +0100

    adding omeka parser

---
+++ omeka/parser.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform OMEKA
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+  // let param = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  let match;
+
+  if ((match = /^\/files\/original\/([0-9a-z]+)\.(jpg|pdf)$/i.exec(path)) !== null) {
+    // /files/original/7f89e1ff3f55c4719445087d15f40b79.jpg
+    result.rtype = 'IMAGE';
+    result.mime = match[2] === 'pdf' ? 'PDF' : 'JPEG';
+    result.unitid = match[1];
+  } else if ((match = /^\/(ark:\/[0-9]+\/[a-z0-9]+)$/i.exec(path)) !== null) {
+    // /ark%3A/67375/WLXWKr99tB1E
+    result.rtype = 'REF';
+    result.mime = 'HTML';
+    result.unitid = match[1];
+    result.ark = match[1];
+  } else if (/^\/solr-search$/i.test(path)) {
+    // /solr-search?q=statue
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
+  } else if (/^\/items\/browse$/i.test(path)) {
+    // /items/browse
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
+  }
+
+  return result;
+});
