e42dfebe874eb81379db955983256d29a43c6ff3
felixleo22 <leofelixpro22@gmail.com>
2022-02-16 14:20:08 +0100

    update cub

--- cup/parser.js
+++ cup/parser.js
@@ -23,59 +23,59 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
 
   if (match) {
     switch (match[1]) {
-      case 'displayJournal':
-        result.unitid = param.jid;
-        result.rtype = 'TOC';
-        result.mime = 'HTML';
-        break;
-      case 'displayJournalTab':
-        // http://journals.cambridge.org.gate1.inist.fr/action/displayJournal?jid=VNS&bVolume=y
-        result.rtype = 'TOC';
-        result.mime = 'HTML';
-        break;
-      case 'displayIssue':
-        // http://journals.cambridge.org.gate1.inist.fr/action/displayIssue?decade=2010&jid=VNS&volumeId=27&issueId=3-4&iid=7880012
-        result.rtype = 'TOC';
-        result.mime = 'HTML';
-        result.unitid = param.iid;
+    case 'displayJournal':
+      result.unitid = param.jid;
+      result.rtype = 'TOC';
+      result.mime = 'HTML';
+      break;
+    case 'displayJournalTab':
+      // http://journals.cambridge.org.gate1.inist.fr/action/displayJournal?jid=VNS&bVolume=y
+      result.rtype = 'TOC';
+      result.mime = 'HTML';
+      break;
+    case 'displayIssue':
+      // http://journals.cambridge.org.gate1.inist.fr/action/displayIssue?decade=2010&jid=VNS&volumeId=27&issueId=3-4&iid=7880012
+      result.rtype = 'TOC';
+      result.mime = 'HTML';
+      result.unitid = param.iid;
+      result.volume = param.volumeId;
+      result.issue = param.issueId;
+      break;
+    case 'displayFulltext':
+      if (param.pdftype) {
+        // http://journals.cambridge.org.gate1.inist.fr/action/displayFulltext?type=1&pdftype=1&fid=7880027&jid=VNS&volumeId=27&issueId=3-4&aid=7880025
+        result.unitid = param.aid;
+        result.rtype = 'ARTICLE';
+        result.mime = 'PDF';
         result.volume = param.volumeId;
         result.issue = param.issueId;
-        break;
-      case 'displayFulltext':
-        if (param.pdftype) {
-          // http://journals.cambridge.org.gate1.inist.fr/action/displayFulltext?type=1&pdftype=1&fid=7880027&jid=VNS&volumeId=27&issueId=3-4&aid=7880025
+      } else {
+        // if (param.fulltextType == 'RA')
+        // http://journals.cambridge.org.gate1.inist.fr/action/displayFulltext?type=6&fid=7880026&jid=VNS&volumeId=27&issueId=3-4&aid=7880025&fulltextType=RA&fileId=S0952523810000179
+        if (!result.unitid) {
           result.unitid = param.aid;
-          result.rtype = 'ARTICLE';
-          result.mime = 'PDF';
-          result.volume = param.volumeId;
-          result.issue = param.issueId;
-        } else {
-          // if (param.fulltextType == 'RA')
-          // http://journals.cambridge.org.gate1.inist.fr/action/displayFulltext?type=6&fid=7880026&jid=VNS&volumeId=27&issueId=3-4&aid=7880025&fulltextType=RA&fileId=S0952523810000179
-          if (!result.unitid) {
-            result.unitid = param.aid;
-          }
-          result.rtype = 'ARTICLE';
-          result.mime = 'HTML';
-          result.volume = param.volumeId;
-          result.issue = param.issueId;
-          if (param.fileId) {
-            result.doi = '10.1017/' + param.fileId;
-          }
         }
-        break;
-      case 'displayAbstract':
-        // http://journals.cambridge.org.gate1.inist.fr/action/displayAbstract?fromPage=online&aid=9010487&fulltextType=RV&fileId=S0952523813000345
-        result.rtype = 'ABS';
+        result.rtype = 'ARTICLE';
         result.mime = 'HTML';
+        result.volume = param.volumeId;
+        result.issue = param.issueId;
         if (param.fileId) {
           result.doi = '10.1017/' + param.fileId;
         }
-        break;
-      default:
-        // if nothing recognized remove jid
-        result.title_id = null;
-        break;
+      }
+      break;
+    case 'displayAbstract':
+      // http://journals.cambridge.org.gate1.inist.fr/action/displayAbstract?fromPage=online&aid=9010487&fulltextType=RV&fileId=S0952523813000345
+      result.rtype = 'ABS';
+      result.mime = 'HTML';
+      if (param.fileId) {
+        result.doi = '10.1017/' + param.fileId;
+      }
+      break;
+    default:
+      // if nothing recognized remove jid
+      result.title_id = null;
+      break;
     }
   } else if ((match = /^\/core\/services\/aop-cambridge-core\/content\/view\/([a-z0-9]+)\/([SB]?[0-9]+)[a-z0-9._-]+\.pdf\//i.exec(pathname)) !== null) {
     // /core/services/aop-cambridge-core/content/view/A95C410BA1767D60C3DA96901466AABD/S1053837209990411a.pdf/old_generation_of_economists_and_the_new_an_intellectual_historians_approach_to_a_significant_transition.pdf
