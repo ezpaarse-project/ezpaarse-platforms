d2a3ab02eff13efbd8284cafa052e9e677d7a007
felixleo22 <leofelixpro22@gmail.com>
2022-03-17 09:41:15 +0100

    fix: add verif on database.json

--- wbo/parser.js
+++ wbo/parser.js
@@ -119,8 +119,8 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   }
 
   if (result.db_id) {
-    if (database[result.db_id].toUpperCase()) {
-      result.db_name = database[result.db_id.toUpperCase()];
+    if (database[result.db_id.toLowerCase()]) {
+      result.db_name = database[result.db_id.toLowerCase()];
     }
   }
 
