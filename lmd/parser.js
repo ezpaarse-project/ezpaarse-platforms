fe77e716ef78ebfdf7516a96bc995aa4c72cf87a
felixleo22 <leofelixpro22@gmail.com>
2022-04-27 11:20:13 +0200

    feat: update lmd parser

--- lmd/parser.js
+++ lmd/parser.js
@@ -12,24 +12,33 @@ const Parser = require('../.lib/parser.js');
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
-  let path   = parsedUrl.pathname;
-  // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let path = parsedUrl.pathname;
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
+  let match;
 
-  if (/^\/([a-z]+)\/article\/([0-9]+)\/([0-9]+)\/([0-9]+)\/([0-9a-z-_]+).html$/i.test(path)) {
-    // https://www.lemonde.fr/pixels/article/2021/01/20/investiture-de-joe-biden-les-soutiens-de-qanon-face-au-difficile-mur-de-la-realite_6066996_4408996.html
-    // https://www.lemonde.fr/societe/article/2019/06/26/la-maire-de-montauban-refuse-d-ouvrir-un-centre-d-accueil-pour-les-sans-abri_5481713_3224.html
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
+  if ((match = /^\/[a-z]+\/article\/([0-9]+)\/([0-9]{2})\/([0-9]{2})\/[a-z-_]+([0-9_]+).html$/i.exec(path)) !== null) {
+    // /pixels/article/2021/01/20/investiture-de-joe-biden-les-soutiens-de-qanon-face-au-difficile-mur-de-la-realite_6066996_4408996.html
+    // /societe/article/2019/06/26/la-maire-de-montauban-refuse-d-ouvrir-un-centre-d-accueil-pour-les-sans-abri_5481713_3224.html
+    result.rtype = 'ARTICLE';
+    result.mime = 'HTML';
+
+    result.publication_date = `${match[1]}/${match[2]}/${match[3]}`;
+    result.unitid = match[4];
 
   } else if (/^\/recherche\/$/i.test(path)) {
-    // https://www.lemonde.fr/recherche/?search_keywords=Biden&start_at=19%2F12%2F1944&end_at=20%2F01%2F2021&search_sort=relevance_desc
-    result.rtype    = 'SEARCH';
-    result.mime     = 'HTML';
+    // /recherche/?search_keywords=Biden&start_at=19%2F12%2F1944&end_at=20%2F01%2F2021&search_sort=relevance_desc
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
+
+  } else if (/^\/data\/[0-9]+\/reader\/reader.html$/i.test(path)) {
+    // /data/1990/reader/reader.html?t=1648651781505#!1990
+    result.rtype = 'ISSUE';
+    result.mime = 'HTML';
+    result.unitid = param.t;
   }
 
   return result;
