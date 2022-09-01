34c4ed0eff41000164afd20b58c7f17a0a5dccf2
Karen Coombs <coombsk@oclc.org>
2022-09-01 10:15:47 -0500

    added parser logic for britannica-online-jp

--- britannica-online-jp/parser.js
+++ britannica-online-jp/parser.js
@@ -14,33 +14,33 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
 
   let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
-    result.title_id = match[1];
+  if (/^\/search$/i.test(path)) {
+    // https://japan-eb-com/search?query1=man&matchType=f&x=0&y=0
+    // https://japan-eb.com/search?matchType=p&searchType=advanced&query1=man&exOp2=AND&query2=war&exOp3=AND&query3=american&exOp4=AND&query4=&gcode=&perPage=10&x=39&y=9
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
 
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
-    result.unitid = match[2];
-
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
+  } else if ((match = /^\/mb\/article-([0-9]+)_$/i.exec(path)) !== null) {
+    // https://japan-eb.com/mb/article-130900_
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
+    result.unitid   = match[1];
+  } else if (/^\/mb\/print$/i.test(path)) {
+    // https://japan-eb.com/mb/print?view=fullprint&articleId=130900
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'HTML';
+    result.unitid   = param.articleId;
+  } else if (/^\/resources\/pdf\/([a-z_]+)\.pdf$/i.test(path)) {
+    // https://japan-eb.com/resources/pdf/BOLJ_Guidetour.pdf
+    result.rtype    = 'RECORD_VIEW';
+    result.mime     = 'PDF';
   }
 
   return result;
