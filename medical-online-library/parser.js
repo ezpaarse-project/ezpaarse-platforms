a58dc7ce3e1e8b8ec5a9af2fe1176f22637e1e20
Karen Coombs <coombsk@oclc.org>
2022-09-05 13:33:08 -0400

    added parser for medical-online-library

--- medical-online-library/parser.js
+++ medical-online-library/parser.js
@@ -21,26 +21,19 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   let match;
 
-  if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.pdf)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.pdf?sequence=1
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
-    result.title_id = match[1];
-
-    /**
-     * unitid is a crucial information needed to filter double-clicks phenomenon, like described by COUNTER
-     * it described the most fine-grained of what's being accessed by the user
-     * it can be a DOI, an internal identifier or a part of the accessed URL
-     * more at http://ezpaarse.readthedocs.io/en/master/essential/ec-attributes.html#unitid
-     */
-    result.unitid = match[2];
-
-  } else if ((match = /^\/platform\/path\/to\/(document-([0-9]+)-test\.html)$/i.exec(path)) !== null) {
-    // http://parser.skeleton.js/platform/path/to/document-123456-test.html?sequence=1
-    result.rtype    = 'ARTICLE';
+  if (/^\/(index|library)\/(search|ebooks)(?:\/(result|search))?$/i.test(path)) {
+    // https://pha.medicalonline.jp/index/search?searchtarget=2&from=form_simple&from=form_option&criteria=ibuprofen&perpage_=10&is_iryouyaku_=1&is_iryo_zaikei_naiyou_=1&is_iryo_zaikei_gaiyou_=1&is_iryo_zaikei_chusya_=1&is_iryo_zaikei_shika_=1&is_shouhin_mei_=1&is_ippan_mei_=1&is_tekiou_sikkanmei_=1
+    // https://dev.medicalonline.jp/index/search?searchtarget=3&criteria=heart&perpage=10&accepted=0&accepted=1&unaccepted=0&unaccepted=1&maker_id=&from=form
+    // https://mol.medicalonline.jp/library/search/result?from=form_simple&query=dog&num=20&UserID=13.236.81.23
+    // https://mol.medicalonline.jp/library/ebooks/search?from=form_simple&purchased=0&query=medicine&num=20&UserID=13.236.81.23
+    result.rtype    = 'SEARCH';
     result.mime     = 'HTML';
-    result.title_id = match[1];
-    result.unitid   = match[2];
+
+  } else if ((match = /^\/tenpu_html\/iryouyaku\/([0-9a-z]+)\.htm$/i.exec(path)) !== null) {
+    // https://pha.medicalonline.jp/tenpu_html/iryouyaku/dat1149001F2214.htm
+    result.rtype    = 'ENCYCLOPAEDIA_ENTRY';
+    result.mime     = 'HTML';
+    result.unitid   = match[1];
   }
 
   return result;
