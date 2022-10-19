7906cfa98efc36134b388fa89bf5565daa684f03
Karen Coombs <coombsk@oclc.org>
2022-10-19 07:50:56 -0500

    Added title_id for journals

--- hk/parser.js
+++ hk/parser.js
@@ -25,6 +25,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // https://journals.humankinetics.com/view/journals/japa/30/5/article-p761.xml?content=pdf
     result.rtype = 'ARTICLE';
     result.mime = 'PDF';
+    result.title_id = match[1];
     result.vol = match[2];
     result.issue = match[3];
     result.unitid = match[1] + '-' + match[2] + '-' + match[3] + '-' + match[4];
@@ -32,6 +33,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // https://journals.humankinetics.com/view/journals/japa/30/5/article-p761.xml
     result.rtype = 'ABS';
     result.mime = 'HTML';
+    result.title_id = match[1];
     result.vol = match[2];
     result.issue = match[3];
     result.unitid = match[1] + '-' + match[2] + '-' + match[3] + '-' + match[4];
@@ -39,6 +41,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // https://journals.humankinetics.com/view/journals/krj/aop/article-10.1123-kr.2022-0018/article-10.1123-kr.2022-0018.xml?rskey=qLCrSF&result=2
     result.rtype    = 'ABS';
     result.mime     = 'HTML';
+    result.title_id = match[1];
     result.unitid   = match[4];
   } else if ((match = /^\/products\/([0-9a-z-]+)$/i.exec(path)) !== null) {
     // https://us.humankinetics.com/products/Brain-Compatible-Dance-Education-Web-Resource-2nd-Edition?
