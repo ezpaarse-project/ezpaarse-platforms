29b7dba109d2389a3cd58dacb4f024281fd01e56
Karen Coombs <coombsk@oclc.org>
2022-10-27 09:44:19 -0500

    added doi, TOC urls to parser

--- hk/parser.js
+++ hk/parser.js
@@ -21,28 +21,45 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   let match;
 
-  if ((match = /^\/view\/journals\/([a-z]+)\/([0-9]+)\/([0-9]+)\/([a-z0-9-]+)\.xml$/i.exec(path)) !== null && param.content == 'pdf') {
+  if ((match = /^\/view\/journals\/([a-z]+)\/([0-9]+)\/([0-9]+)\/(article-p([0-9]+))\.xml$/i.exec(path)) !== null && param.content == 'pdf') {
     // https://journals.humankinetics.com/view/journals/japa/30/5/article-p761.xml?content=pdf
     result.rtype = 'ARTICLE';
     result.mime = 'PDF';
     result.title_id = match[1];
     result.vol = match[2];
     result.issue = match[3];
+    result.spage = match[5];
     result.unitid = match[4];
-  } else if ((match = /^\/view\/journals\/([a-z]+)\/([0-9]+)\/([0-9]+)\/([a-z0-9-]+)\.xml$/i.exec(path)) !== null) {
+  } else if ((match = /^\/view\/journals\/([a-z]+)\/([0-9]+)\/([0-9]+)\/(article-p([0-9]+))\.xml$/i.exec(path)) !== null) {
     // https://journals.humankinetics.com/view/journals/japa/30/5/article-p761.xml
     result.rtype = 'ABS';
     result.mime = 'HTML';
     result.title_id = match[1];
     result.vol = match[2];
     result.issue = match[3];
+    result.spage = match[5];
     result.unitid = match[4];
-  } else if ((match = /^\/view\/journals\/([a-z]+)\/([a-z]+)\/([a-z0-9.-]+)\/([a-z0-9.-]+)\.xml$/i.exec(path)) !== null) {
+  } else if ((match = /^\/view\/journals\/([a-z]+)\/([0-9]+)\/([0-9]+)\/(([a-z+]+).([0-9-]+).issue-([0-9-]+))\.xml$/i.exec(path)) !== null) {
+    // https://journals.humankinetics.com/view/journals/apaq/38/4/apaq.38.issue-4.xml
+    result.rtype = 'TOC';
+    result.mime = 'HTML';
+    result.title_id = match[1];
+    result.vol = match[2];
+    result.issue = match[3];
+    result.unitid = match[4];
+  } else if ((match = /^\/view\/journals\/([a-z]+)\/(([a-z]+)-overview)\.xml$/i.exec(path)) !== null) {
+    // https://journals.humankinetics.com/view/journals/apaq/apaq-overview.xml
+    result.rtype = 'TOC';
+    result.mime = 'HTML';
+    result.title_id = match[1];
+    result.unitid = match[2];
+  } else if ((match = /^\/view\/journals\/([a-z]+)\/([a-z]+)\/article-([a-z0-9.-]+)\/([a-z0-9.-]+)\.xml$/i.exec(path)) !== null) {
     // https://journals.humankinetics.com/view/journals/krj/aop/article-10.1123-kr.2022-0018/article-10.1123-kr.2022-0018.xml?rskey=qLCrSF&result=2
     result.rtype    = 'ABS';
     result.mime     = 'HTML';
     result.title_id = match[1];
     result.unitid   = match[4];
+    result.doi = match[3];
   } else if ((match = /^\/products\/([0-9a-z-]+)$/i.exec(path)) !== null) {
     // https://us.humankinetics.com/products/Brain-Compatible-Dance-Education-Web-Resource-2nd-Edition?
     result.rtype    = 'RECORD';
