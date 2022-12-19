053720ee0c153bf071fd93a00215e563a20311b6
Karen Coombs <coombsk@oclc.org>
2022-12-19 17:26:57 -0500

    updated parser for British Pharmacopoeia

--- bp/parser.js
+++ bp/parser.js
@@ -19,33 +19,28 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   // use console.error for debuging
   // console.error(parsedUrl);
 
-  let match;
-
-  if ((match = /^\/monographs\/([0-9a-z-]+)\.pdf$/i.exec(path)) !== null) {
+  if (/^\/monographs\/([0-9a-z-]+)\.pdf$/i.test(path)) {
     // https://www.pharmacopoeia.com/monographs/Phenylephrine-Eye-Drops.pdf
     result.rtype = 'REPORT';
     result.mime = 'PDF';
-    result.unitid = match[1];
-
-  } else if ((match = /^\/[0-9a-z-]+\/[a-z]+\/([a-z-]+)\.html$/i.exec(path)) !== null) {
+  } else if (/^\/$/i.test(path) || /^\/search$/i.test(path)) {
+    // https://www.pharmacopoeia.com?text=eye
+    // https://www.pharmacopoeia.com/search?text=eye
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
+  } else if (/^\/[0-9a-z-]+\/[a-z]+\/([a-z-]+)\.html$/i.test(path)) {
     // https://www.pharmacopoeia.com/bp-2023/surgical/absorbent-viscose-wadding.html?date=2023-01-01
     result.rtype = 'ISSUE';
     result.mime = 'HTML';
-    result.unitid = match[1];
-  } else if ((match = /^\/([0-9a-z]+)$/i.exec(path)) !== null) {
+  } else if (/^\/([0-9a-z]+)$/i.test(path)) {
     // https://www.pharmacopoeia.com/BP2023?utm_source=TSO&utm_medium=Carousel&utm_campaign=Just+Published+23&utm_id=Just+Published+
     result.rtype = 'ISSUE';
     result.mime = 'HTML';
-    result.unitid = match[1];
   } else if (/^\/Catalogue\/ProductDetails$/i.test(path)) {
     // https://www.pharmacopoeia.com/Catalogue/ProductDetails?productid=1000015262&page=1&pageSize=20&searchText=&startsWith=
     result.rtype = 'RECORD';
     result.mime = 'HTML';
     result.unitid = param.productid;
-  } else if (/^\/$/i.test(path)) {
-    // https://www.pharmacopoeia.com?text=eye
-    result.rtype = 'SEARCH';
-    result.mime = 'HTML';
   }
 
   return result;
