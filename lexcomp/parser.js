9239073f202087ff7ea913062450dca9478aaa69
Karen Coombs <coombsk@oclc.org>
2023-01-03 13:27:16 -0600

    updated lexcomp with unitid

--- lexcomp/parser.js
+++ lexcomp/parser.js
@@ -19,10 +19,12 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   // use console.error for debuging
   // console.error(parsedUrl);
 
-  if (/^\/lco\/action\/doc\/retrieve\/docid\/patch_f\/([0-9]+)$/i.test(path)) {
+  let match;
+  if ((match = /^\/lco\/action\/doc\/retrieve\/docid\/patch_f\/([0-9]+)$/i.exec(path)) !== null) {
     // http://online.lexi.com/lco/action/doc/retrieve/docid/patch_f/6292?cesid=0JCQpOh9bjp&searchUrl=%2Flco%2Faction%2Fsearch%3Fq%3Dalbuterol%26t%3Dname%26va%3Dalbuterol#goilist-ext
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
+    result.unitid = match[1];
   } else if (/^\/lco\/action\/search$/i.test(path)) {
     // http://online.lexi.com/lco/action/search?q=albuterol&t=name&va=albuterol
     result.rtype    = 'SEARCH';
