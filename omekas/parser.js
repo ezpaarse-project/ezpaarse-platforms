fedd934c32f2f44479144ad3ebc7cbc7befa8f1e
felixleo22 <leofelixpro22@gmail.com>
2023-01-11 14:21:22 +0100

    feat: omekas parser

--- omekas/parser.js
+++ omekas/parser.js
@@ -24,7 +24,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   if ((match = /^\/s\/([a-z]+)\/item\/([0-9]+)\/mirador$/i.exec(path)) !== null) {
     // /s/bjc/item/6181/mirador
     result.mime = 'HTML';
-    result.unitid = `${match[1]}${match[2]}`;
+    result.unitid = `${match[1]}-${match[2]}`;
     result.db_id = match[1];
   } else if ((match = /^\/s\/([a-z]+)\/searchAll$/i.exec(path)) !== null) {
     // /s/bjc/searchAll?q=grotte
@@ -40,7 +40,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // /s/bjc/item/6181
     result.rtype = 'RECORD_VIEW';
     result.mime = 'HTML';
-    result.unitid = `${match[1]}${match[2]}`;
+    result.unitid = `${match[1]}-${match[2]}`;
     result.db_id = match[1];
   }
 
