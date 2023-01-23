28b576577a2d2c3424085124c7df8bba8d2c4a23
felixleo22 <leofelixpro22@gmail.com>
2023-01-23 08:49:56 +0100

    style: use '===' than '=='

--- alkem/parser.js
+++ alkem/parser.js
@@ -19,7 +19,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   // use console.error for debuging
   // console.error(parsedUrl);
 
-  if (/^\/index\.php$/i.test(path) && param.view == 'article') {
+  if (/^\/index\.php$/i.test(path) && param.view === 'article') {
     // http://tpsg.alkemlibrary.com/index.php?option=com_content&id=11&view=article&catid=14#modal_63344d614850c8549
     result.rtype    = 'BOOK';
     result.mime     = 'HTML';
