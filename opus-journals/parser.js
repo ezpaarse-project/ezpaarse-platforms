438e05478de8b668089faca2074b3c128a81f992
Yannick Schurter <yannick.schurter@gmail.com>
2023-04-11 16:29:16 +0200

    opus-journals: handle optional /ojs prefix

--- opus-journals/parser.js
+++ opus-journals/parser.js
@@ -21,14 +21,14 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   let match;
 
-  if ((match = /^\/article\/view\/([0-9]+)$/i.exec(path)) !== null) {
+  if ((match = /^\/(?:ojs\/)?article\/view\/([0-9]+)$/i.exec(path)) !== null) {
     // /article/view/33
     result.rtype = 'ARTICLE';
     result.mime = 'HTML';
     result.unitid = `article/view/${match[1]}`;
 
 
-  } else if ((match = /^\/article\/view\/([0-9]+)\/([0-9]+)$/i.exec(path)) !== null) {
+  } else if ((match = /^\/(?:ojs\/)?article\/view\/([0-9]+)\/([0-9]+)$/i.exec(path)) !== null) {
     // article/view/33/43
     result.rtype = 'ARTICLE';
     result.mime = 'PDF';
