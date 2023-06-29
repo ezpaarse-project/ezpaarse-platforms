0fd18606da250781bdce8db9940cede42d9927b6
Karen Coombs <coombsk@oclc.org>
2023-06-28 21:06:16 -0500

    added parser for cabells

---
+++ cab/parser.js
@@ -0,0 +1,30 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform Cabells
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path   = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+  // let param = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  if (/^\/([a-z]+)$/i.test(path)) {
+    // https://www2.cabells.com/journalytics
+    // https://www2.cabells.com/predatory
+    result.rtype    = 'ANALYSIS';
+    result.mime     = 'HTML';
+  }
+
+  return result;
+});
