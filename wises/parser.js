15608c08c2f294241b53e466d124989f5b825425
Karen Coombs <coombsk@oclc.org>
2023-07-23 09:12:28 -0400

    added parser for WISE Search

---
+++ wises/parser.js
@@ -0,0 +1,32 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('../.lib/parser.js');
+
+/**
+ * Recognizes the accesses to the platform Wise Search
+ * @param  {Object} parsedUrl an object representing the URL to analyze
+ *                            main attributes: pathname, query, hostname
+ * @param  {Object} ec        an object representing the EC whose URL is being analyzed
+ * @return {Object} the result
+ */
+module.exports = new Parser(function analyseEC(parsedUrl, ec) {
+  let result = {};
+  let path   = parsedUrl.pathname;
+  // uncomment this line if you need parameters
+  // let param = parsedUrl.query || {};
+
+  // use console.error for debuging
+  // console.error(parsedUrl);
+
+  //let match;
+
+  if (/^\/wortal\/index\.do$/i.test(path)) {
+    // https://wisesearch.wisers.net/wortal/index.do?srp_restore=discard&new-login=true
+    result.rtype    = 'SESSION';
+    result.mime     = 'HTML';
+
+  }
+
+  return result;
+});
