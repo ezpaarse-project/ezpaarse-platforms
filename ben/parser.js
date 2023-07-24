e505531de44d97e2d05ce37e7e1e03fddf923e4a
felixleo22 <leofelixpro22@gmail.com>
2023-07-24 08:36:54 +0200

    feat: add db_id to ARTICLE HTML

--- ben/parser.js
+++ ben/parser.js
@@ -12,7 +12,7 @@ const Parser = require('../.lib/parser.js');
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
-  let path   = parsedUrl.pathname;
+  let path = parsedUrl.pathname;
   // uncomment this line if you need parameters
   // let param = parsedUrl.query || {};
 
@@ -23,22 +23,23 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   if ((match = /^\/online\/([a-z]+)\/publications\/([0-9]+)$/i.exec(path)) !== null) {
     // https://benjamins.com/online/etsb/publications/52963
-    result.rtype    = 'RECORD';
-    result.mime     = 'HTML';
+    result.rtype = 'RECORD';
+    result.mime = 'HTML';
     result.db_id = match[1];
     result.unitid = match[2];
 
-  } else if  ((match = /^\/online\/([a-z]+)\/articles\/([a-z0-9]+)?(\.[a-z]+)?$/i.exec(path)) !== null) {
+  } else if ((match = /^\/online\/([a-z]+)\/articles\/([a-z0-9]+)?(\.[a-z]+)?$/i.exec(path)) !== null) {
     // https://www.benjamins.com/online/hts/articles/ada1
     // https://benjamins.com/online/hts/articles/hyb1.fr
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
+    result.rtype = 'ARTICLE';
+    result.mime = 'HTML';
+    result.db_id = match[1];
     result.title_id = match[1];
-    result.unitid   = match[2];
-  } else if  ((match = /^\/online\/([a-z]+)\/search$/i.exec(path)) !== null) {
+    result.unitid = match[2];
+  } else if ((match = /^\/online\/([a-z]+)\/search$/i.exec(path)) !== null) {
     // https://www.benjamins.com/online/hts/search?r=all&q=Adaption
-    result.rtype    = 'SEARCH';
-    result.mime     = 'HTML';
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
     result.db_id = match[1];
   }
 
