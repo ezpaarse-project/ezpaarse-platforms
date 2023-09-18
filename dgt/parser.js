3302df5b6326f4fda3f0f864ef1794fd6455310e
felixleo22 <leofelixpro22@gmail.com>
2023-09-18 10:14:19 +0200

    feat: add parser in dgt + lint

--- dgt/parser.js
+++ dgt/parser.js
@@ -12,7 +12,7 @@ const Parser = require('../.lib/parser.js');
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
-  let path   = parsedUrl.pathname;
+  let path = parsedUrl.pathname;
   // uncomment this line if you need parameters
   let param = parsedUrl.query || {};
 
@@ -21,25 +21,33 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   let match;
 
+  if ((match = /^\/a\/([0-9]+)$/i.exec(path)) !== null) {
+    // https://www.digitaliapublishing.com/a/85727
+    result.rtype = 'RECORD';
+    result.mime = 'HTML';
+    result.title_id = match[1];
+    result.unitid = match[1];
+  }
+
   if ((match = /^\/a\/([0-9]+)\/([a-z0-9-]+)$/i.exec(path)) !== null) {
     // https://www.digitaliapublishing.com/a/85727/la-hendidura-de-la-roca
     // https://www.digitaliapublishing.com/a/54993/agronomia-costarricense--volumen-40--numero-1
-    result.rtype    = 'RECORD';
-    result.mime     = 'HTML';
+    result.rtype = 'RECORD';
+    result.mime = 'HTML';
     result.title_id = match[1];
     result.unitid = match[1];
 
   } else if (/^\/viewepub\/$/i.test(path)) {
     // https://www.digitaliapublishing.com/viewepub/?id=85727
     // https://www.digitaliapublishing.com/viewepub/?id=125550
-    result.rtype    = 'BOOK';
-    result.mime     = 'HTML';
+    result.rtype = 'BOOK';
+    result.mime = 'HTML';
     result.title_id = param.id;
-    result.unitid   = param.id;
+    result.unitid = param.id;
   } else if (/^\/fulltext$/i.test(path)) {
     // https://www.digitaliapublishing.com/fulltext
-    result.rtype    = 'SEARCH';
-    result.mime     = 'HTML';
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
   }
 
   return result;
