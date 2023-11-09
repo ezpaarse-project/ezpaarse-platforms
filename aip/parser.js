30fc24a70062a742fe78e3df19b9670c1a781dd0
Karen Coombs <librarywebchic@gmail.com>
2023-11-09 14:20:52 -0600

    Update aip/parser.js
    Co-authored-by: Tom SUBLET <34627360+oxypomme@users.noreply.github.com>

--- aip/parser.js
+++ aip/parser.js
@@ -83,14 +83,14 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.mime  = 'PDF';
     result.title_id = match[1];
     result.unitid = match[2];
-  } else if ((match = /^\/aip\/([a-z]+)\/article\/([0-9]+)\/([0-9]+)\/([0-9]+)\/([0-9]+)\/([a-z-]+)$/i.exec(path)) !== null) {
+  } else if ((match = /^\/aip\/([a-z]+)\/article\/([0-9]+)\/([0-9]+)\/[0-9]+\/([0-9]+)\/[a-z-]+$/i.exec(path)) !== null) {
     //https://pubs.aip.org/aip/acp/article/2872/1/120047/2913626/Brain-activity-vs-seismicity-Scaling-and-memory?searchresult=1
     result.rtype = 'ARTICLE';
     result.mime  = 'HTML';
     result.title_id = match[1];
     result.vol  = match[2];
     result.issue = match[3];
-    result.unitid = match[5];
+    result.unitid = match[4];
   } else if ((match = /^\/aip\/([a-z]+)\/article-abstract\/([0-9]+)\/([0-9]+)\/([0-9]+)\/([0-9]+)\/([a-z-]+)$/i.exec(path)) !== null) {
     //https://pubs.aip.org/aip/acp/article-abstract/2845/1/050013/2911196/Brain-cancer-detection-in-the-MRI-using-the?redirectedFrom=fulltext
     result.rtype = 'ABS';
