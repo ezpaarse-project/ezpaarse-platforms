3adaddd474e80d473c1f70a72851dfe48fce69c8
Karen Coombs <librarywebchic@gmail.com>
2023-11-13 03:24:39 -0600

    feat(api): updated parser for American Institute of Physics (#766)
    * added new test data
    
    * updated parser for American Institute of Physics
    
    * Update aip/parser.js
    
    Co-authored-by: Tom SUBLET <34627360+oxypomme@users.noreply.github.com>
    
    * Update aip/parser.js
    
    Co-authored-by: Tom SUBLET <34627360+oxypomme@users.noreply.github.com>
    
    * Update aip/parser.js
    
    Co-authored-by: Tom SUBLET <34627360+oxypomme@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Karen Coombs <coombsk@oclc.org>
    Co-authored-by: Tom SUBLET <34627360+oxypomme@users.noreply.github.com>

--- aip/parser.js
+++ aip/parser.js
@@ -20,12 +20,14 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
     result.rtype    = 'ARTICLE';
     result.mime     = match[3].toUpperCase();
 
-  } else if ((match = /^\/content\/[a-z]+\/journal\/(([a-z0-9]+)\/[0-9]+\/[0-9]+)$/i.exec(path)) !== null) {
+  } else if ((match = /^\/content\/[a-z]+\/journal\/(([a-z0-9]+)\/([0-9]+)\/([0-9]+))$/i.exec(path)) !== null) {
     // /content/aip/journal/chaos/21/4
     result.title_id = match[2];
     result.unitid   = match[1];
     result.rtype    = 'TOC';
     result.mime     = 'HTML';
+    result.vol = match[3];
+    result.issue = match[4];
 
   } else if ((match = /^\/content\/[a-z]+\/journal\/([a-z0-9]+)\/browse$/i.exec(path)) !== null) {
     // /content/aip/journal/chaos/browse
@@ -68,7 +70,45 @@ module.exports = new Parser(function analyseEC(parsedUrl) {
       break;
     }
 
+  } else if ((match = /^\/aip\/([a-z]+)\/article-pdf\/doi\/(10\.[0-9]+\/([a-z0-9.]+))\/[0-9]+\/[0-9._]+\.pdf$/i.exec(path)) !== null) {
+    // https://pubs.aip.org/aip/acp/article-pdf/doi/10.1063/5.0164351/18116273/020004_1_5.0164351.pdf
+    result.rtype = 'ARTICLE';
+    result.mime  = 'PDF';
+    result.title_id = match[1];
+    result.doi = match[2];
+    result.unitid = match[3];
+  } else if ((match = /^\/aip\/([a-z]+)\/article-pdf\/[0-9]+\/([0-9._]+)\.pdf$/i.exec(path)) !== null) {
+    // https://pubs.aip.org/aip/acp/article-pdf/18146317/020023_1_5.0167690.pdf
+    result.rtype = 'ARTICLE';
+    result.mime  = 'PDF';
+    result.title_id = match[1];
+    result.unitid = match[2];
+  } else if ((match = /^\/aip\/([a-z]+)\/article\/([0-9]+)\/([0-9]+)\/[0-9]+\/([0-9]+)\/[a-z-]+$/i.exec(path)) !== null) {
+    //https://pubs.aip.org/aip/acp/article/2872/1/120047/2913626/Brain-activity-vs-seismicity-Scaling-and-memory?searchresult=1
+    result.rtype = 'ARTICLE';
+    result.mime  = 'HTML';
+    result.title_id = match[1];
+    result.vol  = match[2];
+    result.issue = match[3];
+    result.unitid = match[4];
+  } else if ((match = /^\/aip\/([a-z]+)\/article-abstract\/([0-9]+)\/([0-9]+)\/[0-9]+\/([0-9]+)\/[a-z-]+$/i.exec(path)) !== null) {
+    //https://pubs.aip.org/aip/acp/article-abstract/2845/1/050013/2911196/Brain-cancer-detection-in-the-MRI-using-the?redirectedFrom=fulltext
+    result.rtype = 'ABS';
+    result.mime = 'HTML';
+    result.title_id = match[1];
+    result.vol = match[2];
+    result.issue = match[3];
+    result.unitid = match[4];
+  } else if ((match = /^\/docserver\/fulltext\/[a-z]+\/journal\/([a-z0-9]+)\/[0-9]+\/[0-9]+\/([0-9a-z.]+\.gif)$/i.exec(path)) !== null) {
+    //http://scitation.aip.org/docserver/fulltext/aip/journal/chaos/21/4/1.3665984.online.f1.gif
+    result.title_id = match[1];
+    result.unitid   = match[2];
+    result.rtype    = 'IMAGE';
+    result.mime     = 'GIF';
+  } else if (/^\/search-results$/i.test(path)) {
+    //https://pubs.aip.org/search-results?page=1&q=rocks
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
   }
-
   return result;
 });
