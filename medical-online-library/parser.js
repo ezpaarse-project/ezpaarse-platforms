dadaa9f6141daa05410c21621427d1fe3e73bf48
Karen Coombs <coombsk@oclc.org>
2024-02-22 11:16:38 -0600

    updated medical-online-library parser

--- medical-online-library/parser.js
+++ medical-online-library/parser.js
@@ -3,6 +3,15 @@
 'use strict';
 const Parser = require('../.lib/parser.js');
 
+function getMetadata(parameter) {
+  let matchMetadata;
+  if ((matchMetadata = /^([a-z0-9]+)\/(\d{4})\/(\d{4})(\d{2})\/[0-9]+$/i.exec(parameter)) !== null) {
+    //dq4hdtib/2023/003201/008
+    console.log(matchMetadata);
+    return matchMetadata;
+  }
+}
+
 /**
  * Recognizes the accesses to the platform Medical Online Library
  * @param  {Object} parsedUrl an object representing the URL to analyze
@@ -14,7 +23,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  // let param = parsedUrl.query || {};
+  let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
@@ -34,7 +43,47 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.rtype    = 'ENCYCLOPAEDIA_ENTRY';
     result.mime     = 'HTML';
     result.unitid   = match[1];
+  } else if (/^\/library\/archive\/[search|select]/i.test(path)) {
+    // https://mol.medicalonline.jp/library/archive/search?jo=di4aatex&ye=2022&vo=27&issue=1&UserID=13.236.81.23
+    // https://mol.medicalonline.jp/library/archive/select?jo=di4aatex&UserID=13.236.81.23
+    result.rtype    = 'TOC';
+    result.mime     = 'HTML';
+    result.title_id = param.jo;
+    result.unitid   = param.jo;
+    if (param.ye) {
+      result.publication_date = param.ye;
+    }
+    if (param.vo) {
+      result.vol = param.vo;
+    }
+    if (param.issue) {
+      result.issue = param.issue;
+    }
+  } else if (/^\/library\/journal\/abstract/i.test(path)) {
+    // https://mol.medicalonline.jp/library/journal/abstract?GoodsID=dq4hdtib/2023/003201/008&name=0057-0066e&UserID=13.236.81.23
+    result.rtype    = 'ABS';
+    result.mime     = 'HTML';
+    result.title_id   = param.GoodsID.split('/')[0];
+    result.unitid   = param.GoodsID;
+    let metadata = getMetadata(param.GoodsID);
+    result.title_id   = metadata[1];
+    result.publication_date = metadata[2];
+    result.vol = metadata[3];
+    result.issue = metadata[4];
+    result.first_page = param.name.split('-')[0];
+    result.last_page = param.name.split('-')[1].slice(0, -1);
+  } else if (/^\/library\/journal\/download/i.test(path)) {
+    // https://mol.medicalonline.jp/library/journal/download?GoodsID=dq4hdtib/2019/002801/001&name=0001-0006e&UserID=13.236.81.23
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'PDF';
+    result.unitid   = param.GoodsID;
+    let metadata = getMetadata(param.GoodsID);
+    result.title_id   = metadata[1];
+    result.publication_date = metadata[2];
+    result.vol = metadata[3];
+    result.issue = metadata[4];
+    result.first_page = param.name.split('-')[0];
+    result.last_page = param.name.split('-')[1].slice(0, -1);
   }
-
   return result;
 });
