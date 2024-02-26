052cd2f18deb75536583eaae254797f51d7ad2c1
felixleo22 <leofelixpro22@gmail.com>
2024-02-26 14:16:37 +0100

    lint

--- asahi/parser.js
+++ asahi/parser.js
@@ -12,7 +12,7 @@ const Parser = require('../.lib/parser.js');
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
-  let path   = parsedUrl.pathname;
+  let path = parsedUrl.pathname;
   // uncomment this line if you need parameters
   let param = parsedUrl.query || {};
 
@@ -20,40 +20,40 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   // console.error(parsedUrl);
 
   if (/^\/photoDB\/detail\.html$/i.test(path)) {
-    // http://database.asahi.com/photoDB/detail.html?id=0000053140&keywords=a
-    result.rtype    = 'IMAGE';
-    result.mime     = 'HTML';
+    // /photoDB/detail.html?id=0000053140&keywords=a
+    result.rtype = 'IMAGE';
+    result.mime = 'HTML';
     result.unitid = param.id;
 
   } else if (/^\/library2\/topic\/t-detail\.php$/i.test(path)) {
-    // http://database.asahi.com/library2/topic/t-detail.php
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
+    // /library2/topic/t-detail.php
+    result.rtype = 'ARTICLE';
+    result.mime = 'HTML';
   } else if (/^\/library2\/graph\/g-image-frameset-main\.php$/i.test(path)) {
-    // http://database.asahi.com/library2/graph/g-image-frameset-main.php
-    result.rtype    = 'BOOK';
-    result.mime     = 'HTML';
+    // /library2/graph/g-image-frameset-main.php
+    result.rtype = 'BOOK';
+    result.mime = 'HTML';
   } else if (/^\/[a-z]+\/detail\/$/i.test(path)) {
-    // https://xsearch.asahi.com/kiji/detail/?1705417150644
-    // https://xsearch.asahi.com/kiji/detail/?1705417046206
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'HTML';
+    // /kiji/detail/?1705417150644
+    // /kiji/detail/?1705417046206
+    result.rtype = 'ARTICLE';
+    result.mime = 'HTML';
     result.unitid = Object.keys(parsedUrl.query)[0];
   } else if (/^\/[a-z]+\/pdf\/$/i.test(path)) {
-    // https://xsearch.asahi.com/shimen/pdf/?1705416744329
-    // https://xsearch.asahi.com/shimen/pdf/?1705416517642
-    result.rtype    = 'ARTICLE';
-    result.mime     = 'PDF';
+    // /shimen/pdf/?1705416744329
+    // /shimen/pdf/?1705416517642
+    result.rtype = 'ARTICLE';
+    result.mime = 'PDF';
     result.unitid = Object.keys(parsedUrl.query)[0];
   } else if (/^\/[a-z]+\/result\.html$/i.test(path)) {
-    // http://database.asahi.com/photoDB/result.html?search_method=top&image_keywords=a&btn_execution=Submit&keywords_synonymous=1&per_page=20&mode=thumbnail&sort_key1=category_code&sort_key2=shooting_location_code&sort_key3=shooting_date_acceptance_date&sort_type=1&ascdesc1=desc&ascdesc2=asc&ascdesc3=desc
-    result.rtype    = 'SEARCH';
-    result.mime     = 'HTML';
+    // /photoDB/result.html?search_method=top&image_keywords=a&btn_execution=Submit&keywords_synonymous=1&per_page=20&mode=thumbnail&sort_key1=category_code&sort_key2=shooting_location_code&sort_key3=shooting_date_acceptance_date&sort_type=1&ascdesc1=desc&ascdesc2=asc&ascdesc3=desc
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
   } else if (/^\/[a-z]+\/?([a-z]+\/)$/i.test(path)) {
-    // https://xsearch.asahi.com/top/
-    //https://xsearch.asahi.com/shimen/date/
-    result.rtype    = 'SEARCH';
-    result.mime     = 'HTML';
+    // /top/
+    // /shimen/date/
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
   }
 
   return result;
