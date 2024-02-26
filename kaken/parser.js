55855d09fa111dddea418a555795173b07f37ed5
felixleo22 <leofelixpro22@gmail.com>
2024-02-26 14:17:48 +0100

    lint

--- kaken/parser.js
+++ kaken/parser.js
@@ -12,7 +12,7 @@ const Parser = require('../.lib/parser.js');
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
-  let path   = parsedUrl.pathname;
+  let path = parsedUrl.pathname;
   // uncomment this line if you need parameters
   let param = parsedUrl.query || {};
 
@@ -21,29 +21,29 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   let match;
 
-  if ((match = /^\/[a-z]+\/file\/[a-z-0-9]+\/([a-z0-9]+)\.pdf$/i.exec(path)) !== null) {
+  if ((match = /^\/[a-z]+\/file\/[a-z0-9-]+\/([a-z0-9]+)\.pdf$/i.exec(path)) !== null) {
     // https://kaken.nii.ac.jp/en/file/KAKENHI-PROJECT-15K10014/15K10014seika.pdf
-    result.rtype    = 'REPORT';
-    result.mime     = 'PDF';
+    result.rtype = 'REPORT';
+    result.mime = 'PDF';
     result.unitid = match[1];
 
   } else if (/^\/[a-z]+\/[a-z]+\/download\/$/i.test(path)) {
     // https://nrid.nii.ac.jp/en/nrid/download/?rkey=90421825&mode=grants
     // https://nrid.nii.ac.jp/en/nrid/download/?rkey=20209399&mode=grants
-    result.rtype    = 'RECORD';
-    result.mime     = 'CSV';
-    result.unitid   = param.rkey;
+    result.rtype = 'RECORD';
+    result.mime = 'CSV';
+    result.unitid = param.rkey;
   } else if ((match = /^\/[a-z]+\/[a-z]+\/([a-z0-9-]+)\/$/i.exec(path)) !== null) {
     // https://nrid.nii.ac.jp/en/nrid/1000090421825/
     // https://kaken.nii.ac.jp/en/grant/KAKENHI-PROJECT-15H04637/
-    result.rtype    = 'RECORD';
-    result.mime     = 'HTML';
-    result.unitid   = match[1];
+    result.rtype = 'RECORD';
+    result.mime = 'HTML';
+    result.unitid = match[1];
   } else if (/^\/([a-z]+\/)?search\/$/i.test(path)) {
     // https://kaken.nii.ac.jp/search/?kw=physics
     // https://kaken.nii.ac.jp/en/search/?qm=90421825
-    result.rtype    = 'SEARCH';
-    result.mime     = 'HTML';
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
   }
 
   return result;
