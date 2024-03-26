e3cd2b26e1187aa95673848926c19ac8b212844d
Wayne <internet221102@gmail.com>
2024-03-26 15:36:01 +0800

    update parser morning

--- morning/parser.js
+++ morning/parser.js
@@ -14,7 +14,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   let result = {};
   let path   = parsedUrl.pathname;
   // uncomment this line if you need parameters
-  let param = parsedUrl.query || {};
+  // let param = parsedUrl.query || {};
 
   // use console.error for debuging
   // console.error(parsedUrl);
@@ -25,18 +25,14 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     // http://library.morningstar.com/ArticleSuppot/article?id=1016715
     result.rtype    = 'ARTICLE';
     result.mime     = 'HTML';
-    result.unitid = param.id;
-
   } else if ((match = /^\/videos\/$/i.exec(path)) !== null) {
     // http://library.morningstar.com/videos/?id=1017223
     result.rtype    = 'VIDEO';
     result.mime     = 'MISC';
-    result.unitid   = param.id;
   } else if ((match = /^\/v2\/quote$/i.exec(path)) !== null) {
     // http://library.morningstar.com/v2/quote?id=0P0001KOSA&typeid=ST
     result.rtype    = 'REPORT';
     result.mime     = 'HTML';
-    result.unitid   = param.id;
   } else if ((match = /^\/Returns\/([0-9a-z]+.html)$/i.exec(path)) !== null) {
     // http://library.morningstar.com/Returns/CategoryReturns.html
     result.rtype    = 'REPORT';
@@ -44,6 +40,31 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   } else if ((match = /^\/Returns\/([0-9a-z_]+).pdf$/i.exec(path)) !== null) {
     // http://library.morningstar.com/Returns/Consumer_Cyclical_Q4_2020.pdf
     result.rtype    = 'REPORT';
+    result.mime     = 'PDF';
+  } else if (/^\/home$/i.test(path) && parsedUrl.hostname === 'research.morningstar.com') {
+    // https://research.morningstar.com/home
+    result.rtype    = 'SESSION';
+    result.mime     = 'HTML';
+  } else if (/^\/chart$/i.test(path) && parsedUrl.hostname === 'research.morningstar.com') {
+    // https://research.morningstar.com/chart
+    result.rtype    = 'DATASET';
+    result.mime     = 'HTML';
+  } else if (/^\/calendar$/i.test(path) && parsedUrl.hostname === 'research.morningstar.com') {
+    // https://research.morningstar.com/calendar
+    result.rtype    = 'TOOL';
+    result.mime     = 'HTML';
+  } else if ((match = /^\/articles\/([0-9a-zA-Z]+)\/([a-zA-Z0-9-]+)$/i.exec(path)) !== null) {
+    // https://research.morningstar.com/articles/1190828/charging-the-future-unleashing-the-power-of-battery-technology
+    // https://research.morningstar.com/articles/1189996/inflation-still-expected-to-plummet
+    result.rtype    = 'ARTICLE';
+    result.mime     = 'HTML';
+    result.title_id   = match[2];
+    result.db_id      = match[1];
+    result.unitid     = `${match[1]}/${match[2]}`;
+  } else if ((match = /^\/api\/v1\/articles\/([0-9]+)\/file$/i.exec(path)) !== null) {
+    // https://research.morningstar.com/api/v1/articles/1189179/file?type=AnalystClientResearch
+    // https://research.morningstar.com/api/v1/articles/20151/file?type=CommonArticle
+    result.rtype    = 'ARTICLE';
     result.mime     = 'PDF';
     result.unitid   = match[1];
   }
