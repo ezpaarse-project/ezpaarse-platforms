23adb92bc7221d09a5eafbc38c775b909fdc26a9
Wayne <internet221102@gmail.com>
2024-08-15 14:25:09 +0800

    develop parser euromonitor

--- euromonitor/parser.js
+++ euromonitor/parser.js
@@ -18,8 +18,7 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
 
   // use console.error for debuging
   // console.error(parsedUrl);
-
-  // let match;
+  let match;
 
   if (/^\/portal\/policy\/accepttermsandconditions$/i.test(path)) {
     // http://www.portal.euromonitor.com:80/portal/policy/accepttermsandconditions?ControllerName=Default&ActionName=Index
@@ -27,6 +26,10 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime     = 'MISC';
   } else if (/^\/([A-z]+)\/main.js$/i.test(path)) {
     // http://www.portal.euromonitor.com:80/searchui/main.js?version=20180704_1
+    result.rtype    = 'SEARCH';
+    result.mime     = 'HTML';
+  } else if (/^\/resultslist\/index$/i.test(path)) {
+    // https://www.portal.euromonitor.com/resultslist/index
     result.rtype    = 'SEARCH';
     result.mime     = 'HTML';
   } else if (/^\/portal\/dashboard\/analysisrelateddashboards$/i.test(path)) {
@@ -46,6 +49,31 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.rtype    = 'DATASET';
     result.mime     = 'HTML';
     result.unitid   = param._;
+  } else if (/^\/StatisticsEvolution\/index$/i.test(path)) {
+    // https://www.portal.euromonitor.com/StatisticsEvolution/index
+    result.rtype    = 'DATASET';
+    result.mime     = 'HTML';
+  } else if (/^\/analysis\/tab$/i.test(path)) {
+    // https://www.portal.euromonitor.com/analysis/tab
+    result.rtype    = 'ANALYSIS';
+    result.mime     = 'HTML';
+  } else if (/^\/CountryReportsApi\/download-report$/i.test(path) && param.documentId !== undefined) {
+    // https://api.passport.euromonitor.com/CountryReportsApi/download-report?documentId=21003
+    // https://api.passport.euromonitor.com/CountryReportsApi/download-report?documentId=2100395
+    result.rtype    = 'ANALYSIS';
+    result.mime     = 'PDF';
+    result.unitid   = param.documentId;
+  } else if (/^\/dashboard\/dashboarddetails$/i.test(path) && param.id !== undefined) {
+    // https://www.portal.euromonitor.com/dashboard/dashboarddetails?id=766edb3d-2fe1-4af9-8f65-e4d88fd2e762
+    // https://www.portal.euromonitor.com/dashboard/dashboarddetails?id=e41c23bc-20d4-4442-99d0-87df52e25c35
+    result.rtype    = 'MAP';
+    result.mime     = 'HTML';
+    result.unitid   = param.id;
+  } else if ((match = /^\/documents\/([a-zA-Z0-9%-/]+).ppt$/i.exec(path)) !== null) {
+    // https://download.portal.euromonitor.com/documents/67/ppt/f-1970309-39143267.ppt?sv=2019-02-02&sr=b&sig=zW0XRGYbYieXfjjyD9qWokohhdL0AKLdXWDlvH3l594%3D&se=2024-08-06T18%3A38%3A56Z&sp=r&rscd=attachment%3B%20filename%20%3D%20Analysis.ppt
+    result.rtype    = 'ANALYSIS';
+    result.mime     = 'MISC';
+    result.unitid   = match[1];
   }
   return result;
 });
