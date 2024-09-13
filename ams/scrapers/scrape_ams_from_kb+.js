cdb2e3f90e159d8e6c90a24dad624266f4d06228
Yannick Schurter <yannick.schurter@gmail.com>
2024-09-13 08:50:12 +0200

    feat: replace KBplus scrapers by Bacon

--- ams/scrapers/scrape_ams_from_kb+.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-// Write on KBART file the Masterlist Collection journals PKB
-// Write on stderr the progression
-
-'use strict';
-
-var pkbKbp = require('../../.lib/parsXmlKb+.js');
-
-pkbKbp.generatePkbKbp(1535, 'ams');
--- cup/scrapers/scrape_from_kbplus.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-
-var PkbKbp = require('../../.lib/parsXmlKb+.js');
-
-PkbKbp.generatePkbKbp(472, 'cup');
