8160407807e22c6166a268ee3c2e4a141b6a01f3
felixleo22 <leofelixpro22@gmail.com>
2024-11-20 11:32:40 +0100

    refactor: use else if and .test

--- numerique-premium/parser.js
+++ numerique-premium/parser.js
@@ -14,25 +14,19 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
     result.mime = 'HTML';
     result.title_id = match[1] + '/' + match[2];
     result.unitid = match[2];
-  }
-
-  if ((match = /^\/doi\/epdf\/(10.[0-9]+\/([a-z0-9-.]+))$/i.exec(path)) !== null) {
+  } else if ((match = /^\/doi\/epdf\/(10.[0-9]+\/([a-z0-9-.]+))$/i.exec(path)) !== null) {
     // https://www.numeriquepremium.com/doi/epdf/10.14375/NP.9782072798238
     result.rtype = 'BOOK';
     result.mime = 'PDF';
     result.unitid = match[2];
     result.doi = match[1];
-  }
-
-  if ((match = /^\/doi\/book\/(10.[0-9]+\/([a-z0-9-.]+))$/i.exec(path)) !== null) {
+  } else if ((match = /^\/doi\/book\/(10.[0-9]+\/([a-z0-9-.]+))$/i.exec(path)) !== null) {
     // https://www.numeriquepremium.com/doi/book/10.14375/NP.9782072798238#toc-containter
     result.rtype = 'TOC';
     result.mime = 'HTML';
     result.unitid = match[2];
     result.doi = match[1];
-  }
-
-  if ((match = /^\/action\/doSearch$/i.exec(path)) !== null) {
+  } else if (/^\/action\/doSearch$/i.test(path)) {
     // https://www-numeriquepremium-com.ezpaarse.univ-paris1.fr/action/doSearch
     result.rtype = 'SEARCH';
     result.mime = 'HTML';
