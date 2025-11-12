ba3068040122df9df5846591d5a75cb06ec7209d
Violita Kovchegov <66086030+violitak@users.noreply.github.com>
2025-11-12 09:46:08 -0500

    Remove unnecessary hostname check as suggested

--- informacionlegal/parser.js
+++ informacionlegal/parser.js
@@ -11,40 +11,36 @@ const Parser = require('../.lib/parser.js');
  * @return {Object} the result
  */
 module.exports = new Parser(function analyseEC(parsedUrl, ec) {
-  let result = {};
-  let path = parsedUrl.pathname;
-  let param = parsedUrl.query || {};
-  let hostname = parsedUrl.hostname;
+  const result = {};
+  const path = parsedUrl.pathname;
+  const param = parsedUrl.query || {};
 
-  // Handle publisher domain only
-  if (hostname && hostname.includes('informacionlegal.com.ar')) {
-    // Document access - ARTICLE/HTML with unitid from docguid
-    if (/^\/maf\/app\/document$/i.test(path) && param.docguid) {
-      result.rtype = 'ARTICLE';
-      result.mime = 'HTML';
-      result.unitid = param.docguid;
-    }
-    // TOC Home - TOC/HTML with unitid from tocguid
-    else if (/^\/maf\/api\/tocectoryHome$/i.test(path) && param.tocguid) {
-      result.rtype = 'TOC';
-      result.mime = 'HTML';
-      result.unitid = param.tocguid;
-    }
-    // Search templates
-    else if (/^\/maf\/api\/v1\/searchtemplates$/i.test(path)) {
-      result.rtype = 'SEARCH';
-      result.mime = 'HTML';
-    }
-    // Search filters
-    else if (/^\/maf\/app\/search\/filters\/retrieve$/i.test(path)) {
-      result.rtype = 'SEARCH';
-      result.mime = 'HTML';
-    }
-    // Search execution
-    else if (/^\/maf\/app\/search\/run\/multi$/i.test(path)) {
-      result.rtype = 'SEARCH';
-      result.mime = 'HTML';
-    }
+  // Document access - ARTICLE/HTML with unitid from docguid
+  if (/^\/maf\/app\/document$/i.test(path) && param.docguid) {
+    result.rtype = 'ARTICLE';
+    result.mime = 'HTML';
+    result.unitid = param.docguid;
+  }
+  // TOC Home - TOC/HTML with unitid from tocguid
+  else if (/^\/maf\/api\/tocectoryHome$/i.test(path) && param.tocguid) {
+    result.rtype = 'TOC';
+    result.mime = 'HTML';
+    result.unitid = param.tocguid;
+  }
+  // Search templates
+  else if (/^\/maf\/api\/v1\/searchtemplates$/i.test(path)) {
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
+  }
+  // Search filters
+  else if (/^\/maf\/app\/search\/filters\/retrieve$/i.test(path)) {
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
+  }
+  // Search execution
+  else if (/^\/maf\/app\/search\/run\/multi$/i.test(path)) {
+    result.rtype = 'SEARCH';
+    result.mime = 'HTML';
   }
 
   return result;
