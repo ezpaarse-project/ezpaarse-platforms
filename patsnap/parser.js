f503a0defd700a5c770b6239c3d29e1b8c7183f2
felixleo22 <leofelixpro22@gmail.com>
2026-01-28 07:49:36 +0100

    lint: put regex in if

--- patsnap/parser.js
+++ patsnap/parser.js
@@ -15,15 +15,16 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   const path = parsedUrl.pathname;
   const hostname = parsedUrl.hostname;
 
+  let match;
+
   // Ignore static assets
   if (/^discovery-static\.patsnap\.com$/i.test(hostname)) {
     return result;
   }
 
   // Company profile page: /company/{company-slug}/
-  const companyMatch = /^\/company\/([a-z0-9-]+)\/?$/i.exec(path);
-  if (companyMatch) {
-    const companySlug = companyMatch[1];
+  if ((match = /^\/company\/([a-z0-9-]+)\/?$/i.exec(path)) !== null) {
+    const companySlug = match[1];
     result.rtype = 'RECORD_VIEW';
     result.mime = 'HTML';
     result.title_id = companySlug;
@@ -32,10 +33,9 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   }
 
   // Company section pages: /company/{company-slug}/{section}/
-  const sectionMatch = /^\/company\/([a-z0-9-]+)\/(news|patent|funding|acquisition|investment|financial|expert|member)\/?$/i.exec(path);
-  if (sectionMatch) {
-    const companySlug = sectionMatch[1];
-    const section = sectionMatch[2].toLowerCase();
+  if ((match = /^\/company\/([a-z0-9-]+)\/(news|patent|funding|acquisition|investment|financial|expert|member)\/?$/i.exec(path)) !== null) {
+    const companySlug = match[1];
+    const section = match[2].toLowerCase();
 
     result.title_id = companySlug;
     result.unitid = `${companySlug}/${section}/`;
