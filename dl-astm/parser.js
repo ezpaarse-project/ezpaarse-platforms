5f828d9e7de29295b6c892a381fc5457f5cce4b3
felixleo22 <leofelixpro22@gmail.com>
2026-07-20 10:59:03 +0200

    docs: add comment to regex

--- dl-astm/parser.js
+++ dl-astm/parser.js
@@ -20,33 +20,42 @@ module.exports = new Parser(function analyseEC(parsedUrl, ec) {
   // console.error(parsedUrl);
 
   if ((match = /^\/books\/book\/chapter-pdf\/([0-9]+)\/([a-zA-Z0-9-_]+)\.pdf$/i.exec(path)) !== null) {
+    // /books/book/chapter-pdf/13691/10_1520_mnl12086d.pdf
+    // /books/book/chapter-pdf/33286/tr4_1fm.pdf
+    // /books/book/chapter-pdf/198088/tr5_1fm.pdf
+
     result.rtype  = 'BOOK_CHAPTERS_BUNDLE';
     result.mime   = 'PDF';
     result.unitid = match[1];
-
   } else if ((match = /^\/books\/monograph\/chapter-pdf\/([0-9]+)\/([a-zA-Z0-9-_]+)\.pdf$/i.exec(path)) !== null) {
+    // /books/monograph/chapter-pdf/206203/10_1520_mono62014fm.pdf
+
     result.rtype  = 'BOOK_CHAPTERS_BUNDLE';
     result.mime   = 'PDF';
     result.unitid = `${match[1]}/${match[2]}`;
-
   } else if ((match = /^\/books\/monograph\/([0-9]+)\/chapter\/[0-9]+\/([a-zA-Z0-9-_]+)/i.exec(path)) !== null) {
+    // /books/monograph/1733/chapter/616169/Chapter-3-Xenograft-Use-in-Orthopedic-Surgery
+
     result.rtype  = 'BOOK_CHAPTERS_BUNDLE';
     result.mime   = 'HTML';
     result.unitid = match[1];
-
   } else if ((match = /^\/jte\/article-pdf\/doi\/([^/]+\/[^/]+)\/([0-9]+)\/([a-zA-Z0-9-_]+)\.pdf$/i.exec(path)) !== null) {
+    // /jte/article-pdf/doi/10.1520/JTE20250226/234638/jte20250226.pdf
+
     result.doi    = match[1];
     result.unitid = match[2];
     result.rtype  = 'ARTICLE';
     result.mime   = 'PDF';
-
   } else if ((match = /^\/jte\/article\/doi\/([^/]+\/[^/]+)\/([0-9]+)\/([a-zA-Z0-9-_]+)/i.exec(path)) !== null) {
+    // /jte/article/doi/10.1520/JTE20250226/32400/A-Critical-Review-of-Engineering-Properties-of?searchresult=1
+
     result.doi    = match[1];
     result.unitid = match[2];
     result.rtype  = 'ARTICLE';
     result.mime   = 'HTML';
-
   } else if (/^\/search-results$/i.test(path)) {
+    // /search-results?page=1&q=environmental%20effects
+
     result.rtype = 'SEARCH';
     result.mime  = 'HTML';
   }
